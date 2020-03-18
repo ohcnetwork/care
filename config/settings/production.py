5d2c1bbea14cf191ae3c679ff6e762548ad74247
Vignesh Hari <vichuhari100@gmail.com>
2020-03-19 01:00:13 +0530

    Removed Whitenoise from production

--- config/settings/production.py
+++ config/settings/production.py
@@ -64,7 +64,7 @@ SECURE_CONTENT_TYPE_NOSNIFF = env.bool(
 
 # STATIC
 # ------------------------
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # MEDIA
 # ------------------------------------------------------------------------------
 
