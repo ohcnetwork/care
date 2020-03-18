0d68533d6b8295b06efe88ce6dd19daf8b32a54d
Vignesh Hari <vichuhari100@gmail.com>
2020-03-18 22:43:31 +0530

    String Representation of Skill Model

--- care/users/models.py
+++ care/users/models.py
@@ -17,6 +17,9 @@ class Skill(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
 
+    def __str__(self):
+        return self.name
+
 
 class User(AbstractUser):
 
