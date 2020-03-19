31ffb238d21252867f2f75d18eb8405bef9e5db6
Vignesh Hari <vichuhari100@gmail.com>
2020-03-19 18:21:16 +0530

    Added the rest of the models

--- care/facility/admin.py
+++ care/facility/admin.py
@@ -5,9 +5,21 @@ from facility.models import (
     FacilityLocation,
     FacilityStaff,
     FacilityVolunteer,
+    Building,
+    Room,
+    StaffRoomAllocation,
+    InventoryItem,
+    Inventory,
+    InventoryLog,
 )
 
 admin.site.register(Facility)
 admin.site.register(FacilityLocation)
 admin.site.register(FacilityStaff)
 admin.site.register(FacilityVolunteer)
+admin.site.register(Building)
+admin.site.register(Room)
+admin.site.register(StaffRoomAllocation)
+admin.site.register(InventoryItem)
+admin.site.register(Inventory)
+admin.site.register(InventoryLog)
--- care/facility/models.py
+++ care/facility/models.py
@@ -15,6 +15,9 @@ class DateBaseModel(models.Model):
         abstract = True
 
 
+# Facility Model Start
+
+
 class FacilityLocation(DateBaseModel):
     district = models.IntegerField(choices=DISTRICT_CHOICES, blank=False)
 
@@ -55,3 +58,118 @@ class FacilityVolunteer(DateBaseModel):
 
     def __str__(self):
         return str(self.volunteer) + " for facility " + str(self.facility)
+
+
+# Facility Model End
+
+
+# Building Model Start
+
+BUILDING_TYPES = [(1, "Educational Inst"), (2, "Hospital"), (3, "Other")]
+
+
+class Building(DateBaseModel):
+    facility = models.ForeignKey(
+        "Facility", on_delete=models.CASCADE, null=False, blank=False
+    )
+    name = models.CharField(max_length=1000)
+    num_rooms = models.IntegerField(validators=[MinValueValidator(0)], default=0)
+    num_floors = models.IntegerField(validators=[MinValueValidator(0)], default=0)
+    building_type = models.IntegerField(choices=BUILDING_TYPES)
+
+    def __str__(self):
+        return self.name + " under " + str(self.facility)
+
+
+# Building Model End
+
+
+# Room Model Start
+
+ROOM_TYPES = [(1, "Normal"), (10, "ICU"), (20, "ICCU")]
+
+
+class Room(DateBaseModel):
+    building = models.ForeignKey(
+        "Building", on_delete=models.CASCADE, null=False, blank=False
+    )
+    num = models.CharField(max_length=1000)
+    floor = models.IntegerField(validators=[MinValueValidator(0)], default=0)
+    beds_capacity = models.IntegerField(validators=[MinValueValidator(0)], default=0)
+    occupied_beds = models.IntegerField(validators=[MinValueValidator(0)], default=0)
+    room_type = models.IntegerField(choices=ROOM_TYPES)
+
+    def __str__(self):
+        return self.num + " under " + str(self.building)
+
+
+class StaffRoomAllocation(DateBaseModel):
+    staff = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, null=False, blank=False)
+
+    def __str__(self):
+        return str(self.staff) + " Allocated For " + str(self.room)
+
+
+# Room Model End
+
+# Inventory Model Start
+
+
+class InventoryItem(DateBaseModel):
+    name = models.CharField(max_length=1000)
+    description = models.TextField()
+    minimum_stock = models.IntegerField(validators=[MinValueValidator(0)], default=0)
+    unit = models.CharField(max_length=20)
+
+    def __str__(self):
+        return (
+            self.name
+            + " with unit "
+            + self.unit
+            + " with minimum stock "
+            + str(self.minimum_stock)
+        )
+
+
+class Inventory(DateBaseModel):
+    facility = models.ForeignKey(
+        "Facility", on_delete=models.CASCADE, null=False, blank=False
+    )
+    item = models.ForeignKey("InventoryItem", on_delete=models.CASCADE)
+    quantitiy = models.IntegerField(validators=[MinValueValidator(0)], default=0)
+
+    def __str__(self):
+        return (
+            self.item.name
+            + " : "
+            + str(self.quantitiy)
+            + " "
+            + self.item.unit
+            + " in "
+            + str(self.facility)
+        )
+
+
+class InventoryLog(DateBaseModel):
+    inventory = models.ForeignKey("Inventory", on_delete=models.CASCADE)
+    updated_by = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, blank=True
+    )
+    prev_count = models.IntegerField(validators=[MinValueValidator(0)], default=0)
+    new_count = models.IntegerField(validators=[MinValueValidator(0)], default=0)
+
+    def __str__(self):
+        return (
+            "Item "
+            + str(self.inventory)
+            + " Updated from "
+            + str(self.prev_count)
+            + " to "
+            + str(self.new_count)
+            + " updated by "
+            + str(self.updated_by)
+        )
+
+
+# Inventory Model End
