09250c5b42719463fb1e611a745aa6d0f99800ea
RejulS <s.rejul10@gmail.com>
2020-03-21 13:04:36 +0530

    Parts of #13

--- care/facility/forms.py
+++ care/facility/forms.py
@@ -7,13 +7,14 @@ class FacilityCreationForm(ModelForm):
     class Meta:
         model = Facility
         fields = ["name", "district", "address"]
+        labels = { "name":"Name of Hospital","address":"Enter Hospital Address" }
 
 
 class FacilityCapacityCreationForm(ModelForm):
     class Meta:
         model = FacilityCapacity
         fields = ["room_type", "total_capacity", "current_capacity"]
-
+        labels = {"room_type":"Bed Type","total_capacity":"Total Capacity","current_capacity":"Current Capacity Utilisation"}
 
 class DoctorsCountCreationForm(ModelForm):
     class Meta:
