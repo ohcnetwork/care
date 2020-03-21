c67063925813e2f04bc7015bce043079374ada8a
Abin Mn <abinmn619@gmail.com>
2020-03-21 21:34:12 +0530

    - Add drf to installed_apps - Add base url(api/v1) to urls.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -74,6 +74,7 @@ THIRD_PARTY_APPS = [
     "allauth.account",
     "allauth.socialaccount",
     "storages",
+    "rest_framework"
 ]
 
 LOCAL_APPS = ["care.users.apps.UsersConfig", "care.facility"]
