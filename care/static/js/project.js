850cbd96023ab8038183deb771e9305be36a4bb7
aswinmohanme <dev.aswinmohan@gmail.com>
2020-03-21 07:38:01 +0530

    Fix PR consistency issues

--- care/static/js/project.js
+++ care/static/js/project.js
@@ -1,17 +1,17 @@
 /* Project specific Javascript goes here. */
 
 // Use JS to Style all inputs with the same Tailwind classes
-let inputElements = document.querySelectorAll('input');
-let selectElements = document.querySelectorAll('select');
+let inputElements = document.querySelectorAll("input");
+let selectElements = document.querySelectorAll("select");
 
 for (let i = 0; i < inputElements.length; i++) {
     defaultElementStyle(inputElements[i]);
-    inputElements[i].classList.add('form-input');
+    inputElements[i].classList.add("form-input");
 }
 
 for (let i = 0; i < selectElements.length; i++) {
     defaultElementStyle(selectElements[i]);
-    selectElements[i].classList.add('form-select');
+    selectElements[i].classList.add("form-select");
 }
 
 function defaultElementStyle(elem) {
