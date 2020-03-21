0f353aa73e7e7e7fcb2e408159030b4664f0639b
aswinmohanme <dev.aswinmohan@gmail.com>
2020-03-21 07:30:24 +0530

    Add JS to style with default styling

--- care/static/js/project.js
+++ care/static/js/project.js
@@ -1,1 +1,21 @@
 /* Project specific Javascript goes here. */
+
+// Use JS to Style all inputs with the same Tailwind classes
+let inputElements = document.querySelectorAll('input');
+let selectElements = document.querySelectorAll('select');
+
+for (let i = 0; i < inputElements.length; i++) {
+    defaultElementStyle(inputElements[i]);
+    inputElements[i].classList.add('form-input');
+}
+
+for (let i = 0; i < selectElements.length; i++) {
+    defaultElementStyle(selectElements[i]);
+    selectElements[i].classList.add('form-select');
+}
+
+function defaultElementStyle(elem) {
+    elem.classList.add("w-full");
+    elem.classList.add("block");
+    elem.classList.add("my-1");
+}
