7b30307912b413e79ba5639473b783f3a677a693
Afzal Shahul Hameed <aflibnush@gmail.com>
2020-03-25 17:07:09 +0530

    Facility upsert owner check: Adds unit tests

--- care/facility/tests/test_facility.py
+++ care/facility/tests/test_facility.py
@@ -189,12 +189,17 @@ class TestFacility:
             ],
         }
 
-    def test_bulk_upsert(self, client, user, facility):
+
+@pytest.mark.django_db(transaction=True)
+class TestFacilityBulkUpsert:
+    def test_success(self, client, user, facility):
         capacity = FacilityCapacity.objects.create(
             facility=facility, room_type=1, total_capacity=50, current_capacity=0
         )
-
+        facility.created_by = user
+        facility.save()
         client.force_authenticate(user=user)
+
         name = "Another"
         address = "Dasappan's House, Washington Jn, OolaMukk P.O."
         phone_number = "7776665554"
@@ -282,3 +287,41 @@ class TestFacility:
             "oxygen_capacity": facility.oxygen_capacity,
             "phone_number": facility.phone_number,
         }
+
+    def test_others_cant_update_ones_facility(self, client, user, facility):
+        client.force_authenticate(user=user)
+        response = client.post(
+            "/api/v1/facility/bulk_upsert/",
+            data=[
+                {
+                    "name": facility.name,
+                    "district": facility.district,
+                    "facility_type": 3,
+                    "address": facility.address,
+                    "capacity": [
+                        {"room_type": 1, "total_capacity": 100, "current_capacity": 48,}
+                    ],
+                },
+            ],
+        )
+        assert response.status_code == 403
+
+    def test_admins_can_update_ones_facility(self, client, user, facility):
+        client.force_authenticate(user=user)
+        user.is_superuser = True
+        user.save()
+        response = client.post(
+            "/api/v1/facility/bulk_upsert/",
+            data=[
+                {
+                    "name": facility.name,
+                    "district": facility.district,
+                    "facility_type": 3,
+                    "address": facility.address,
+                    "capacity": [
+                        {"room_type": 1, "total_capacity": 100, "current_capacity": 48,}
+                    ],
+                },
+            ],
+        )
+        assert response.status_code == 204
