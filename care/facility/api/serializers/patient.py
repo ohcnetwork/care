661fadaae58466da1f329935470e3b0ea0e71f1e
Afzal Shahul Hameed <aflibnush@gmail.com>
2020-03-28 18:07:17 +0530

    Patient APIs: Returns last consultation details

--- care/facility/api/serializers/patient.py
+++ care/facility/api/serializers/patient.py
@@ -1,7 +1,9 @@
 from rest_framework import fields, serializers
 
+from care.facility.api.serializers.patient_consultation import PatientConsultationSerializer
 from care.facility.models import (
     MEDICAL_HISTORY_CHOICES,
+    PatientConsultation,
     PatientRegistration,
     PatientTeleConsultation,
 )
@@ -9,6 +11,13 @@ from care.facility.models import (
 
 class PatientSerializer(serializers.ModelSerializer):
     medical_history = fields.MultipleChoiceField(choices=MEDICAL_HISTORY_CHOICES)
+    last_consultation = serializers.SerializerMethodField()
+
+    def get_last_consultation(self, obj):
+        last_consultation = PatientConsultation.objects.filter(patient=obj).last()
+        if last_consultation:
+            return PatientConsultationSerializer(last_consultation).data
+        return None
 
     class Meta:
         model = PatientRegistration
