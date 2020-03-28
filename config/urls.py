94ed2669d259469f7e64e20753da10af3ba8ad32
Vignesh Hari <vichuhari100@gmail.com>
2020-03-28 20:30:01 +0530

    Added Rate Limiting in Login and Signup

--- config/urls.py
+++ config/urls.py
@@ -9,10 +9,11 @@ from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
     TokenRefreshView,
     TokenVerifyView,
 )
+
+from .auth_views import TokenObtainPairView
 
 from config import api_router
 
@@ -32,14 +33,28 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
-    path("ksdma/", TemplateView.as_view(template_name="pages/ksdma.html"), name="ksdma"),
+    path(
+        "ksdma/", TemplateView.as_view(template_name="pages/ksdma.html"), name="ksdma"
+    ),
     # API Docs
-    url(r"^swagger(?P<format>\.json|\.yaml)$", schema_view.without_ui(cache_timeout=0), name="schema-json",),
-    url(r"^swagger/$", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui",),
-    url(r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    url(
+        r"^swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    url(
+        r"^swagger/$",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    url(
+        r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
+    ),
     # Rest API
     path("api/v1/auth/login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/v1/auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path(
+        "api/v1/auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"
+    ),
     path("api/v1/auth/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
@@ -56,9 +71,21 @@ if settings.DEBUG:
     # This allows the error pages to be debugged during development, just visit
     # these url in browser to see how these error pages look like.
     urlpatterns += [
-        path("400/", default_views.bad_request, kwargs={"exception": Exception("Bad Request!")},),
-        path("403/", default_views.permission_denied, kwargs={"exception": Exception("Permission Denied")},),
-        path("404/", default_views.page_not_found, kwargs={"exception": Exception("Page not Found")},),
+        path(
+            "400/",
+            default_views.bad_request,
+            kwargs={"exception": Exception("Bad Request!")},
+        ),
+        path(
+            "403/",
+            default_views.permission_denied,
+            kwargs={"exception": Exception("Permission Denied")},
+        ),
+        path(
+            "404/",
+            default_views.page_not_found,
+            kwargs={"exception": Exception("Page not Found")},
+        ),
         path("500/", default_views.server_error),
     ]
     if "debug_toolbar" in settings.INSTALLED_APPS:
