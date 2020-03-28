0cb24623b086b88080a61eaccd1127d38d5409f3
Afzal Shahul Hameed <aflibnush@gmail.com>
2020-03-28 10:21:12 +0530

    Lexical corrections: Fixes line length

--- care/facility/migrations/0035_auto_20200328_0442.py
+++ care/facility/migrations/0035_auto_20200328_0442.py
@@ -6,30 +6,63 @@ import django.db.models.deletion
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('facility', '0034_auto_20200327_1628'),
-    ]
+    dependencies = [("facility", "0034_auto_20200327_1628")]
 
     operations = [
         migrations.CreateModel(
-            name='PatientConsultation',
+            name="PatientConsultation",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('suggestion', models.CharField(choices=[('HI', 'HOME ISOLATION'), ('A', 'ADMISSION'), ('R', 'REFERRAL')], max_length=3)),
-                ('admitted', models.BooleanField(default=False)),
-                ('admission_date', models.DateTimeField(null=True)),
-                ('discharge_date', models.DateTimeField(null=True)),
-                ('facility', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='consultations', to='facility.Facility')),
-                ('patient', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='consultations', to='facility.PatientRegistration')),
-                ('referred_to', models.ForeignKey(null=True, on_delete=django.db.models.deletion.PROTECT, related_name='referred_patients', to='facility.Facility')),
+                ("id", models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")),
+                (
+                    "suggestion",
+                    models.CharField(
+                        choices=[("HI", "HOME ISOLATION"), ("A", "ADMISSION"), ("R", "REFERRAL")], max_length=3
+                    ),
+                ),
+                ("admitted", models.BooleanField(default=False)),
+                ("admission_date", models.DateTimeField(null=True)),
+                ("discharge_date", models.DateTimeField(null=True)),
+                (
+                    "facility",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="consultations",
+                        to="facility.Facility",
+                    ),
+                ),
+                (
+                    "patient",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="consultations",
+                        to="facility.PatientRegistration",
+                    ),
+                ),
+                (
+                    "referred_to",
+                    models.ForeignKey(
+                        null=True,
+                        on_delete=django.db.models.deletion.PROTECT,
+                        related_name="referred_patients",
+                        to="facility.Facility",
+                    ),
+                ),
             ],
         ),
         migrations.AddConstraint(
-            model_name='patientconsultation',
-            constraint=models.CheckConstraint(check=models.Q(models.Q(_negated=True, suggestion='R'), ('referred_to__isnull', False), _connector='OR'), name='if_referral_suggested'),
+            model_name="patientconsultation",
+            constraint=models.CheckConstraint(
+                check=models.Q(
+                    models.Q(_negated=True, suggestion="R"), ("referred_to__isnull", False), _connector="OR"
+                ),
+                name="if_referral_suggested",
+            ),
         ),
         migrations.AddConstraint(
-            model_name='patientconsultation',
-            constraint=models.CheckConstraint(check=models.Q(('admitted', False), ('admission_date__isnull', False), _connector='OR'), name='if_admitted'),
+            model_name="patientconsultation",
+            constraint=models.CheckConstraint(
+                check=models.Q(("admitted", False), ("admission_date__isnull", False), _connector="OR"),
+                name="if_admitted",
+            ),
         ),
     ]
