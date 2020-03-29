3ad84d0eba1c28be7fd2a79088753414a483912b
Afzal Shahul Hameed <45993339+afzalIbnSH@users.noreply.github.com>
2020-03-29 10:11:28 +0530

    Users API: Restrict users from seeing other users' sensitive information (#159)
    Returns only id, first_name and last_name

--- care/users/tests/test_users.py
+++ care/users/tests/test_users.py
@@ -26,28 +26,36 @@ class TestUser:
 
         response = client.post("/api/v1/users/", data,)
         assert response.status_code == 201
+        newuser = response.json()
+        password = data.pop("password")
 
         client.force_authenticate(user=user)
 
         response = client.get("/api/v1/users/")
         assert response.status_code == 200
         assert response.json()["count"] == 2
-        assert user.username in {r["username"] for r in response.json()["results"]}
-        assert data["username"] in {r["username"] for r in response.json()["results"]}
-
-        response = client.get(f"/api/v1/users/{data['username']}/")
-        assert response.status_code == 200
-        res_data = response.json()
-        res_data.pop("id")
-        password = data.pop("password")
-        assert res_data == {
+        assert response.json()["results"][0]["username"] == user.username
+        assert {
             **data,
+            "id": newuser["id"],
             "district": 11,
             "gender": "Male",
             "user_type": "Doctor",
             "first_name": "",
             "last_name": "",
-        }
+        } == response.json()["results"][1], "reading others is not limited"
+
+        response = client.get(f"/api/v1/users/{data['username']}/")
+        assert response.status_code == 200
+        assert response.json() == {
+            **data,
+            "id": newuser["id"],
+            "district": 11,
+            "gender": "Male",
+            "user_type": "Doctor",
+            "first_name": "",
+            "last_name": "",
+        }, "reading others is not limited"
 
         response = client.put(f"/api/v1/users/{data['username']}/", {**data, "age": 31, "password": password,})
         assert response.status_code == 200
@@ -62,17 +70,27 @@ class TestUser:
     def test_user_can_read_cant_modify_delete_others(self, client, user, data):
         response = client.post("/api/v1/users/", data,)
         assert response.status_code == 201
+        newuser = response.json()
 
         client.force_authenticate(user=user)
 
         response = client.get("/api/v1/users/")
         assert response.status_code == 200
         assert response.json()["count"] == 2
-        assert user.username in {r["username"] for r in response.json()["results"]}
-        assert data["username"] in {r["username"] for r in response.json()["results"]}
+        assert response.json()["results"][0]["id"] == user.id
+        assert response.json()["results"][1] == {
+            "id": newuser["id"],
+            "first_name": newuser["first_name"],
+            "last_name": newuser["last_name"],
+        }, "reading others is limited"
 
         response = client.get(f"/api/v1/users/{data['username']}/")
         assert response.status_code == 200
+        assert response.json() == {
+            "id": newuser["id"],
+            "first_name": newuser["first_name"],
+            "last_name": newuser["last_name"],
+        }, "reading others is limited"
 
         response = client.put(f"/api/v1/users/{data['username']}/", data)
         assert response.status_code == 404
@@ -85,17 +103,27 @@ class TestUser:
         assert response.status_code == 201
 
         user = User.objects.get(username=data["username"])
+        password = data.pop("password")
         client.force_authenticate(user=user)
 
         response = client.get("/api/v1/users/")
         assert response.status_code == 200
         assert response.json()["count"] == 1
-        assert user.username in {r["username"] for r in response.json()["results"]}
+        assert user.id in {r["id"] for r in response.json()["results"]}
 
         response = client.get(f"/api/v1/users/{user.username}/")
         assert response.status_code == 200
+        assert response.json() == {
+            **data,
+            "id": user.id,
+            "district": 11,
+            "gender": "Male",
+            "user_type": "Doctor",
+            "first_name": "",
+            "last_name": "",
+        }
 
-        response = client.put(f"/api/v1/users/{user.username}/", {**data, "age": 31,})
+        response = client.put(f"/api/v1/users/{user.username}/", {**data, "age": 31, "password": password})
         assert response.status_code == 200
         assert response.json()["age"] == 31
         assert User.objects.only("age").get(username=data["username"]).age == 31
