249f1177289ee4c576d43a6194537356d3bd1485
Roopak A N <anroopak@gmail.com>
2020-03-31 13:35:26 +0530

    fixing facility tests

--- care/facility/models/facility.py
+++ care/facility/models/facility.py
@@ -102,6 +102,9 @@ class Facility(FacilityBaseModel):
     def has_write_permission(request):
         return True
 
+    def has_object_write_permission(self, request):
+        return True
+
     def has_object_read_permission(self, request):
         return True
 
--- care/utils/tests/test_base.py
+++ care/utils/tests/test_base.py
@@ -1,15 +1,18 @@
+import abc
 import datetime
 
 from django.contrib.gis.geos import Point
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from care.facility.models import Facility, User
+from care.facility.models import Facility, LocalBody, User
 from care.users.models import District, State
 
 
 class TestBase(APITestCase):
-    URL = ""
+    """
+    Base class for tests, handles most of the test setup and tools for setting up data
+    """
 
     @classmethod
     def create_user(cls, district: District, username: str = "user", **kwargs):
@@ -56,6 +59,7 @@ class TestBase(APITestCase):
 
     @classmethod
     def setUpClass(cls) -> None:
+        super(TestBase, cls).setUpClass()
         cls.state = cls.create_state()
         cls.district = cls.create_district(cls.state)
         cls.user = cls.create_user(cls.district)
@@ -65,13 +69,87 @@ class TestBase(APITestCase):
     def setUp(self) -> None:
         self.client.force_login(self.user)
 
+    @abc.abstractmethod
+    def get_base_url(self):
+        """
+        Should return the base url of the testing viewset
+        WITHOUT trailing slash
+
+        eg: return "api/v1/facility"
+        :return: str
+        """
+        raise NotImplementedError()
+
     def get_url(self, id=None, action=None):
-        url = self.URL
+        url = self.get_base_url()
         if id is not None:
             url = f"{url}/{id}"
         if action is not None:
             url = f"{url}/{action}"
         return f"{url}/"
+
+    @classmethod
+    def clone_object(cls, obj):
+        new_obj = obj._meta.model.objects.get(pk=obj.id)
+        new_obj.pk = None
+        new_obj.id = None
+        new_obj.save()
+        return new_obj
+
+    @abc.abstractmethod
+    def get_list_representation(self, obj) -> dict:
+        """
+        Returns the dict representation of the obj in list API
+        :param obj: Object to be represented
+        :return: dict
+        """
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def get_detail_representation(self, obj=None) -> dict:
+        """
+        Returns the dict representation of the obj in detail/retrieve API
+        :param obj: Object to be represented
+        :param data: data
+        :return: dict
+        """
+        raise NotImplementedError()
+
+    def get_local_body_district_state_representation(self, obj):
+        """
+        Returns the local body, district and state representation for the obj.
+        The obj is expected to have `local_body`, `district` and `state` in it's attributes
+        Eg: Facility, Patient, User
+
+        :param obj: Any object which has `local_body`, `district` and `state` in attrs
+        :return:
+        """
+        response = {}
+        response.update(self.get_local_body_representation(getattr(obj, "local_body", None)))
+        response.update(self.get_district_representation(getattr(obj, "district", None)))
+        response.update(self.get_state_representation(getattr(obj, "state", None)))
+
+    def get_local_body_representation(self, local_body: LocalBody):
+        if local_body is None:
+            return {"local_body": None, "local_body_object": None}
+        else:
+            return {
+                "local_body": local_body.id,
+                "local_body_object": {"id": local_body.id, "name": local_body.name, "district": local_body.district.id},
+            }
+
+    def get_district_representation(self, district: District):
+        if district is None:
+            return {"district": None, "district_object": None}
+        return {
+            "district": district.id,
+            "district_object": {"id": district.id, "name": district.name, "state": district.state.id},
+        }
+
+    def get_state_representation(self, state: State):
+        if state is None:
+            return {"state": None, "state_object": None}
+        return {"state": state.id, "state_object": {"id": state.id, "name": state.name}}
 
     def test_login_required(self):
         """Test unlogged unusers are thrown an error 403"""
