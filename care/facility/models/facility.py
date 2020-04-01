718c6c14582272ce048c1fb7d65f5d492938ac52
Vignesh Hari <vichuhari100@gmail.com>
2020-04-01 15:02:26 +0530

    Solved Ambulance Vehicle Number Regex Issue

--- care/facility/models/facility.py
+++ care/facility/models/facility.py
@@ -310,7 +310,7 @@ class InventoryLog(FacilityBaseModel):
 
 class Ambulance(FacilityBaseModel):
     vehicle_number_regex = RegexValidator(
-        regex="^[A-Z]{2}[0-9]{1,2}[A-Z]{1,2}[0-9]{1,4}$",
+        regex="^[A-Z]{2}[0-9]{1,2}[A-Z]{0,2}[0-9]{1,4}$",
         message="Please Enter the vehicle number in all uppercase without spaces, eg: KL13AB1234",
         code="invalid_vehicle_number",
     )
