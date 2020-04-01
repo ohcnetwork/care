9f0b301933d9f6b091829ac9005ee2647521a894
Afzal Shahul Hameed <45993339+afzalIbnSH@users.noreply.github.com>
2020-04-01 11:59:28 +0530

    Implements test sample status flow (#204)

--- care/facility/api/viewsets/patient_sample.py
+++ care/facility/api/viewsets/patient_sample.py
@@ -1,17 +1,17 @@
-import datetime
-
 from django.db import transaction
 from django.db.models import Prefetch
 from django.db.models.query_utils import Q
-from django.utils.timezone import make_aware
 from django_filters import rest_framework as filters
 from dry_rest_permissions.generics import DRYPermissionFiltersBase, DRYPermissions
 from rest_framework import viewsets
 from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import IsAuthenticated
 
-from care.facility.api.mixins import UserAccessMixin
-from care.facility.api.serializers.patient_sample import PatientSampleReadSerializer, PatientSampleSerializer
+from care.facility.api.serializers.patient_sample import (
+    PatientSamplePatchSerializer,
+    PatientSampleReadSerializer,
+    PatientSampleSerializer,
+)
 from care.facility.models import PatientSample, PatientSampleFlow, User, patient_data
 
 
@@ -35,7 +35,7 @@ class PatientSampleFilterSet(filters.FilterSet):
     result = filters.ChoiceFilter(choices=PatientSample.SAMPLE_TEST_RESULT_CHOICES)
 
 
-class PatientSampleViewSet(UserAccessMixin, viewsets.ModelViewSet):
+class PatientSampleViewSet(viewsets.ModelViewSet):
     serializer_class = PatientSampleSerializer
     queryset = PatientSample.objects.all().prefetch_related(
         Prefetch(
@@ -59,6 +59,8 @@ class PatientSampleViewSet(UserAccessMixin, viewsets.ModelViewSet):
         serializer_class = self.serializer_class
         if self.request.method == "GET":
             serializer_class = PatientSampleReadSerializer
+        elif self.request.method == "PATCH":
+            serializer_class = PatientSamplePatchSerializer
         return serializer_class
 
     def get_queryset(self):
@@ -108,11 +110,6 @@ class PatientSampleViewSet(UserAccessMixin, viewsets.ModelViewSet):
         validated_data = serializer.validated_data
         notes = validated_data.pop("notes", f"updated by {self.request.user.get_username()}")
         with transaction.atomic():
-            if validated_data.get("status") == PatientSample.SAMPLE_TEST_FLOW_MAP["SENT_TO_COLLECTON_CENTRE"]:
-                validated_data["date_of_sample"] = make_aware(datetime.datetime.now())
-            elif validated_data.get("result") is not None:
-                validated_data["date_of_result"] = make_aware(datetime.datetime.now())
-                validated_data["status"] = PatientSample.SAMPLE_TEST_FLOW_MAP["COMPLETED"]
             instance = serializer.update(serializer.instance, validated_data)
             instance.patientsampleflow_set.create(status=instance.status, notes=notes, created_by=self.request.user)
             return instance
