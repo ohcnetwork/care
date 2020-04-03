d11a419ed07b509693dc458c11d56c63cd254eab
abhiabhi94 <abhy.abhy94@gmail.com>
2020-04-03 20:30:24 +0530

    Add tests for others to work on fixing
    Failing tests
       - 2 in patient API
       - 5 in facility API
       - 3 in patient stats API
    
    After these tests are fixed, we can port completely to unittest

--- care/facility/tests/test_patient_api.py
+++ care/facility/tests/test_patient_api.py
@@ -1,59 +1,120 @@
+from typing import Any
+
 from rest_framework import status
-from rest_framework.test import APITestCase
 
 from care.facility.models import Disease, PatientRegistration
-from care.users.models import User
+from care.utils.tests.test_base import TestBase
 
 
-class TestPatient(APITestCase):
+class TestPatient(TestBase):
     """Test patient APIs"""
 
     @classmethod
-    def setUpTestData(cls):
+    def setUpClass(cls):
         """
-        Run once per class
-            - Create a patient
-            - Create a disease
-            - Create a user
+        Runs once per class
+            - Initialize the attributes useful for class methods
         """
+        super(TestPatient, cls).setUpClass()
         cls.patient_data = {
             "name": "Foo",
-            "age": 40,
+            "age": 32,
             "gender": 2,
-            "phone_number": "9998887776",
-            "contact_with_carrier": True,
-            "medical_history": [],
-            "medical_history_details": [{"disease": 1, "details": "Quite bad"}],
+            "phone_number": "8888888888",
+            "address": "Global citizen",
+            "contact_with_confirmed_carrier": True,
+            "contact_with_suspected_carrier": True,
+            "estimated_contact_date": None,
+            "past_travel": False,
+            "countries_travelled": None,
+            "present_health": "Fine",
+            "has_SARI": False,
+            "is_active": True,
+            "state": None,
+            "district": None,
+            "local_body": None,
+            "facility": None,
+            "state_object": None,
+            "local_body_object": None,
+            "district_object": None,
         }
-        cls.patient = PatientRegistration.objects.create(
-            name="Bar", age=31, gender=2, phone_number="7776665554", contact_with_carrier=False,
-        )
-        # flake8:noqa
-        disease = Disease.objects.create(disease=1, details="Quite bad", patient=cls.patient)
-        cls.user_data = {
-            "user_type": 5,
-            "email": "some.email@somedomain.com",
-            "phone_number": "5554446667",
-            "age": 30,
-            "gender": 2,
-            "district": 11,
-            "username": "user_2",
-            "password": "bar",
-        }
-        cls.user = User.objects.create_user(**cls.user_data)
 
-    def setUp(self):
-        """
-        Run once before every class method
-            - log in the user
-        """
-        self.client.login(username=self.user_data["username"], password=self.user_data["password"])
+        cls.patient = PatientRegistration.objects.create(name="Bar", age=31, gender=2, phone_number="7776665554",)
+        cls.patient_data["id"] = cls.patient.id
+
+    def get_base_url(self):
+        return "/api/v1/patient"
+
+    def get_list_representation(self, patient=None):
+
+        if isinstance(patient, dict):
+            medical_history = patient.pop("medical_history", default="[]")
+            patient = PatientRegistration(**patient)
+            patient.medical_history.set(medical_history)
+
+        return {
+            "id": patient.id,
+            "name": patient.name,
+            "age": patient.age,
+            "gender": patient.gender,
+            "phone_number": patient.phone_number,
+            "address": patient.address,
+            "contact_with_confirmed_carrier": patient.contact_with_confirmed_carrier,
+            "contact_with_suspected_carrier": patient.contact_with_suspected_carrier,
+            "estimated_contact_date": patient.estimated_contact_date,
+            "past_travel": patient.past_travel,
+            "countries_travelled": patient.countries_travelled,
+            "present_health": patient.present_health,
+            "has_SARI": patient.has_SARI,
+            "is_active": patient.is_active,
+            "state": patient.state,
+            "district": patient.district,
+            "local_body": patient.local_body,
+            "facility": patient.facility,
+            "state_object": None,
+            "local_body_object": None,
+            "district_object": None,
+        }
+
+    def get_detail_representation(self, patient: Any):
+        if isinstance(patient, dict):
+            medical_history = patient.pop("medical_history")
+            patient = PatientRegistration(**patient)
+            patient.medical_history.set(medical_history)
+
+        return {
+            "id": patient.id,
+            "name": patient.name,
+            "age": patient.age,
+            "gender": patient.gender,
+            "phone_number": patient.phone_number,
+            "address": patient.address,
+            "contact_with_suspected_carrier": patient.contact_with_suspected_carrier,
+            "contact_with_confirmed_carrier": patient.contact_with_confirmed_carrier,
+            "estimated_contact_date": patient.estimated_contact_date,
+            "medical_history": [],
+            "tele_consultation_history": [],
+            "is_active": True,
+            "state": patient.state,
+            "district": patient.district,
+            "local_body": patient.local_body,
+            "past_travel": patient.past_travel,
+            "present_health": patient.present_health,
+            "has_SARI": patient.has_SARI,
+            "facility": patient.facility,
+            "countries_travelled": patient.countries_travelled,
+            "last_consultation": None,
+            "state_object": None,
+            "local_body_object": None,
+            "district_object": None,
+            "facility_object": None,
+        }
 
     def test_login_required(self):
         """Test permission error is raised for unauthorised access"""
         # logout the user logged in during setUp function
         self.client.logout()
-        response = self.client.post("/api/v1/patient/", {},)
+        response = self.client.post(self.get_url(), {},)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_patient_creation(self):
@@ -65,38 +126,28 @@ class TestPatient(APITestCase):
         """
         patient_data = self.patient_data
         user = self.user
-        response = self.client.post("/api/v1/patient/", patient_data)
+        response = self.client.post(self.get_url(), patient_data, format="json")
 
+        breakpoint()
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         response.data.pop("id")
-        """
-        is_active: True failed on my machine due to some weird reason
-        for now, I(abhyudai) have set it to False
-        """
-        self.assertDictEqual(
-            response.data,
-            {
-                **patient_data,
-                "medical_history_details": [{"disease": "NO", "details": "Quite bad"}],
-                "is_active": False,
-                "last_consultation": None,
-            },
-        )
+        self.assertDictEqual(response.data, self.get_detail_representation(patient_data))
 
         patient = PatientRegistration.objects.get(
             name=patient_data["name"],
             age=patient_data["age"],
             gender=patient_data["gender"],
             phone_number=patient_data["phone_number"],
-            contact_with_carrier=patient_data["contact_with_carrier"],
+            contact_with_suspected_carrier=patient_data["contact_with_suspected_carrier"],
+            contact_with_confirmed_carrier=patient_data["contact_with_confirmed_carrier"],
             created_by=user,
             is_active=True,
         )
-        assert Disease.objects.get(patient=patient, **patient_data["medical_history"][0])
+        self.assertIsNotNone(Disease.objects.get(patient=patient, **patient_data["medical_history"][0]))
 
     def test_users_cant_retrieve_others_patients(self):
         """Test users can't retrieve patients not created by them"""
-        response = self.client.get(f"/api/v1/patient/{self.patient.id}/")
+        response = self.client.get(self.get_url(self.patient.id))
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_patient_retrieval(self):
@@ -108,23 +159,9 @@ class TestPatient(APITestCase):
         patient = self.patient
         patient.created_by = self.user
         patient.save()
-        response = self.client.get(f"/api/v1/patient/{patient.id}/")
+        response = self.client.get(self.get_url(self.patient.id))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertDictEqual(
-            response.data,
-            {
-                "id": patient.id,
-                "name": patient.name,
-                "age": patient.age,
-                "gender": patient.gender,
-                "phone_number": patient.phone_number,
-                "contact_with_carrier": patient.contact_with_carrier,
-                "medical_history": [{"disease": "NO", "details": "Quite bad"}],
-                "tele_consultation_history": [],
-                "is_active": True,
-                "last_consultation": None,
-            },
-        )
+        self.assertDictEqual(response.data, self.get_detail_representation(patient))
 
     def test_patient_update(self):
         """
@@ -138,54 +175,38 @@ class TestPatient(APITestCase):
         patient.save()
 
         new_phone_number = "9999997775"
-        response = self.client.put(
-            f"/api/v1/patient/{patient.id}/",
-            {
-                "name": patient.name,
-                "age": patient.age,
-                "gender": patient.gender,
-                "phone_number": new_phone_number,
-                "contact_with_carrier": patient.contact_with_carrier,
-                "medical_history": [{"disease": 4, "details": "Mild"}],
-            },
-        )
+        response = self.client.put(self.get_url(patient.id), self.get_detail_representation(patient), format="json",)
+        breakpoint()
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        """
-        is_active: True failed on my machine due to some weird reason
-        for now, I have set it to False
-        """
-        self.assertDictEqual(
-            response.data,
-            {
-                "id": patient.id,
-                "name": patient.name,
-                "age": patient.age,
-                "gender": patient.gender,
-                "phone_number": new_phone_number,
-                "contact_with_carrier": patient.contact_with_carrier,
-                "medical_history": [
-                    {"disease": "NO", "details": "Quite bad"},
-                    {"disease": "HyperTension", "details": "Mild"},
-                ],
-                "is_active": False,
-                "last_consultation": None,
-                "tele_consultation_history": [],
-            },
-        )
+        self.assertDictEqual(response.data, self.get_detail_representation(self.patient_data))
         patient.refresh_from_db()
         self.assertEqual(patient.phone_number, new_phone_number)
 
     def test_user_can_delete_patient(self):
         """
         Test users can delete patient
-            - test status code
-            - test data from database
+            - test permission error
         """
         patient = self.patient
         patient.created_by = self.user
         patient.save()
+        response = self.client.delete(self.get_url(patient.id))
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
-        response = self.client.delete(f"/api/v1/patient/{patient.id}/",)
+    def test_superuser_can_delete_patient(self):
+        """
+        Test superuser can delete patient
+            - test status code
+            - test data from database
+        """
+        patient = self.patient
+        user = self.user
+        user.is_superuser = True
+        user.save()
+        patient.created_by = user
+
+        patient.save()
+        response = self.client.delete(self.get_url(patient.id))
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         with self.assertRaises(PatientRegistration.DoesNotExist):
             PatientRegistration.objects.get(id=patient.id)
@@ -196,7 +217,7 @@ class TestPatient(APITestCase):
 
         patient.created_by = self.user
         patient.save()
-        response = self.client.get(f"/api/v1/patient/")
+        response = self.client.get(self.get_url())
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_patient_list_retrieval(self):
@@ -205,27 +226,10 @@ class TestPatient(APITestCase):
 
         patient.created_by = self.user
         patient.save()
-        response = self.client.get(f"/api/v1/patient/")
+        response = self.client.get(self.get_url())
         self.assertDictEqual(
             response.data,
-            {
-                "count": 1,
-                "next": None,
-                "previous": None,
-                "results": [
-                    {
-                        "id": patient.id,
-                        "name": patient.name,
-                        "age": patient.age,
-                        "gender": patient.gender,
-                        "phone_number": patient.phone_number,
-                        "medical_history": [{"disease": "NO", "details": "Quite bad"}],
-                        "tele_consultation_history": [],
-                        "is_active": True,
-                        "last_consultation": None,
-                    },
-                ],
-            },
+            {"count": 1, "next": None, "previous": None, "results": [self.get_list_representation(patient)],},
         )
 
     def test_superuser_access(self):
@@ -238,20 +242,6 @@ class TestPatient(APITestCase):
         user = self.user
         user.is_superuser = True
         user.save()
-        response = self.client.get(f"/api/v1/patient/{patient.id}/")
+        response = self.client.get(self.get_url(entry_id=patient.id))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertDictEqual(
-            response.data,
-            {
-                "id": patient.id,
-                "name": patient.name,
-                "age": patient.age,
-                "gender": patient.gender,
-                "phone_number": patient.phone_number,
-                "contact_with_carrier": patient.contact_with_carrier,
-                "medical_history": [{"disease": "NO", "details": "Quite bad"}],
-                "is_active": True,
-                "last_consultation": None,
-                "tele_consultation_history": [],
-            },
-        )
+        self.assertDictEqual(response.data, self.get_detail_representation(patient))
