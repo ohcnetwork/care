0d5d886c904e45a88a0d598f1f4e791dbb8efc2e
Vignesh Hari <vichuhari100@gmail.com>
2020-04-05 23:55:22 +0530

    Added Experimental Feature to more models

--- care/facility/admin.py
+++ care/facility/admin.py
@@ -83,37 +83,45 @@ class FacilityAdmin(DjangoQLSearchMixin, admin.ModelAdmin):
     djangoql_completion_enabled_by_default = True
 
 
-class FacilityStaffAdmin(admin.ModelAdmin):
+class FacilityStaffAdmin(DjangoQLSearchMixin, admin.ModelAdmin):
     autocomplete_fields = ["facility", "staff"]
+    djangoql_completion_enabled_by_default = True
 
 
-class FacilityCapacityAdmin(admin.ModelAdmin):
+class FacilityCapacityAdmin(DjangoQLSearchMixin, admin.ModelAdmin):
     autocomplete_fields = ["facility"]
+    djangoql_completion_enabled_by_default = True
 
 
-class FacilityVolunteerAdmin(admin.ModelAdmin):
+class FacilityVolunteerAdmin(DjangoQLSearchMixin, admin.ModelAdmin):
     autocomplete_fields = ["facility", "volunteer"]
+    djangoql_completion_enabled_by_default = True
 
 
-class InventoryAdmin(admin.ModelAdmin):
+class InventoryAdmin(DjangoQLSearchMixin, admin.ModelAdmin):
     autocomplete_fields = ["facility", "item"]
+    djangoql_completion_enabled_by_default = True
 
 
-class InventoryItemAdmin(admin.ModelAdmin):
+class InventoryItemAdmin(DjangoQLSearchMixin, admin.ModelAdmin):
     search_fields = ["name", "description"]
+    djangoql_completion_enabled_by_default = True
 
 
-class RoomAdmin(admin.ModelAdmin):
+class RoomAdmin(DjangoQLSearchMixin, admin.ModelAdmin):
     autocomplete_fields = ["building"]
     search_fields = ["building", "num"]
+    djangoql_completion_enabled_by_default = True
 
 
-class StaffRoomAllocationAdmin(admin.ModelAdmin):
+class StaffRoomAllocationAdmin(DjangoQLSearchMixin, admin.ModelAdmin):
     autocomplete_fields = ["staff", "room"]
+    djangoql_completion_enabled_by_default = True
 
 
-class AmbulanceDriverInline(admin.TabularInline):
+class AmbulanceDriverInline(DjangoQLSearchMixin, admin.TabularInline):
     model = AmbulanceDriver
+    djangoql_completion_enabled_by_default = True
 
 
 class AmbulanceAdmin(admin.ModelAdmin):
@@ -121,14 +129,21 @@ class AmbulanceAdmin(admin.ModelAdmin):
     inlines = [
         AmbulanceDriverInline,
     ]
+    djangoql_completion_enabled_by_default = True
 
 
-class AmbulanceDriverAdmin(admin.ModelAdmin):
+class AmbulanceDriverAdmin(DjangoQLSearchMixin, admin.ModelAdmin):
     autocomplete_fields = ["ambulance"]
+    djangoql_completion_enabled_by_default = True
 
 
-class PatientAdmin(admin.ModelAdmin):
+class PatientAdmin(DjangoQLSearchMixin, admin.ModelAdmin):
     list_display = ("id", "name", "age", "gender")
+    djangoql_completion_enabled_by_default = True
+
+
+class PatientSampleAdmin(DjangoQLSearchMixin, admin.ModelAdmin):
+    djangoql_completion_enabled_by_default = True
 
 
 admin.site.register(Facility, FacilityAdmin)
@@ -145,5 +160,5 @@ admin.site.register(Ambulance, AmbulanceAdmin)
 admin.site.register(AmbulanceDriver, AmbulanceDriverAdmin)
 admin.site.register(PatientRegistration, PatientAdmin)
 admin.site.register(PatientTeleConsultation)
-admin.site.register(PatientSample)
+admin.site.register(PatientSample, PatientSampleAdmin)
 admin.site.register(Disease)
