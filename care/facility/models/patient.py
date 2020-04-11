f736d3bf97136fe9bce70341507dae0a33b406b7
Vignesh Hari <vichuhari100@gmail.com>
2020-04-11 11:36:53 +0530

    Made Age Nullable in Models

--- care/facility/models/patient.py
+++ care/facility/models/patient.py
@@ -29,7 +29,7 @@ class PatientRegistration(PatientBaseModel):
     facility = models.ForeignKey("Facility", on_delete=models.SET_NULL, null=True)
 
     name = EncryptedCharField(max_length=200)
-    age = models.PositiveIntegerField()
+    age = models.PositiveIntegerField(null=True, blank=True)
     gender = models.IntegerField(choices=GENDER_CHOICES, blank=False)
     phone_number = EncryptedCharField(max_length=14, validators=[phone_number_regex])
     address = EncryptedTextField(default="")
