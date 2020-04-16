60245f9b7a84a6bcc3643c112ee88b987cbce7ce
Pnija <nijap@techversantinfotech.com>
2020-04-16 16:08:15 +0530

    Consultation should have an option to mark bed number

--- care/facility/api/viewsets/patient_consultation.py
+++ care/facility/api/viewsets/patient_consultation.py
@@ -40,6 +40,14 @@ class PatientConsultationViewSet(ModelViewSet):
         """
         return super().list(request, *args, **kwargs)
 
+    def perform_create(self, serializer):
+        try:
+            if not self.request.data["admitted"]:
+                self.request.data["bed_number"] = ""
+        except KeyError:
+            self.request.data["bed_number"] = ""
+        serializer.save()
+
 
 class DailyRoundsViewSet(ModelViewSet):
     serializer_class = DailyRoundSerializer
