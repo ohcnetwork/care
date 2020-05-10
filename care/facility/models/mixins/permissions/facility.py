f19db19dd09b0195ddde4bbeb80837fa5362cf67
Vignesh Hari <vichuhari100@gmail.com>
2020-05-10 17:26:06 +0530

    Solved Permisson Issue in Facilities

--- care/facility/models/mixins/permissions/facility.py
+++ care/facility/models/mixins/permissions/facility.py
@@ -7,15 +7,25 @@ class FacilityPermissionMixin(BasePermissionMixin):
         return request.user.is_superuser
 
     def has_object_read_permission(self, request):
-        return super().has_object_read_permission(request) or request.user in self.users.all()
+        return (
+            super().has_object_read_permission(request) or request.user.is_superuser or request.user in self.users.all()
+        )
 
     def has_object_write_permission(self, request):
-        return super().has_write_permission(request) or request.user in self.users.all()
+        return super().has_write_permission(request) or request.user.is_superuser or request.user in self.users.all()
 
 
 class FacilityRelatedPermissionMixin(BasePermissionMixin):
     def has_object_read_permission(self, request):
-        return super().has_object_read_permission(request) or request.user in self.facility.users.all()
+        return (
+            super().has_object_read_permission(request)
+            or request.user.is_superuser
+            or request.user in self.facility.users.all()
+        )
 
     def has_object_write_permission(self, request):
-        return super().has_write_permission(request) or request.user in self.facility.users.all()
+        return (
+            super().has_write_permission(request)
+            or request.user.is_superuser
+            or request.user in self.facility.users.all()
+        )
