df8ec121c61b81a72636aab28ce5372d02c8f81c
mintu <mintu.sah@johstechnonoloygroup.com>
2020-05-22 11:01:48 +0530

    Added swagger code

--- care/urls.py
+++ care/urls.py
@@ -14,9 +14,29 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Care API",
+        default_version="v1",
+        description="API Documentation for Care",
+        contact=openapi.Contact(email="-"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/accounts/', include('care.apps.accounts.urls')),
+    # API Docs
+    re_path(r"^api/swagger(?P<format>\.json|\.yaml)$", schema_view.without_ui(cache_timeout=0), name="schema-json", ),
+    re_path(r"^api/swagger/$", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui", ),
+    re_path(r"^api/redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
