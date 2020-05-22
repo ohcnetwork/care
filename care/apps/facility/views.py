467563b8c6b6d53665a421a2ad2d79d66933e6a0
RunjeethNikam <runjeeth.nikam@joshtechnologygroup.com>
2020-05-22 20:47:42 +0530

    import changed

--- care/apps/facility/views.py
+++ care/apps/facility/views.py
@@ -1,6 +1,4 @@
-from rest_framework import viewsets
-from rest_framework import mixins
-from rest_framework import permissions
+from rest_framework import viewsets, mixins, permissions
 
 from apps.facility import (
     models as facility_models,
