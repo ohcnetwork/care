4b5c1b4cb49bfb698598a141ec37f8dbac330505
Ayush Sidana <ayush.sidana@joshtechnolgygroup.com>
2020-05-25 13:53:08 +0530

    inventory add update list api

---
+++ care/apps/facility/filters.py
@@ -0,0 +1,12 @@
+from django_filters import rest_framework as filters
+
+from apps.facility import models as facility_models
+
+
+class InventoryFilter(filters.FilterSet):
+    inventory_type = filters.CharFilter(field_name="item__name")
+    facility_name = filters.CharFilter(field_name="facility__name", lookup_expr="istartswith")
+
+    class Meta:
+        model = facility_models.Inventory
+        fields = ("inventory_type", "facility_name", "required_quantity", "current_quantity")
