4202e21864dc2bf3f3b67163f0edca3f492becd3
shashank <shashank.verma@joshtechnologygroup.com>
2020-05-29 18:23:10 +0530

    facilty filters integration completed

--- care/apps/facility/filters.py
+++ care/apps/facility/filters.py
@@ -5,24 +5,6 @@ from apps.facility import models as facility_models
 
 
 class FacilityFilter(filters.FilterSet):
-    code = filters.CharFilter(field_name="facility_code", lookup_expr="istartswith")
-    name = filters.CharFilter(field_name="name", lookup_expr="istartswith")
-    address = filters.CharFilter(field_name="address", lookup_expr="istartswith")
-    facility_type_name = filters.CharFilter(
-        field_name="facility_type__name", lookup_expr="istartswith"
-    )
-    owned_by_name = filters.CharFilter(
-        field_name="owned_by__name", lookup_expr="istartswith"
-    )
-    total_patient = filters.RangeFilter(field_name="total_patient", lookup_expr="range")
-    positive_patient = filters.RangeFilter(
-        field_name="positive_patient", lookup_expr="range"
-    )
-    negative_patient = filters.RangeFilter(
-        field_name="negative_patient", lookup_expr="range"
-    )
-    created_at = filters.DateTimeFromToRangeFilter(field_name="created_at")
-    updated_at = filters.DateTimeFromToRangeFilter(field_name="updated_at")
     district = filters.filters.ModelMultipleChoiceFilter(
         field_name='district',
         queryset=accounts_models.District.objects.all()
@@ -30,20 +12,12 @@ class FacilityFilter(filters.FilterSet):
 
     class Meta:
         model = facility_models.Facility
-        fields = (
-            "name",
-            "code",
-            "address",
-            "district",
-            "facility_type_name",
-            "owned_by_name",
-            "district",
-            "total_patient",
-            "positive_patient",
-            "negative_patient",
-            "created_at",
-            "updated_at",
-        )
+        fields = {
+            "positive_patient": ['exact', 'range', 'gt', 'lt'],
+            "negative_patient": ['exact', 'range', 'gt', 'lt'],
+            "total_patient": ['exact', 'range', 'gt', 'lt'],
+            "district": ['exact']
+        }
 
 
 class InventoryFilter(filters.FilterSet):
