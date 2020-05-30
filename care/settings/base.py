12166c74cdc7d557b985bf94521befa25661f78a
SHIVAM JINDAL <shivam.jindal@joshtechnologygroup.com>
2020-05-31 00:20:09 +0530

    Remove default permission

--- care/settings/base.py
+++ care/settings/base.py
@@ -310,9 +310,6 @@ class Settings(LoggerSettingsMixin, Configuration):
         ),
         "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
         "PAGE_SIZE": 100,
-        "DEFAULT_PERMISSION_CLASSES": (
-            "rest_framework.permissions.IsAuthenticated",
-        )
     }
 
     # Your stuff...
