280b18f12da597456b89ae371ac6487541b0cc1f
RunjeethNikam <runjeeth.nikam@joshtechnologygroup.com>
2020-05-30 13:44:42 +0530

    filters updated

--- care/apps/patients/views.py
+++ care/apps/patients/views.py
@@ -45,6 +45,7 @@ class PatientViewSet(
     )
 
     def get_queryset(self):
+        print(self.request.query_params)
         queryset = patient_models.Patient.objects.all()
         if (
             self.request.user.user_type
