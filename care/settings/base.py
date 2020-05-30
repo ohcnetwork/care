79a94b8cb7758a3ce8d29688ddf4afc043f57b0c
SHIVAM JINDAL <shivam.jindal@joshtechnologygroup.com>
2020-05-30 23:46:36 +0530

    minor fix

--- care/settings/base.py
+++ care/settings/base.py
@@ -311,7 +311,7 @@ class Settings(LoggerSettingsMixin, Configuration):
         "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
         "PAGE_SIZE": 100,
         "DEFAULT_PERMISSION_CLASSES": (
-            "rest_framework.permissions.IsAuthenticated"
+            "rest_framework.permissions.IsAuthenticated",
         )
     }
 
