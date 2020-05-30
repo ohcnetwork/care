71b7da2cb21890b3ac397383ccd97dc50da12d6e
SHIVAM JINDAL <shivam.jindal@joshtechnologygroup.com>
2020-05-30 22:50:16 +0530

    update default permission class

--- care/settings/base.py
+++ care/settings/base.py
@@ -310,6 +310,9 @@ class Settings(LoggerSettingsMixin, Configuration):
         ),
         "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
         "PAGE_SIZE": 100,
+        "DEFAULT_PERMISSION_CLASSES": (
+            "rest_framework.permissions.IsAuthenticated"
+        )
     }
 
     # Your stuff...
