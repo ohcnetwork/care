c8ca9264cc84d52b46e70b0aaed13f941c76b28a
Vignesh Hari <vichuhari100@gmail.com>
2020-06-08 01:01:07 +0530

    suburl for password reset

--- config/reset_password_signal.py
+++ config/reset_password_signal.py
@@ -25,7 +25,7 @@ def password_reset_token_created(sender, instance, reset_password_token, *args, 
         "current_user": reset_password_token.user,
         "username": reset_password_token.user.username,
         "email": reset_password_token.user.email,
-        "reset_password_url": "{}?token={}".format(settings.CURRENT_DOMAIN, reset_password_token.key),
+        "reset_password_url": "{}/password_reset/?token={}".format(settings.CURRENT_DOMAIN, reset_password_token.key),
     }
 
     # render email text
