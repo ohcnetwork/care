5138d4ceb75e975b01c51bc27131e9b2093a1f0e
Vignesh Hari <vichuhari100@gmail.com>
2020-06-11 19:49:33 +0530

    Fix Api Docs

--- care/users/api/serializers/user.py
+++ care/users/api/serializers/user.py
@@ -186,4 +186,4 @@ class UserListSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ("id", "first_name", "last_name", "local_body_object", "district_object", "state_object")
+        fields = ("id", "first_name", "last_name", "local_body_object", "district_object", "state_object" , "user_type")
