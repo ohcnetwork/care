eb38a6e9060e93be752bf7a88f5a54546ffeeb71
Vignesh Hari <vichuhari100@gmail.com>
2020-06-12 23:25:30 +0530

    Fixed SVE-CARE-1

--- care/facility/models/mixins/permissions/base.py
+++ care/facility/models/mixins/permissions/base.py
@@ -9,25 +9,10 @@ class BasePermissionMixin:
 
     @staticmethod
     def has_write_permission(request):
-        from care.facility.models.facility import Facility
-
-        facility = False
-        try:
-            facility = Facility.objects.get(external_id=request.parser_context["kwargs"]["facility_external_id"])
-        except Facility.DoesNotExist:
-            return False
-        return (request.user.is_superuser or request.user.verified) and (
-            (hasattr(facility, "created_by") and request.user == facility.created_by)
-            or (
-                hasattr(facility, "district")
-                and request.user.user_type >= User.TYPE_VALUE_MAP["DistrictAdmin"]
-                and request.user.district == facility.district
-            )
-            or (
-                hasattr(facility, "state")
-                and request.user.user_type >= User.TYPE_VALUE_MAP["StateAdmin"]
-                and request.user.state == facility.state
-            )
+        return (
+            request.user.is_superuser
+            or request.user.verified
+            or request.user.user_type >= User.TYPE_VALUE_MAP["DistrictAdmin"]
         )
 
     def has_object_read_permission(self, request):
--- care/facility/models/mixins/permissions/facility.py
+++ care/facility/models/mixins/permissions/facility.py
@@ -1,4 +1,5 @@
 from care.facility.models.mixins.permissions.base import BasePermissionMixin
+from care.users.models import User
 
 
 class FacilityPermissionMixin(BasePermissionMixin):
@@ -16,6 +17,29 @@ class FacilityPermissionMixin(BasePermissionMixin):
 
 
 class FacilityRelatedPermissionMixin(BasePermissionMixin):
+    @staticmethod
+    def has_write_permission(request):
+        from care.facility.models.facility import Facility
+
+        facility = False
+        try:
+            facility = Facility.objects.get(external_id=request.parser_context["kwargs"]["facility_external_id"])
+        except Facility.DoesNotExist:
+            return False
+        return (request.user.is_superuser or request.user.verified) and (
+            (hasattr(facility, "created_by") and request.user == facility.created_by)
+            or (
+                hasattr(facility, "district")
+                and request.user.user_type >= User.TYPE_VALUE_MAP["DistrictAdmin"]
+                and request.user.district == facility.district
+            )
+            or (
+                hasattr(facility, "state")
+                and request.user.user_type >= User.TYPE_VALUE_MAP["StateAdmin"]
+                and request.user.state == facility.state
+            )
+        )
+
     def has_object_read_permission(self, request):
         return (
             super().has_object_read_permission(request)
