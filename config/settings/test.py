367ae33eb9b60c9e63fe9f6829345e3efbd099bb
Vignesh Hari <vichuhari100@gmail.com>
2020-07-06 21:50:41 +0530

    Fixing Postgis connection issue

--- config/settings/test.py
+++ config/settings/test.py
@@ -46,6 +46,6 @@ INSTALLED_APPS += [  # noqa F405
     "test_without_migrations",
 ]
 
-DATABASES = {"default": env.db("DATABASE_URL", default="postgis:///care_test")}
+DATABASES = {"default": env.db("DATABASE_URL", default="postgis:///care-test")}
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
 DATABASES["default"]["ENGINE"] = "django.contrib.gis.db.backends.postgis"
