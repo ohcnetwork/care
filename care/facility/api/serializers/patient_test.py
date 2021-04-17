f298ccfde37460c3f652f8a12bcd8b667d2683cf
Vignesh Hari <vichuhari100@gmail.com>
2021-04-17 22:46:23 +0530

    Fixed Serializers

--- care/facility/api/serializers/patient_test.py
+++ care/facility/api/serializers/patient_test.py
@@ -1,27 +1,18 @@
 from rest_framework import serializers
-from care.facility.models.patient_test import TestValue, TestGroup, PatientTest
+from care.facility.models.patient_test import TestValue, PatientTestGroup, PatientTest
 from care.facility.api.serializers import TIMESTAMP_FIELDS
 
 
-class TestGroupSerializer(serializers.ModelSerializer):
+class PatientTestGroupSerializer(serializers.ModelSerializer):
     class Meta:
-        model = TestGroup
-        read_only_fields = TIMESTAMP_FIELDS
+        model = PatientTestGroup
         exclude = TIMESTAMP_FIELDS + ("id",)
 
 
 class PatientTestSerializer(serializers.ModelSerializer):
     class Meta:
         model = PatientTest
-        read_only_fields = TIMESTAMP_FIELDS
         exclude = TIMESTAMP_FIELDS + ("id",)
-        required_fields = [
-            "name",
-            "group",
-            "unit",
-            "type"
-        ]
-        extra_kwargs = {i: {'required': True} for i in required_fields}
 
 
 class TestValueSerializer(serializers.ModelSerializer):
