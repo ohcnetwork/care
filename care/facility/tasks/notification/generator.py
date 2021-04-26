04e6481137a1c5de85d378cca42da83214ab3fe2
Vignesh Hari <vichuhari100@gmail.com>
2021-04-26 10:48:44 +0530

    Remove Redundant Notification Generation

--- care/facility/tasks/notification/generator.py
+++ care/facility/tasks/notification/generator.py
@@ -12,17 +12,23 @@ from django.conf import settings
 def generate_notifications_for_facility(facility_id, data, defer_notifications):
     extra_users = data.get("extra_users", [])
     del data["extra_users"]
+    caused_user_id = data.get("caused_by_id", None)
+    caused_user = None
+    if caused_user_id:
+        caused_user = User.objects.get(id=caused_user_id)
     facility_users = FacilityUser.objects.filter(facility_id=facility_id)
     # notifications = []
     for facility_user in facility_users:
-        generate_message_for_user(facility_user.user, data.copy())
-        if not defer_notifications:
-            send_webpush_user(facility_user.user, data["message"])
+        if facility_user.user.id != caused_user.id:
+            generate_message_for_user(facility_user.user, data.copy())
+            if not defer_notifications:
+                send_webpush_user(facility_user.user, data["message"])
     for user_id in extra_users:
         user_obj = User.objects.get(id=user_id)
-        generate_message_for_user(user_obj, data.copy())
-        if not defer_notifications:
-            send_webpush_user(user_obj, data["message"])
+        if user_obj.id != caused_user.id:
+            generate_message_for_user(user_obj, data.copy())
+            if not defer_notifications:
+                send_webpush_user(user_obj, data["message"])
     # Notification.objects.bulk_create(notifications)
     # for facility_user in facility_users:
     #     if not defer_notifications:
