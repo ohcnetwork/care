301d0ced24f00d7ffd683500528ed804b63b8da1
Vignesh Hari <vichuhari100@gmail.com>
2021-04-30 21:45:27 +0530

    Cleanup Life Data

--- care/life/api/serializers/lifedata.py
+++ care/life/api/serializers/lifedata.py
@@ -5,9 +5,11 @@ from care.life.models import LifeData
 
 
 class LifeDataSerializer(serializers.ModelSerializer):
-    state_name = serializers.CharField(source="state.name")
-    district_name = serializers.CharField(source="district.name")
-    data_source = serializers.CharField(source="created_job.file_url")
+    state = serializers.CharField(source="state.name")
+    district = serializers.CharField(source="district.name")
+    state_id = serializers.CharField(source="state.id")
+    district_id = serializers.CharField(source="district.id")
+    # data_source = serializers.CharField(source="created_job.file_url")
     data_name = serializers.CharField(source="created_job.name")
 
     class Meta:
