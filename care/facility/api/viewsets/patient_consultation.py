3b3b8e2e1cb5261b5db59d7636a831daa5b83324
Vignesh Hari <vichuhari100@gmail.com>
2021-05-13 15:02:12 +0530

    Cleaning Files

--- care/facility/api/viewsets/patient_consultation.py
+++ care/facility/api/viewsets/patient_consultation.py
@@ -1,14 +1,12 @@
 from django.db.models.query_utils import Q
 from django_filters import rest_framework as filters
 from dry_rest_permissions.generics import DRYPermissions
-from rest_framework import mixins, status
+from rest_framework import mixins
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from care.facility.api.serializers.patient_consultation import DailyRoundSerializer, PatientConsultationSerializer
 from care.facility.models.patient_consultation import DailyRound, PatientConsultation
-from care.facility.models.patient import PatientRegistration
 from care.users.models import User
 
 
