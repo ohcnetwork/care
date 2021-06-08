444e7ff2356a8a285ff66737ca266242d0b778c1
Vignesh Hari <vichuhari100@gmail.com>
2021-06-08 11:22:24 +0530

    Whatsapp base API

--- care/utils/whatsapp/send_mesage.py
+++ care/utils/whatsapp/send_mesage.py
@@ -1,17 +1,41 @@
 import requests
 from django.conf import settings
+
 from care.users.models import phone_number_regex
 
 
+def _opt_in(phone_number):
+    url_data = {
+        "method": "OPT_IN",
+        "auth_scheme": "plain",
+        "v": "1.1",
+        "phone_number": phone_number,
+        "password": settings.WHATSAPP_API_PASSWORD,
+        "userid": settings.WHATSAPP_API_USERNAME,
+        "channel": "whatsapp",
+    }
+    resp = requests.post(settings.WHATSAPP_API_ENDPOINT, params=url_data)
+    return resp
+
+
 def _send(phone_number, message):
-    data = {
+    _opt_in(phone_number)
+    url_data = {
         "method": "SendMessage",
         "auth_scheme": "plain",
         "v": "1.1",
         "send_to": phone_number,
         "msg": message,
+        "isHSM": "True",
+        "buttonUrlParam": "bDQ2NTkz",
+        "msg_type": "HSM",
+        "password": settings.WHATSAPP_API_PASSWORD,
+        "userid": settings.WHATSAPP_API_USERNAME,
+        "isTemplate": "true",
+        "header": "Specialist Consultation Requested",
+        "footer": "Click the following to link to view patient details.",
     }
-    resp = requests.get(settings.WHATSAPP_API_ENDPOINT, data=data, auth=requests.auth.HTTPBasicAuth("user", "pass"))
+    resp = requests.post(settings.WHATSAPP_API_ENDPOINT, params=url_data)
     return resp
 
 
