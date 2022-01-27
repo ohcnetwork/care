59382f5d6b7945455eecee47549ea77f5542c3cd
Vignesh Hari <vichuhari100@gmail.com>
2022-01-28 03:20:02 +0530

    Added Management command to sync daily rounds to consultations

---
+++ care/facility/management/commands/add_daily_round_consultation.py
@@ -0,0 +1,27 @@
+from django.core.management.base import BaseCommand
+
+from care.facility.models import PatientConsultation, DailyRound
+
+
+class Command(BaseCommand):
+    """
+    Management command to Populate daily round for consultations.
+    """
+
+    help = "Populate daily round for consultations"
+
+    def handle(self, *args, **options):
+        consultations = PatientConsultation.objects.filter(last_daily_round__isnull=True)
+        total_count = consultations.count()
+        print(f"{total_count} Consultations need to be updated")
+        i = 0
+        for consultation in consultations:
+            if i > 1000 and i % 1000 == 0:
+                print(f"{i} operations performed")
+            i += 1
+            PatientConsultation.objects.filter(id=consultation.id).update(
+                last_daily_round=DailyRound.objects.filter(consultation_id=consultation.id)
+                .order_by("-created_date")
+                .first()
+            )
+        print("Operation Completed")
