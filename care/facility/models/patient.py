c5d95516b5d0a3d230e2f5120a79c52c422c2441
siddnikh <mailtosiddnikh@gmail.com>
2022-05-21 13:03:20 +0530

    Modified maximum number of doses for a patient

--- care/facility/models/patient.py
+++ care/facility/models/patient.py
@@ -285,7 +285,7 @@ class PatientRegistration(PatientBaseModel, PatientPermissionMixin):
     # Vaccination Fields
     is_vaccinated = models.BooleanField(default=False, verbose_name="Is the Patient Vaccinated Against COVID-19")
     number_of_doses = models.PositiveIntegerField(
-        default=0, null=False, blank=False, validators=[MinValueValidator(0), MaxValueValidator(2)]
+        default=0, null=False, blank=False, validators=[MinValueValidator(0), MaxValueValidator(3)]
     )
     vaccine_name = models.CharField(choices=vaccineChoices, default=None, null=True, blank=False, max_length=15)
 
