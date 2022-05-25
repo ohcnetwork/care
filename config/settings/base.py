d61891918725e043a9c43b186e380e840263e7f6
Aakash Singh <46787056+sainAk@users.noreply.github.com>
2022-05-26 01:23:48 +0530

    serve staticfiles using whitenoise

--- config/settings/base.py
+++ config/settings/base.py
@@ -179,15 +179,10 @@ if USE_S3:
     AWS_DEFAULT_ACL = "public-read"
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
     AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
-    # s3 static settings
-    AWS_S3_REGION_NAME = "ap-south-1"
-    AWS_LOCATION = "static"
-    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/"
-    STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-else:
-    STATIC_URL = "/staticfiles/"
-    STATIC_ROOT = str(ROOT_DIR("staticfiles"))
-    STATICFILES_STORAGE = "django.core.files.storage.FileSystemStorage"
+
+STATIC_URL = "/staticfiles/"
+STATIC_ROOT = str(ROOT_DIR("staticfiles"))
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATICFILES_DIRS = [str(APPS_DIR.path("static"))]
 
