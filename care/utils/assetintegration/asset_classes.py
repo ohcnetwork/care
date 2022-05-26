852cfa8b2d5ab76774b2bc694929c6412e55d53c
Suryashankar Das <suryashankardas.2002@gmail.com>
2022-05-26 16:30:31 +0530

    add HL7MonitorAsset class (#776)

--- care/utils/assetintegration/asset_classes.py
+++ care/utils/assetintegration/asset_classes.py
@@ -1,6 +1,8 @@
 import enum
 from care.utils.assetintegration.onvif import OnvifAsset
+from care.utils.assetintegration.hl7monitor import HL7MonitorAsset
 
 
 class AssetClasses(enum.Enum):
     ONVIF = OnvifAsset
+    HL7MONITOR = HL7MonitorAsset
