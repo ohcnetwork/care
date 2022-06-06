fcbc9cc8fc89e75563a48069dbbcb3848fc6e53e
Aakash Singh <46787056+sainAk@users.noreply.github.com>
2022-06-06 19:46:42 +0530

    updated `/add_user` endpoint to return status code instead of user object (#718)
    * Update `/add_user` endpoint to return status code instead of user object
    Fixes #707
    
    * removed unessary unique username validation

--- care/users/api/viewsets/users.py
+++ care/users/api/viewsets/users.py
@@ -161,14 +161,8 @@ class UserViewSet(
             data={**request.data, "password": password}, context={"created_by": request.user},
         )
         serializer.is_valid(raise_exception=True)
-        username = request.data["username"]
-        if User.objects.filter(username=username).exists():
-            raise ValidationError({"username": "User with Given Username Already Exists"})
-        user = serializer.create(serializer.validated_data)
-
-        response_data = UserCreateSerializer(user).data
-        # response_data["password"] = password
-        return Response(data=response_data, status=status.HTTP_201_CREATED)
+        serializer.save()
+        return Response(status=status.HTTP_201_CREATED)
 
     def has_facility_permission(self, user, facility):
         return (
