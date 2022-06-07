9c31d8f9023587b3189593bfcdfaa09ba5befe0c
Aakash Singh <46787056+sainAk@users.noreply.github.com>
2022-06-07 19:34:58 +0530

    fix merge issues

--- config/settings/test.py
+++ config/settings/test.py
@@ -44,8 +44,6 @@ EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
 
 MIDDLEWARE.remove("care.audit_log.middleware.AuditLogMiddleware")
 
-MIDDLEWARE.remove("care.audit_log.middleware.AuditLogMiddleware")
-
 DATABASES = {"default": env.db("DATABASE_URL", default="postgis:///care-test")}
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
 DATABASES["default"]["ENGINE"] = "django.contrib.gis.db.backends.postgis"
