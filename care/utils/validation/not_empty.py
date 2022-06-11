ce4c850f95f0933943e87231b9057e24a230a17a
cp-Coder <gupta.abhiuday.109@gmail.com>
2022-06-11 14:07:39 +0530

    Added notEmptyValidator

---
+++ care/utils/validation/not_empty.py
@@ -0,0 +1,21 @@
+from django.utils.translation import gettext as _
+from rest_framework.utils.representation import smart_repr
+from rest_framework import serializers
+
+class notEmptyValidator:
+    """
+    Validator for checking whether the field has empty value or not
+    """
+    message = _('{field} cannot be empty')
+
+    def __init__(self, field, message=None):
+        self.field = field
+        self.message = message
+
+    def __call__(self, attrs):
+        if not attrs[self.field].strip():
+            message = self.message.format(field=self.field)
+            raise serializers.ValidationError({self.field:message})
+
+    def __repr__(self):
+        return '<%s(field=%s)>' % (self.__class__.__name__, smart_repr(self.field))
