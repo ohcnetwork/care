70e47c3d23f78dee760b587e58557798ee18a7a2
Suryashankar Das <suryashankardas.2002@gmail.com>
2022-06-23 19:26:30 +0530

    add GET_VITALS action for hl7monitor asset

--- care/utils/assetintegration/hl7monitor.py
+++ care/utils/assetintegration/hl7monitor.py
@@ -1,11 +1,26 @@
+import enum
+
 from care.utils.assetintegration.base import BaseAssetIntegration
 
 
 class HL7MonitorAsset(BaseAssetIntegration):
     _name = "hl7monitor"
 
+    class HL7MonitorActions(enum.Enum):
+        GET_VITALS = "get_vitals"
+
     def __init__(self, meta):
         try:
             super().__init__(meta)
         except KeyError:
             print("Error: Invalid HL7Monitor Asset; Missing required fields")
+
+    def handle_action(self, action):
+        if action.type == self.HL7MonitorActions.GET_VITALS.value:
+            request_params = {
+                "device_id": self.host
+            }
+            return self.api_get(self.get_url("vitals"), data=request_params)
+        else:
+            raise Exception("Invalid action")
+
