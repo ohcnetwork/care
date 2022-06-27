6015e80787bcf6378708087ae733c679f6e6a423
Aakash Singh <46787056+sainAk@users.noreply.github.com>
2022-06-28 01:46:40 +0530

    Finalize MiddlewareAuthentication for required endpoints (#811)
    * refactor MiddlewareAuthentication
    
    * add permission component for asset users
    
    * add asset dry permissions to daily round model
    
    * reformat
    
    * reformat
    
    * fix permissions in AssetUserAccessMixin
    
    * use direct query to lookup asset bed relation instead of reverse lookup
    
    * added endpoint to get patient from asset
    
    * fix ordering in patient_from_asset action
    
    * fix exception handling
    
    * pre-commit fixes

--- care/facility/models/mixins/permissions/asset.py
+++ care/facility/models/mixins/permissions/asset.py
@@ -1,5 +1,28 @@
+from dry_rest_permissions.generics import DRYPermissions
+
 from care.facility.models.mixins.permissions.base import BasePermissionMixin
 from care.users.models import User
+
+
+class IsAssetUser:
+    def has_permission(self, request, view):
+        return bool(
+            request.user and request.user.is_authenticated and request.user.asset
+        )
+
+    def has_object_permission(self, request, view, obj):
+        return self.has_permission(request, view)
+
+
+class DRYAssetPermissions(DRYPermissions):
+    """
+    Adds additional prefix for asset users to the permission names.
+    """
+
+    global_permissions = False
+
+    def _get_action(self, action):
+        return f"asset_{super()._get_action(action)}"
 
 
 class AssetsPermissionMixin(BasePermissionMixin):
