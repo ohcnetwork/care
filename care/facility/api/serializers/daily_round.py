09ce974bbfaa6f654edf6334c0f488121013d212
cp-Coder <gupta.abhiuday.109@gmail.com>
2022-06-27 10:39:45 +0530

    Fixed saving of daily round object

--- care/facility/api/serializers/daily_round.py
+++ care/facility/api/serializers/daily_round.py
@@ -158,9 +158,13 @@ class DailyRoundSerializer(serializers.ModelSerializer):
         daily_round_obj.created_by = self.context["request"].user
         daily_round_obj.last_edited_by = self.context["request"].user
         daily_round_obj.consultation.last_updated_by_telemedicine = validated_data["last_updated_by_telemedicine"]
+        daily_round_obj.consultation.save(
+            update_fields=[
+                "last_updated_by_telemedicine"
+            ]
+        )
         daily_round_obj.save(
             update_fields=[
-                "consultation__last_updated_by_telemedicine",
                 "created_by",
                 "last_edited_by",
             ]
