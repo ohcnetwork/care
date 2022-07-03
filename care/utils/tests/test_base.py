1d4f90b7decbb305147d1f333bd62b6c4b63cf15
Shivank Kacker <kacker.shivank@gmail.com>
2022-07-04 01:30:27 +0530

    Renamed existing medication to history of present illness (#890)
    renamed existing medication to history of present illness

--- care/utils/tests/test_base.py
+++ care/utils/tests/test_base.py
@@ -389,7 +389,7 @@ class TestBase(APITestCase):
             "symptoms_onset_date": make_aware(datetime.datetime(2020, 4, 7, 15, 30)),
             "category": CATEGORY_CHOICES[0][0],
             "examination_details": "examination_details",
-            "existing_medication": "existing_medication",
+            "history_of_present_illness": "history_of_present_illness",
             "prescribed_medication": "prescribed_medication",
             "suggestion": PatientConsultation.SUGGESTION_CHOICES[0][0],
             "referred_to": None,
