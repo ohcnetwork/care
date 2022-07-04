b29515406c602d80c8b538320893064536b38a5a
Suryashankar Das <suryashankardas.2002@gmail.com>
2022-07-04 15:28:24 +0530

    update asset integration actions

--- care/utils/assetintegration/hl7monitor.py
+++ care/utils/assetintegration/hl7monitor.py
@@ -10,53 +10,19 @@ class HL7MonitorAsset(BaseAssetIntegration):
 
     class HL7MonitorActions(enum.Enum):
         GET_VITALS = "get_vitals"
-        GET_CAMERA_STATUS = "get_status"
-        GET_PRESETS = "get_presets"
-        GOTO_PRESET = "goto_preset"
-        ABSOLUTE_MOVE = "absolute_move"
-        RELATIVE_MOVE = "relative_move"
 
     def __init__(self, meta):
         try:
             super().__init__(meta)
-            self.port = self.meta["port"]
-            self.username = self.meta["camera_access_key"].split(":")[0]
-            self.password = self.meta["camera_access_key"].split(":")[1]
-            self.access_key = self.meta["camera_access_key"].split(":")[2]
         except KeyError as e:
             raise ValidationError(
                 dict((key, f"{key} not found in asset metadata") for key in e.args))
 
     def handle_action(self, action):
         action_type = action["type"]
-        action_data = action.get("data", {})
-
-        request_body = {
-            "hostname": self.host,
-            "port": self.port,
-            "username": self.username,
-            "password": self.password,
-            "accessKey": self.access_key,
-            **action_data
-        }
 
         if action_type == self.HL7MonitorActions.GET_VITALS.value:
             request_params = {"device_id": self.host}
             return self.api_get(self.get_url("vitals"), request_params)
-
-        if action_type == self.HL7MonitorActions.GET_CAMERA_STATUS.value:
-            return self.api_get(self.get_url("status"), request_body)
-
-        if action_type == self.HL7MonitorActions.GET_PRESETS.value:
-            return self.api_get(self.get_url("presets"), request_body)
-
-        if action_type == self.HL7MonitorActions.GOTO_PRESET.value:
-            return self.api_post(self.get_url("gotoPreset"), request_body)
-
-        if action_type == self.HL7MonitorActions.ABSOLUTE_MOVE.value:
-            return self.api_post(self.get_url("absoluteMove"), request_body)
-
-        if action_type == self.HL7MonitorActions.RELATIVE_MOVE.value:
-            return self.api_post(self.get_url("relativeMove"), request_body)
 
         raise ValidationError({"action": "invalid action type"})
