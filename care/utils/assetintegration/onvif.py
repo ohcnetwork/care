0247b810ef1d14efd7e0842526140a882a1b2a3f
Suryashankar Das <suryashankardas.2002@gmail.com>
2022-07-07 23:29:25 +0530

    update asset integration actions (#909)
    * minor bug fix for onvif asset integration
    
    * update asset integration actions
    
    * allow using insecure connections for asset integration

--- care/utils/assetintegration/onvif.py
+++ care/utils/assetintegration/onvif.py
@@ -9,41 +9,49 @@ class OnvifAsset(BaseAssetIntegration):
     _name = "onvif"
 
     class OnvifActions(enum.Enum):
-        MOVE_ABSOLUTE = "move_absolute"
+        GET_CAMERA_STATUS = "get_status"
+        GET_PRESETS = "get_presets"
         GOTO_PRESET = "goto_preset"
+        ABSOLUTE_MOVE = "absolute_move"
+        RELATIVE_MOVE = "relative_move"
 
     def __init__(self, meta):
         try:
             super().__init__(meta)
-            self.name = self.meta["camera_type"]
-            self.port = self.meta["camera_port"] or 80
+            self.port = self.meta["port"]
             self.username = self.meta["camera_access_key"].split(":")[0]
-            self.password = self.meta["access_credentials"].split(":")[1]
+            self.password = self.meta["camera_access_key"].split(":")[1]
+            self.access_key = self.meta["camera_access_key"].split(":")[2]
         except KeyError as e:
             raise ValidationError(
                 dict((key, f"{key} not found in asset metadata") for key in e.args))
 
     def handle_action(self, action):
-        if action["type"] == self.OnvifActions.MOVE_ABSOLUTE.value:
-            # Make API Call for action
-            request_data = {
-                "x": action.data["x"],
-                "y": action.data["y"],
-                "z": action.data["z"],
-                "speed": action.data["speed"],
-                "meta": self.meta,
-            }
-            return self.api_post(self.get_url("absoluteMove"), data=request_data)
+        action_type = action["type"]
+        action_data = action.get("data", {})
 
-        elif action["type"] == self.OnvifActions.GOTO_PRESET.value:
-            # Make API Call for action
-            request_data = {
-                "preset": action.preset,
-                "meta": self.meta,
-            }
-            return self.api_post(self.get_url("gotoPreset"), data=request_data)
-        elif action["type"] == self.OnvifActions.GOTO_PRESET.value:
-            # Make API Call for action
-            return self.api_get(self.get_url("status"), data={})
-        else:
-            raise ValidationError({"action": "invalid action type"})
+        request_body = {
+            "hostname": self.host,
+            "port": self.port,
+            "username": self.username,
+            "password": self.password,
+            "accessKey": self.access_key,
+            **action_data
+        }
+
+        if action_type == self.OnvifActions.GET_CAMERA_STATUS.value:
+            return self.api_get(self.get_url("status"), request_body)
+
+        if action_type == self.OnvifActions.GET_PRESETS.value:
+            return self.api_get(self.get_url("presets"), request_body)
+
+        if action_type == self.OnvifActions.GOTO_PRESET.value:
+            return self.api_post(self.get_url("gotoPreset"), request_body)
+
+        if action_type == self.OnvifActions.ABSOLUTE_MOVE.value:
+            return self.api_post(self.get_url("absoluteMove"), request_body)
+
+        if action_type == self.OnvifActions.RELATIVE_MOVE.value:
+            return self.api_post(self.get_url("relativeMove"), request_body)
+
+        raise ValidationError({"action": "invalid action type"})
