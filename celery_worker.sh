51a8dc652ab5953f5f7a343ad9ec0b3cf10849ff
syam <hey@syam.dev>
2022-07-14 10:57:20 +0530

    changes for build commands

--- celery_worker.sh
+++ celery_worker.sh
@@ -1,2 +1,2 @@
-export NEW_RELIC_CONFIG_FILE=/etc/newrelic.ini
-newrelic-admin run-program celery --app=config.celery_app --loglevel=info worker
+#!/bin/bash
+/bin/bash -c -- celery --app=config.celery_app --loglevel=info worker
