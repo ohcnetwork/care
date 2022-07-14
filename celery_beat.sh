897ca3f5c88d17a0765132d1e085422d65857cdb
syam <hey@syam.dev>
2022-07-14 15:30:52 +0530

    changes for ECS

--- celery_beat.sh
+++ celery_beat.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-/bin/bash -c -- celery --app=config.celery_app --loglevel=info beat
+
+celery --app=config.celery_app --loglevel=info beat
--- celery_worker.sh
+++ celery_worker.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-/bin/bash -c -- celery --app=config.celery_app --loglevel=info worker
+
+celery --app=config.celery_app --loglevel=info worker
