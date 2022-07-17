0e119ba41e80bc9a601b6d24c69d051b125164c5
cp-Coder <gupta.abhiuday.109@gmail.com>
2022-07-17 19:41:03 +0530

    Updated the script to add units and their conversion rates

--- care/users/management/commands/seed_data.py
+++ care/users/management/commands/seed_data.py
@@ -1,38 +1,75 @@
-from care.facility.models import facility
 from django.core.management import BaseCommand
-from care.facility.models.inventory import FacilityInventoryUnit, FacilityInventoryItem
+from care.facility.models.inventory import (
+    FacilityInventoryItemTag,
+    FacilityInventoryUnit,
+    FacilityInventoryItem,
+    FacilityInventoryUnitConverter,
+)
 
 
 class Command(BaseCommand):
     """
+    Command to add data related to inventory and their conversion rates
     """
 
     help = "Seed Data for Inventory"
 
     def handle(self, *args, **options):
 
-        print("Creating Units for Inventory")
+        print("Creating Units for Inventory as well as their conversion rates")
 
-        kilo_litre, _ = FacilityInventoryUnit.objects.get_or_create(name="Kilo Litre")
-        cylinders, _ = FacilityInventoryUnit.objects.get_or_create(name="Cylinders")
+        # Inventory Unit
+
+        items, _ = FacilityInventoryUnit.objects.get_or_create(name="Items")
+        dozen, _ = FacilityInventoryUnit.objects.get_or_create(name="Dozen")
+        kg, _ = FacilityInventoryUnit.objects.get_or_create(name="kg")
+        gram, _ = FacilityInventoryUnit.objects.get_or_create(name="gram")
         cubic_meter, _ = FacilityInventoryUnit.objects.get_or_create(name="Cubic Meter")
 
-        liquid_oxygen, _ = FacilityInventoryItem.objects.get_or_create(
-            name="Liquid Oxygen", default_unit=cubic_meter, min_quantity=100
+        # Inventory Tags
+
+        safety, _ = FacilityInventoryItemTag.objects.get_or_create(name="Safety")
+        medical, _ = FacilityInventoryItemTag.objects.get_or_create(name="Medical")
+        food, _ = FacilityInventoryItemTag.objects.get_or_create(name="Food")
+
+        # Inventory Item
+
+        ppe, _ = FacilityInventoryItem.objects.get_or_create(
+            name="PPE", default_unit=items, min_quantity=150
         )
+        ppe.tags.add(safety, medical)
+        ppe.allowed_units.add(items, dozen)
+
+        rice, _ = FacilityInventoryItem.objects.get_or_create(
+            name="Rice", default_unit=kg, min_quantity=2
+        )
+        rice.tags.add(food)
+        rice.allowed_units.add(kg, gram)
+
+        fluid, _ = FacilityInventoryItem.objects.get_or_create(
+            name="IV Fluid 500 ml", default_unit=items, min_quantity=2
+        )
+        fluid.tags.add(medical)
+        fluid.allowed_units.add(items, dozen)
+
+        liquid_oxygen, _ = FacilityInventoryItem.objects.get_or_create(
+            name="Liquid Oxygen", default_unit=cubic_meter, min_quantity=10
+        )
+        liquid_oxygen.tags.add(medical)
         liquid_oxygen.allowed_units.add(cubic_meter)
 
-        jumbo_d, _ = FacilityInventoryItem.objects.get_or_create(
-            name="Jumbo D Type Oxygen Cylinder", default_unit=cylinders, min_quantity=100
+        gaseous_oxygen, _ = FacilityInventoryItem.objects.get_or_create(
+            name="Gaseous Oxygen", default_unit=cubic_meter, min_quantity=10
         )
-        jumbo_d.allowed_units.add(cylinders)
+        gaseous_oxygen.tags.add(medical)
+        gaseous_oxygen.allowed_units.add(cubic_meter)
 
-        type_b, _ = FacilityInventoryItem.objects.get_or_create(
-            name="B Type Oxygen Cylinder", default_unit=cylinders, min_quantity=100
-        )
-        type_b.allowed_units.add(cylinders)
+        # Conversion Rates
 
-        type_c, _ = FacilityInventoryItem.objects.get_or_create(
-            name="C Type Oxygen Cylinder", default_unit=cylinders, min_quantity=100
+        _, _ = FacilityInventoryUnitConverter.objects.get_or_create(
+            from_unit=gram, to_unit=kg, multiplier=0.001
         )
-        type_c.allowed_units.add(cylinders)
+
+        _, _ = FacilityInventoryUnitConverter.objects.get_or_create(
+            from_unit=dozen, to_unit=items, multiplier=12
+        )
