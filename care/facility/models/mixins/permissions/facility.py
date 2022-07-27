3265182f4daf060f25c5e542179ea0d49eec3b5b
Suryashankar Das <suryashankardas.2002@gmail.com>
2022-07-28 00:08:02 +0530

    update write access permission for staff

--- care/facility/models/mixins/permissions/facility.py
+++ care/facility/models/mixins/permissions/facility.py
@@ -57,7 +57,10 @@ class FacilityPermissionMixin(BasePermissionMixin):
             or request.user.user_type == User.TYPE_VALUE_MAP["StaffReadOnly"]
         ):
             return False
-        if request.user.user_type >= User.TYPE_VALUE_MAP["DistrictAdmin"]:
+        if (
+            request.user.user_type >= User.TYPE_VALUE_MAP["DistrictAdmin"]
+            or request.user.user_type == User.TYPE_VALUE_MAP["Staff"]
+        ):
             return self.has_object_read_permission(request)
         return request.user.home_facility == self
 
