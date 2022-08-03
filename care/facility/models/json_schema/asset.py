eac07ab6e4e75fa8c6c27e860c65034975be2df1
Suryashankar Das <suryashankardas.2002@gmail.com>
2022-08-03 20:06:56 +0530

    add camera_type to onvif asset schema

--- care/facility/models/json_schema/asset.py
+++ care/facility/models/json_schema/asset.py
@@ -15,12 +15,14 @@ ONVIF_META = {
         "local_ip_address",
         "middleware_hostname",
         "camera_access_key",
+        "camera_type",
         "port",
     ],
     "properties": {
         "local_ip_address": {"type": "string"},
         "middleware_hostname": {"type": "string"},
         "camera_access_key": {"type": "string"},
+        "camera_type": {"type": "string"},
         "port": {"type": "number"},
         "insecure_connection": {"type": "boolean"},
     },
@@ -38,4 +40,3 @@ ASSET_META = {
     ],
     "definitions": {"onvif": ONVIF_META, "hl7monitor": HL7_META, "empty": EMPTY_META},
 }
-
