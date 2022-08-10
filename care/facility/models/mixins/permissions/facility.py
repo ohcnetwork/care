6d77baae5f5c42e883d55b4998dbc6435d2116b4
cp-Coder <gupta.abhiuday.109@gmail.com>
2022-08-10 20:50:16 +0530

    Updated has_write_permission logic

--- care/facility/models/mixins/permissions/facility.py
+++ care/facility/models/mixins/permissions/facility.py
@@ -10,28 +10,31 @@ class FacilityPermissionMixin(BasePermissionMixin):
     @staticmethod
     def has_write_permission(request):
         from care.users.models import State, District, LocalBody
+
         try:
             state = State.objects.get(id=request.data["state"])
-            district = District.objects.get(id=request.data["district"]) 
+            district = District.objects.get(id=request.data["district"])
             local_body = LocalBody.objects.get(id=request.data["local_body"])
             return (
-                request.user.is_superuser or (
-                    request.user.user_type <= User.TYPE_VALUE_MAP["LocalBodyAdmin"]
+                request.user.is_superuser
+                or (
+                    request.user.user_type > User.TYPE_VALUE_MAP["DistrictAdmin"]
+                    and state == request.user.state
+                )
+                or (
+                    request.user.user_type > User.TYPE_VALUE_MAP["LocalBodyAdmin"]
+                    and state == request.user.state
+                    and district == request.user.district
+                )
+                or (
+                    request.user.user_type == User.TYPE_VALUE_MAP["LocalBodyAdmin"]
                     and state == request.user.state
                     and district == request.user.district
                     and local_body == request.user.local_body
-                ) or (
-                    request.user.user_type > User.TYPE_VALUE_MAP["LocalBodyAdmin"]
-                    and request.user.user_type <= User.TYPE_VALUE_MAP["DistrictAdmin"]
-                    and state == request.user.state
-                    and district == request.user.district
-                ) or (
-                    request.user.user_type > User.TYPE_VALUE_MAP["DistrictAdmin"]
-                    and request.user.user_type <= User.TYPE_VALUE_MAP["StateAdmin"]
-                    and state == request.user.state
                 )
             )
-        except Exception:
+        except Exception as e:
+            print(e)
             return False
 
     @staticmethod
