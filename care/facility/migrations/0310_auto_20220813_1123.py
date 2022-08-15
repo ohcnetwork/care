a3726f44ac7e3362c443ebd622b9fbc1f4a67234
cp-Coder <gupta.abhiuday.109@gmail.com>
2022-08-15 12:46:06 +0530

    Added logic to create health details for existing patients

--- care/facility/migrations/0310_auto_20220813_1123.py
+++ care/facility/migrations/0310_auto_20220813_1123.py
@@ -1,6 +1,37 @@
 from django.db import migrations, models
 
 
+def populate_data(apps, schema_editor):
+    HealthDetails = apps.get_model("facility", "PatientHealthDetails")
+    PatientRegistration = apps.get_model("facility", "PatientRegistration")
+    patients = PatientRegistration.objects.all()
+
+    health_details_objs = []
+    for patient in patients:
+        has_allergy = False
+        allergies = ""
+        blood_group = None
+
+        if patient.allergies:
+            has_allergy = True
+            allergies = patient.allergies
+
+        if patient.blood_group is not None:
+            blood_group = patient.blood_group
+
+        health_details_objs.append(
+            HealthDetails(
+                patient=patient,
+                facility=patient.facility,
+                has_allergy=has_allergy,
+                allergies=allergies,
+                blood_group=blood_group,
+            )
+        )
+
+    HealthDetails.objects.bulk_create(health_details_objs)
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -40,6 +71,7 @@ class Migration(migrations.Migration):
             name="has_allergy",
             field=models.BooleanField(default=False),
         ),
+        migrations.RunPython(populate_data, migrations.RunPython.noop),
         migrations.RemoveField(
             model_name="historicalpatientregistration",
             name="allergies",
