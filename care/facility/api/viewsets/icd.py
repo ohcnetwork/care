12c362ebe51ab5e3b4035600a4df2e11ea1f6c23
Vignesh Hari <vichuhari100@gmail.com>
2022-08-21 21:21:02 +0530

    Optimising ICD11 API's

--- care/facility/api/viewsets/icd.py
+++ care/facility/api/viewsets/icd.py
@@ -5,6 +5,8 @@ from rest_framework.viewsets import ViewSet
 def serailize_data(icd11_object):
     result = []
     for object in icd11_object:
+        if type(object) == tuple:
+            object = object[0]
         result.append({"id": object.ID, "label": object.label})
     return result
 
@@ -15,4 +17,8 @@ class ICDViewSet(ViewSet):
 
     def list(self, request):
         from care.facility.static_data.icd11 import ICDDiseases
-        return Response(serailize_data(ICDDiseases[0:10]))
+
+        queryset = ICDDiseases
+        if request.GET.get("query", False):
+            queryset = queryset.search.label(request.GET["query"], limit=100)
+        return Response(serailize_data(queryset[0:100]))
--- care/facility/static_data/icd11.py
+++ care/facility/static_data/icd11.py
@@ -4,16 +4,38 @@ from littletable import Table
 
 
 def fetch_data():
-    with open('data/icd11.json', 'r') as json_file:
+    with open("data/icd11.json", "r") as json_file:
         return json.load(json_file)
+
+
+def parse_int(str_val):
+    try:
+        return int(str_val)
+    except BaseException:
+        return -1
 
 
 ICDDiseases = Table("ICD11")
 icd11_objects = fetch_data()
-
-IGNORE_FIELDS = ["isLeaf", "classKind", "isAdoptedChild", "averageDepth", "breadthValue", "Suggested"]
+entity_id = ""
+IGNORE_FIELDS = [
+    "isLeaf",
+    "classKind",
+    "isAdoptedChild",
+    "averageDepth",
+    "breadthValue",
+    "Suggested",
+]
 
 for icd11_object in icd11_objects:
     for field in IGNORE_FIELDS:
         icd11_object.pop(field, "")
-    ICDDiseases.insert(icd11_object)
+    entity_id = icd11_object["ID"].split("/")[-1]
+    icd11_object["ID"] = parse_int(entity_id)
+    if icd11_object["ID"] == -1:
+        continue
+    if icd11_object["ID"]:
+        ICDDiseases.insert(icd11_object)
+
+ICDDiseases.create_search_index("label")
+# ICDDiseases.create_index("ID", unique=True) ## Duplicates are found so indexing is not possible
