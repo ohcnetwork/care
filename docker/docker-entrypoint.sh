6a42d55fe20f897d38ca8aef92590ee48089e21e
Aakash Singh <mail@singhaakash.dev>
2022-09-09 00:30:48 +0530

    switch to pyproject.toml

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -6,7 +6,7 @@ python manage.py migrate
 
 echo "All migrations have been made successfully"
 
-if ["${DJANGO_DEBUG,,}" == "true"]; then
+if [[ "${DJANGO_DEBUG,,}" == "true" ]]; then
   python -m debugpy --wait-for-client --listen 0.0.0.0:9876 manage.py runserver_plus 0.0.0.0:9000
 else
   python manage.py runserver 0.0.0.0:9000
