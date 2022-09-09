69c0791a3a9d7e6ed97c889b673ae9623f8722b9
Ashesh3 <3626859+Ashesh3@users.noreply.github.com>
2022-09-09 14:16:21 +0530

    Clear asset cache only when updating an existing asset

--- care/facility/api/serializers/asset.py
+++ care/facility/api/serializers/asset.py
@@ -1,4 +1,5 @@
 from re import L
+
 from django.core.cache import cache
 from django.db import transaction
 from django.shortcuts import get_object_or_404
@@ -77,8 +78,9 @@ class AssetSerializer(ModelSerializer):
         return updated_instance
 
     def save(self, **kwargs):
-        cache_key = "asset:" + str(self.instance.external_id)
-        cache.delete(cache_key)
+        if self.instance:
+            cache_key = "asset:" + str(self.instance.external_id)
+            cache.delete(cache_key)
         return super().save(**kwargs)
 
 
