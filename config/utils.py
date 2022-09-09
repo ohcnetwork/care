e593f01f1b356a48548e0766158eff001c4b1b01
Aakash Singh <mail@singhaakash.dev>
2022-09-09 17:37:47 +0530

    created utility class to generate JWT from JWKS

--- config/utils.py
+++ config/utils.py
@@ -1,2 +1,21 @@
+from datetime import datetime
+
+from authlib.jose import jwt
+from django.conf import settings
+
+
 def get_psql_search_tokens(text, operator="&"):
     return f" {operator} ".join([f"{word}:*" for word in text.strip().split(" ")])
+
+
+def generate_jwt(claims=None, exp=60):
+    if claims is None:
+        claims = {}
+    header = {"alg": "RS256"}
+    time = int(datetime.now().timestamp())
+    payload = {
+        "iat": time,
+        "exp": time + exp,
+        **claims,
+    }
+    return jwt.encode(header, payload, settings.JWKS)
