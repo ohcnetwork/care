2b6c282698d50369f29c93edaaf65321a9141b6d
Khavin Shankar <khavinshankar@gmail.com>
2022-10-28 14:39:10 +0530

    Added is_occupied read-only field in Bed (#1083)
    feat (bed): added is_occupied read-only field

--- care/facility/models/bed.py
+++ care/facility/models/bed.py
@@ -30,6 +30,10 @@ class Bed(BaseModel):
         AssetLocation, on_delete=models.PROTECT, null=False, blank=False
     )
 
+    @property
+    def is_occupied(self) -> bool:
+        return ConsultationBed.objects.filter(bed=self, end_date__isnull=True).exists()
+
     def __str__(self):
         return self.name
 
