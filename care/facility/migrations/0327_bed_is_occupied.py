8e7fbf5eded60a9010b2e82fbfd0ac56bc6544f6
Khavin Shankar <khavinshankar@gmail.com>
2022-11-10 18:36:58 +0530

    refactor: fixed black linting error

--- care/facility/migrations/0327_bed_is_occupied.py
+++ care/facility/migrations/0327_bed_is_occupied.py
@@ -6,22 +6,23 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('facility', '0326_auto_20221018_1526'),
+        ("facility", "0326_auto_20221018_1526"),
     ]
-    
+
     def update_previous_beds(apps, schema_editor):
-        Bed = apps.get_model('facility', 'Bed')
-        ConsultationBed = apps.get_model('facility', 'ConsultationBed')
+        Bed = apps.get_model("facility", "Bed")
+        ConsultationBed = apps.get_model("facility", "ConsultationBed")
         for bed in Bed.objects.all():
-            bed.is_occupied = ConsultationBed.objects.filter(bed=bed, end_date__isnull=True).exists()
+            bed.is_occupied = ConsultationBed.objects.filter(
+                bed=bed, end_date__isnull=True
+            ).exists()
             bed.save()
 
     operations = [
         migrations.AddField(
-            model_name='bed',
-            name='is_occupied',
+            model_name="bed",
+            name="is_occupied",
             field=models.BooleanField(default=False),
         ),
-        migrations.RunPython(update_previous_beds)
+        migrations.RunPython(update_previous_beds),
     ]
-
