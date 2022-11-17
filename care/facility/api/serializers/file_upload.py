658744b2e1364ea4f4043dced601564378094d79
Pranshu1902 <pranshu1902@gmail.com>
2022-11-17 16:44:20 +0530

    added is active validation

--- care/facility/api/serializers/file_upload.py
+++ care/facility/api/serializers/file_upload.py
@@ -15,6 +15,8 @@ def check_permissions(file_type, associating_id, user):
     try:
         if file_type == FileUpload.FileType.PATIENT.value:
             patient = PatientRegistration.objects.get(external_id=associating_id)
+            if patient.is_active == False:
+                raise serializers.ValidationError({"Patient": "Patient has been discharged from CARE"})
             if patient.assigned_to:
                 if user == patient.assigned_to:
                     return patient.id
