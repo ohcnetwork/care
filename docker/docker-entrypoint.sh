ce9cc1edb3e18117eab614be61853cda8315cc87
rithviknishad <rithvikn2001@gmail.com>
2022-12-23 17:18:48 +0530

    collectstatic always overwrite

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,11 +3,11 @@ set -euxo pipefail
 
 echo "running migrations"
 python manage.py migrate
+echo "All migrations have been made successfully"
 
 echo "running collectstatic"
-python manage.py collectstatic
+python manage.py collectstatic --noinput
 
-echo "All migrations have been made successfully"
 
 if [[ "${DJANGO_DEBUG,,}" == "true" ]]; then
   python -m debugpy --wait-for-client --listen 0.0.0.0:9876 manage.py runserver_plus 0.0.0.0:9000
