354d58ce823a4e65c0d6cef84b4960228b26a8d2
Vignesh Hari <vichuhari100@gmail.com>
2023-01-01 20:44:34 +0530

    Add V2 API's

--- care/abdm/utils/api_call.py
+++ care/abdm/utils/api_call.py
@@ -9,8 +9,20 @@ HEALTH_SERVICE_API_URL = "https://healthidsbx.abdm.gov.in/api"
 ABDM_TOKEN_URL = GATEWAY_API_URL + "gateway/v0.5/sessions"
 ABDM_TOKEN_CACHE_KEY = "abdm_token"
 
-
 # TODO: Exception handling for all api calls, need to gracefully handle known exceptions
+
+from Crypto.Cipher import PKCS1_OAEP
+from Crypto.PublicKey import RSA
+from base64 import b64encode
+
+
+def encrypt_with_public_key(a_message):
+    rsa_public_key = RSA.importKey(requests.get(HEALTH_SERVICE_API_URL + "/v2/auth/cert", verify=False).text.strip())
+    rsa_public_key = PKCS1_OAEP.new(rsa_public_key)
+    encrypted_text = rsa_public_key.encrypt(a_message.encode())
+
+    return b64encode(encrypted_text).decode()
+
 
 class APIGateway:
     def __init__(self, gateway, token):
@@ -20,11 +32,11 @@ class APIGateway:
             self.url = GATEWAY_API_URL
         self.token = token
 
-    def encrypt(self, data):
-        cert = cache.get("abdm_cert")
-        if not cert:
-            cert = requests.get(settings.ABDM_CERT_URL).text
-            cache.set("abdm_cert", cert, 3600)
+    # def encrypt(self, data):
+    #     cert = cache.get("abdm_cert")
+    #     if not cert:
+    #         cert = requests.get(settings.ABDM_CERT_URL).text
+    #         cache.set("abdm_cert", cert, 3600)
 
     def add_auth_header(self, headers):
         token = cache.get(ABDM_TOKEN_CACHE_KEY)
@@ -39,7 +51,7 @@ class APIGateway:
                 "Accept": "application/json",
             }
             resp = requests.post(
-                ABDM_TOKEN_URL, data=json.dumps(data), headers=auth_headers
+                ABDM_TOKEN_URL, data=json.dumps(data), headers=auth_headers, verify=False
             )
             print("Token Response Status: {}".format(resp.status_code))
             if resp.status_code < 300:
@@ -70,7 +82,7 @@ class APIGateway:
         url = self.url + path
         headers = {}
         headers = self.add_auth_header(headers)
-        response = requests.get(url, headers=headers, params=params)
+        response = requests.get(url, headers=headers, params=params, verify=False)
         return response
 
     def post(self, path, data=None):
@@ -87,7 +99,7 @@ class APIGateway:
         data_json = json.dumps(data)
         # print("curl -X POST {} {} -d {}".format(url, headers_string, data_json))
         print("Posting Request to: {}".format(url))
-        response = requests.post(url, headers=headers, data=data_json)
+        response = requests.post(url, headers=headers, data=data_json, verify=False)
         print("{} Response: {}".format(response.status_code, response.text))
         return response
 
@@ -135,7 +147,16 @@ class HealthIdGatewayV2:
         self.api = APIGateway("health", None)
 
     # V2 APIs
-    def check_and_generate_mobile_otp(self, data):
-        path = "/v2/registration/aadhaar/checkAndGenerateMobileOTP"
+    def generate_aadhaar_otp(self, data):
+        path = "/v2/registration/aadhaar/generateOtp"
+        data["aadhaar"] = encrypt_with_public_key(data["aadhaar"])
+        data.pop("cancelToken", {})
+        response = self.api.post(path, data)
+        return response.json()
+
+    def generate_mobile_otp(self, data):
+        path = "/v2/registration/aadhaar/generateOtp"
+        data["aadhaar"] = encrypt_with_public_key(data["aadhaar"])
+        data.pop("cancelToken", {})
         response = self.api.post(path, data)
         return response.json()
