fcb0c96a00f3370a99f6b4d225e2f498cd4210a6
Khavin Shankar <khavinshankar@gmail.com>
2023-02-14 11:41:11 +0530

    hcx: added policy model

---
+++ care/hcx/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+class HcxConfig(AppConfig):
+    name = 'hcx'
+    verbose_name = _("HCX Integration")
---
+++ care/hcx/models/base.py
@@ -0,0 +1,44 @@
+def reverse_choices(choices):
+    output = {}
+    for choice in choices:
+        output[choice[0]] = choice[1]
+    return output
+
+
+# http://hl7.org/fhir/fm-status
+STATUS_CHOICES = [
+    ("active", "Active"),
+    ("cancelled", "Cancelled"),
+    ("draft", "Draft"),
+    ("entered-in-error", "Entered in Error")
+]
+REVERSE_STATUS_CHOICES = reverse_choices(STATUS_CHOICES)
+
+
+# http://terminology.hl7.org/CodeSystem/processpriority
+PRIORITY_CHOICES = [
+    ("stat", "Immediate"),
+    ("normal", "Normal"),
+    ("deferred", "Deferred"),
+]
+REVERSE_PRIORITY_CHOICES = reverse_choices(PRIORITY_CHOICES)
+
+
+# http://hl7.org/fhir/eligibilityrequest-purpose
+PURPOSE_CHOICES = [
+    ("auth-requirements", "Auth Requirements"),
+    ("benefits", "Benefits"),
+    ("discovery", "Discovery"),
+    ("validation", "Validation")
+]
+REVERSE_PURPOSE_CHOICES = reverse_choices(PURPOSE_CHOICES)
+
+
+# http://hl7.org/fhir/remittance-outcome
+OUTCOME_CHOICES = [
+    ("queued", "Queued"),
+    ("complete", "Processing Complete"),
+    ("error", "Error"),
+    ("partial", "Partial Processing")
+]
+REVERSE_OUTCOME_CHOICES = reverse_choices(OUTCOME_CHOICES)
