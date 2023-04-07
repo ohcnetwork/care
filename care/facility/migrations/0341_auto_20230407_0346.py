e9974b687f308d1afa3d0ad3b80e5a081a7a96e8
Gigin George <mail.gigin@gmail.com>
2023-04-07 21:16:50 +0530

    Revert Custom Migration

--- care/facility/migrations/0341_auto_20230407_0346.py
+++ care/facility/migrations/0341_auto_20230407_0346.py
@@ -15,11 +15,6 @@ class Migration(migrations.Migration):
             name='ventilator_interface',
             field=models.IntegerField(choices=[(0, 'UNKNOWN'), (5, 'INVASIVE'), (10, 'NON_INVASIVE'), (15, 'OXYGEN_SUPPORT')], default=0),
         ),
-        # convert all UNKNOWN fields in db to OXYGEN_SUPPORT
-        migrations.RunSQL(
-            sql="UPDATE facility_dailyround SET ventilator_interface = 15 WHERE ventilator_interface = 0 AND (ventilator_oxygen_modality != 0 OR ventilator_oxygen_modality_flow_rate IS NOT NULL OR ventilator_oxygen_modality_oxygen_rate IS NOT NULL)",
-            reverse_sql="UPDATE facility_dailyround SET ventilator_interface = 0 WHERE ventilator_interface = 15 AND (ventilator_oxygen_modality = 0 AND ventilator_oxygen_modality_flow_rate IS NULL AND ventilator_oxygen_modality_oxygen_rate IS NULL)",
-        ),
     ]
 
     
