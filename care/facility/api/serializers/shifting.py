7aae131a6f24ab2b9cc6c4f640307e37baaababb
Ashesh3 <3626859+Ashesh3@users.noreply.github.com>
2023-04-12 19:50:26 +0530

    Formatting

--- care/facility/api/serializers/shifting.py
+++ care/facility/api/serializers/shifting.py
@@ -4,7 +4,10 @@ from rest_framework.exceptions import ValidationError
 
 from care.facility.api.serializers import TIMESTAMP_FIELDS
 from care.facility.api.serializers.facility import FacilityBasicInfoSerializer
-from care.facility.api.serializers.patient import PatientDetailSerializer, PatientListSerializer
+from care.facility.api.serializers.patient import (
+    PatientDetailSerializer,
+    PatientListSerializer,
+)
 from care.facility.models import (
     BREATHLESSNESS_CHOICES,
     FACILITY_TYPES,
@@ -42,7 +45,10 @@ def has_facility_permission(user, facility):
             user.user_type >= User.TYPE_VALUE_MAP["DistrictLabAdmin"]
             and (facility and user.district == facility.district)
         )
-        or (user.user_type >= User.TYPE_VALUE_MAP["StateLabAdmin"] and (facility and user.state == facility.state))
+        or (
+            user.user_type >= User.TYPE_VALUE_MAP["StateLabAdmin"]
+            and (facility and user.state == facility.state)
+        )
     )
 
 
@@ -53,29 +59,47 @@ class ShiftingSerializer(serializers.ModelSerializer):
     status = ChoiceField(choices=SHIFTING_STATUS_CHOICES)
     breathlessness_level = ChoiceField(choices=BREATHLESSNESS_CHOICES, required=False)
 
-    patient_object = PatientListSerializer(source="patient", read_only=True, required=False)
+    patient_object = PatientListSerializer(
+        source="patient", read_only=True, required=False
+    )
 
-    orgin_facility_object = FacilityBasicInfoSerializer(source="orgin_facility", read_only=True, required=False)
+    orgin_facility_object = FacilityBasicInfoSerializer(
+        source="orgin_facility", read_only=True, required=False
+    )
     shifting_approving_facility_object = FacilityBasicInfoSerializer(
         source="shifting_approving_facility", read_only=True, required=False
     )
-    assigned_facility_object = FacilityBasicInfoSerializer(source="assigned_facility", read_only=True, required=False)
+    assigned_facility_object = FacilityBasicInfoSerializer(
+        source="assigned_facility", read_only=True, required=False
+    )
 
     assigned_facility_type = ChoiceField(choices=FACILITY_TYPES, required=False)
     preferred_vehicle_choice = ChoiceField(choices=VEHICLE_CHOICES, required=False)
 
-    orgin_facility = serializers.UUIDField(source="orgin_facility.external_id", allow_null=False, required=True)
-    shifting_approving_facility = serializers.UUIDField(
-        source="shifting_approving_facility.external_id", allow_null=False, required=True
+    orgin_facility = serializers.UUIDField(
+        source="orgin_facility.external_id", allow_null=False, required=True
     )
-    assigned_facility = serializers.UUIDField(source="assigned_facility.external_id", allow_null=True, required=False)
-    assigned_facility_external = serializers.CharField(required=False, allow_null=True, allow_blank=True)
+    shifting_approving_facility = serializers.UUIDField(
+        source="shifting_approving_facility.external_id",
+        allow_null=False,
+        required=True,
+    )
+    assigned_facility = serializers.UUIDField(
+        source="assigned_facility.external_id", allow_null=True, required=False
+    )
+    assigned_facility_external = serializers.CharField(
+        required=False, allow_null=True, allow_blank=True
+    )
 
-    patient = serializers.UUIDField(source="patient.external_id", allow_null=False, required=True)
+    patient = serializers.UUIDField(
+        source="patient.external_id", allow_null=False, required=True
+    )
 
     assigned_to_object = UserBaseMinimumSerializer(source="assigned_to", read_only=True)
     created_by_object = UserBaseMinimumSerializer(source="created_by", read_only=True)
-    last_edited_by_object = UserBaseMinimumSerializer(source="last_edited_by", read_only=True)
+    last_edited_by_object = UserBaseMinimumSerializer(
+        source="last_edited_by", read_only=True
+    )
 
     def __init__(self, instance=None, **kwargs):
         if instance:
@@ -89,7 +113,9 @@ class ShiftingSerializer(serializers.ModelSerializer):
             "DESTINATION REJECTED",
             "COMPLETED",
         ]
-        LIMITED_RECIEVING_STATUS = [REVERSE_SHIFTING_STATUS_CHOICES[x] for x in LIMITED_RECIEVING_STATUS_]
+        LIMITED_RECIEVING_STATUS = [
+            REVERSE_SHIFTING_STATUS_CHOICES[x] for x in LIMITED_RECIEVING_STATUS_
+        ]
         LIMITED_SHIFTING_STATUS_ = [
             "APPROVED",
             "REJECTED",
@@ -100,8 +126,10 @@ class ShiftingSerializer(serializers.ModelSerializer):
             "PENDING",
             "ON HOLD",
         ]
-        LIMITED_SHIFTING_STATUS = [REVERSE_SHIFTING_STATUS_CHOICES[x] for x in LIMITED_SHIFTING_STATUS_]
-        LIMITED_ORGIN_STATUS = []
+        LIMITED_SHIFTING_STATUS = [
+            REVERSE_SHIFTING_STATUS_CHOICES[x] for x in LIMITED_SHIFTING_STATUS_
+        ]
+        # LIMITED_ORGIN_STATUS = []
 
         RECIEVING_REQUIRED_STATUS_ = [
             "DESTINATION APPROVED",
@@ -111,7 +139,9 @@ class ShiftingSerializer(serializers.ModelSerializer):
             "TRANSFER IN PROGRESS",
             "COMPLETED",
         ]
-        RECIEVING_REQUIRED_STATUS = [REVERSE_SHIFTING_STATUS_CHOICES[x] for x in RECIEVING_REQUIRED_STATUS_]
+        RECIEVING_REQUIRED_STATUS = [
+            REVERSE_SHIFTING_STATUS_CHOICES[x] for x in RECIEVING_REQUIRED_STATUS_
+        ]
 
         user = self.context["request"].user
 
@@ -122,7 +152,9 @@ class ShiftingSerializer(serializers.ModelSerializer):
 
         if "is_kasp" in validated_data:
             if validated_data["is_kasp"] != instance.is_kasp:  # Check only when changed
-                if not has_facility_permission(user, instance.shifting_approving_facility):
+                if not has_facility_permission(
+                    user, instance.shifting_approving_facility
+                ):
                     raise ValidationError({"kasp": ["Permission Denied"]})
 
         if "status" in validated_data:
@@ -131,7 +163,9 @@ class ShiftingSerializer(serializers.ModelSerializer):
                     if not has_facility_permission(user, instance.assigned_facility):
                         raise ValidationError({"status": ["Permission Denied"]})
             elif validated_data["status"] in LIMITED_SHIFTING_STATUS:
-                if not has_facility_permission(user, instance.shifting_approving_facility):
+                if not has_facility_permission(
+                    user, instance.shifting_approving_facility
+                ):
                     raise ValidationError({"status": ["Permission Denied"]})
 
         # Dont allow editing origin or patient
@@ -141,7 +175,9 @@ class ShiftingSerializer(serializers.ModelSerializer):
             validated_data.pop("patient")
 
         if "shifting_approving_facility" in validated_data:
-            shifting_approving_facility_external_id = validated_data.pop("shifting_approving_facility")["external_id"]
+            shifting_approving_facility_external_id = validated_data.pop(
+                "shifting_approving_facility"
+            )["external_id"]
             if shifting_approving_facility_external_id:
                 validated_data["shifting_approving_facility_id"] = Facility.objects.get(
                     external_id=shifting_approving_facility_external_id
@@ -149,7 +185,9 @@ class ShiftingSerializer(serializers.ModelSerializer):
 
         assigned = False
         if validated_data.get("assigned_facility"):
-            assigned_facility_external_id = validated_data.pop("assigned_facility")["external_id"]
+            assigned_facility_external_id = validated_data.pop("assigned_facility")[
+                "external_id"
+            ]
             if assigned_facility_external_id:
                 validated_data["assigned_facility_id"] = Facility.objects.get(
                     external_id=assigned_facility_external_id
@@ -162,8 +200,19 @@ class ShiftingSerializer(serializers.ModelSerializer):
 
         if "status" in validated_data:
             if validated_data["status"] in RECIEVING_REQUIRED_STATUS:
-                if (not (instance.assigned_facility or instance.assigned_facility_external)) and (not assigned):
-                    raise ValidationError({"status": ["Destination Facility is required for moving to this stage."]})
+                if (
+                    not (
+                        instance.assigned_facility
+                        or instance.assigned_facility_external
+                    )
+                ) and (not assigned):
+                    raise ValidationError(
+                        {
+                            "status": [
+                                "Destination Facility is required for moving to this stage."
+                            ]
+                        }
+                    )
 
         instance.last_edited_by = self.context["request"].user
 
@@ -179,7 +228,10 @@ class ShiftingSerializer(serializers.ModelSerializer):
                         caused_by=self.context["request"].user,
                         caused_object=ShiftingRequest.objects.get(id=new_instance.id),
                         facility=new_instance.shifting_approving_facility,
-                        notification_mediums=[Notification.Medium.SYSTEM, Notification.Medium.SMS],
+                        notification_mediums=[
+                            Notification.Medium.SYSTEM,
+                            Notification.Medium.SMS,
+                        ],
                     ).generate()
 
         return new_instance
@@ -192,10 +244,12 @@ class ShiftingSerializer(serializers.ModelSerializer):
 
         validated_data["is_kasp"] = False
 
-        orgin_facility_external_id = validated_data.pop("orgin_facility")["external_id"]
+        # orgin_facility_external_id = validated_data.pop("orgin_facility")["external_id"]
         # validated_data["orgin_facility_id"] = Facility.objects.get(external_id=orgin_facility_external_id).id
 
-        shifting_approving_facility_external_id = validated_data.pop("shifting_approving_facility")["external_id"]
+        shifting_approving_facility_external_id = validated_data.pop(
+            "shifting_approving_facility"
+        )["external_id"]
         validated_data["shifting_approving_facility_id"] = Facility.objects.get(
             external_id=shifting_approving_facility_external_id
         ).id
@@ -206,7 +260,9 @@ class ShiftingSerializer(serializers.ModelSerializer):
             validated_data["assigned_facility_external"] = None
 
         if validated_data.get("assigned_facility"):
-            assigned_facility_external_id = validated_data.pop("assigned_facility")["external_id"]
+            assigned_facility_external_id = validated_data.pop("assigned_facility")[
+                "external_id"
+            ]
             if assigned_facility_external_id:
 
                 validated_data["assigned_facility_id"] = Facility.objects.get(
@@ -216,17 +272,21 @@ class ShiftingSerializer(serializers.ModelSerializer):
         patient_external_id = validated_data.pop("patient")["external_id"]
         patient = PatientRegistration.objects.get(external_id=patient_external_id)
 
-        if patient.is_active == False:
+        if patient.is_active is False:
             raise ValidationError({"patient": ["Cannot shift discharged patient"]})
-        if patient.allow_transfer == False:
+        if patient.allow_transfer is False:
             patient.allow_transfer = True
             patient.save()
 
         validated_data["orgin_facility_id"] = patient.facility.id
         validated_data["patient_id"] = patient.id
 
-        if ShiftingRequest.objects.filter(~Q(status__in=[30, 50, 80]), patient=patient).exists():
-            raise ValidationError({"request": ["Shifting Request for Patient already exists"]})
+        if ShiftingRequest.objects.filter(
+            ~Q(status__in=[30, 50, 80]), patient=patient
+        ).exists():
+            raise ValidationError(
+                {"request": ["Shifting Request for Patient already exists"]}
+            )
 
         validated_data["created_by"] = self.context["request"].user
         validated_data["last_edited_by"] = self.context["request"].user
