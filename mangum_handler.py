9687b430bb42457c909cb5672de648c9b7c6eaac
Vignesh Hari <vichuhari100@gmail.com>
2023-05-17 19:37:43 +0530

    Move from ASGI to WSGI for lambda

--- mangum_handler.py
+++ mangum_handler.py
@@ -1,11 +1,11 @@
-# asgi.py
 import os
-from mangum import Mangum
+
+from asgiref.wsgi import WsgiToAsgi
 from django.core.wsgi import get_wsgi_application
+from mangum import Mangum
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "care.config.settings")
 
-application = get_wsgi_application()
+application = WsgiToAsgi(get_wsgi_application())
 
 handler = Mangum(application, lifespan="off")
-
