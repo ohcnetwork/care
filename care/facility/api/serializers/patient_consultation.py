cdb3e505c859f68091d9633c0b0f3ad34da9b3c7
rithviknishad <rithvikn2001@gmail.com>
2023-05-24 17:50:36 +0530

    skip creating ConsultationBed if suggeestion is OP

--- care/facility/api/serializers/patient_consultation.py
+++ care/facility/api/serializers/patient_consultation.py
@@ -258,7 +258,7 @@ class PatientConsultationSerializer(serializers.ModelSerializer):
         consultation.last_edited_by = self.context["request"].user
         consultation.save()
 
-        if bed:
+        if bed and not consultation.suggestion == SuggestionChoices.OP:
             consultation_bed = ConsultationBed(
                 bed=bed,
                 consultation=consultation,
