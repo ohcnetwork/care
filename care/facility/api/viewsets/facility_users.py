a75b98ddfa2a472f59a4a4d7091016489ce0570f
yaswanthsaivendra <yaswanthsaivendra@gmail.com>
2023-05-27 16:27:29 +0530

    Revert "used prefetch where we are querying the skills using UserAssigned Serializer"
    This reverts commit 7000a38bafd16e789ccf6ffc5f9a1f41d0c34429.

--- care/facility/api/viewsets/facility_users.py
+++ care/facility/api/viewsets/facility_users.py
@@ -1,5 +1,3 @@
-from django.core.exceptions import ObjectDoesNotExist
-from django.db.models import Prefetch
 from django_filters import rest_framework as filters
 from rest_framework import mixins
 from rest_framework.exceptions import ValidationError
@@ -8,14 +6,12 @@ from rest_framework.viewsets import GenericViewSet
 
 from care.facility.models.facility import Facility
 from care.users.api.serializers.user import UserAssignedSerializer
-from care.users.models import Skill, User
+from care.users.models import User
 
 
 class UserFilter(filters.FilterSet):
-    user_type = filters.TypedChoiceFilter(
-        choices=[(key, key) for key in User.TYPE_VALUE_MAP.keys()],
-        coerce=lambda role: User.TYPE_VALUE_MAP[role],
-    )
+    user_type = filters.TypedChoiceFilter(choices=[(key, key) for key in User.TYPE_VALUE_MAP.keys()],
+                                          coerce=lambda role: User.TYPE_VALUE_MAP[role])
 
     class Meta:
         model = User
@@ -25,17 +21,13 @@ class UserFilter(filters.FilterSet):
 class FacilityUserViewSet(GenericViewSet, mixins.ListModelMixin):
     serializer_class = UserAssignedSerializer
     filterset_class = UserFilter
-    queryset = User.objects.all().prefetch_related(
-        Prefetch("skills", queryset=Skill.objects.filter(userskill__deleted=False))
-    )
+    queryset = User.objects.all()
     permission_classes = [IsAuthenticated]
     filter_backends = [filters.DjangoFilterBackend]
 
     def get_queryset(self):
         try:
-            facility = Facility.objects.get(
-                external_id=self.kwargs.get("facility_external_id")
-            )
+            facility = Facility.objects.get(external_id=self.kwargs.get("facility_external_id"))
             return facility.users.filter(deleted=False).order_by("-last_login")
-        except ObjectDoesNotExist:
+        except:
             raise ValidationError({"Facility": "Facility not found"})
