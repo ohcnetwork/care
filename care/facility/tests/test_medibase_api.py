d879fa0944c5e0bc2c0fef32933d608c78f41e87
rithviknishad <rithvikn2001@gmail.com>
2023-06-15 19:46:56 +0530

    fix test casing

--- care/facility/tests/test_medibase_api.py
+++ care/facility/tests/test_medibase_api.py
@@ -15,11 +15,11 @@ class TestMedibaseApi(TestBase):
     def test_search_by_generic_exact_word(self):
         response = self.client.get(self.get_url(query="pAraCetAmoL"))
         self.assertEquals(response.status_code, status.HTTP_200_OK)
-        self.assertEquals(response.data[0]["generic"], "PARACETAMOL")
+        self.assertEquals(response.data[0]["generic"], "paracetamol")
 
     def test_search_by_name_and_generic_exact_word(self):
         response = self.client.get(self.get_url(query="panadol paracetamol"))
         self.assertEquals(response.status_code, status.HTTP_200_OK)
         self.assertEquals(response.data[0]["name"], "PANADOL")
-        self.assertEquals(response.data[0]["generic"], "PARACETAMOL")
+        self.assertEquals(response.data[0]["generic"], "paracetamol")
         self.assertEquals(response.data[0]["company"], "GSK")
