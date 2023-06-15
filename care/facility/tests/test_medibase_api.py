8ac3119c1edbf510cff5816618c02628c5a5e44f
rithviknishad <rithvikn2001@gmail.com>
2023-06-15 19:06:24 +0530

    adds test for medibase

---
+++ care/facility/tests/test_medibase_api.py
@@ -0,0 +1,25 @@
+from rest_framework import status
+
+from care.utils.tests.test_base import TestBase
+
+
+class TestMedibaseApi(TestBase):
+    def get_url(self, query=None):
+        return f"/api/v1/medibase/?query={query}"
+
+    def test_search_by_name_exact_word(self):
+        response = self.client.get(self.get_url(query="dolo"))
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+        self.assertEquals(response.data[0]["name"], "DOLO")
+
+    def test_search_by_generic_exact_word(self):
+        response = self.client.get(self.get_url(query="pAraCetAmoL"))
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+        self.assertEquals(response.data[0]["generic"], "PARACETAMOL")
+
+    def test_search_by_name_and_generic_exact_word(self):
+        response = self.client.get(self.get_url(query="panadol paracetamol"))
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+        self.assertEquals(response.data[0]["name"], "PANADOL")
+        self.assertEquals(response.data[0]["generic"], "PARACETAMOL")
+        self.assertEquals(response.data[0]["company"], "GSK")
