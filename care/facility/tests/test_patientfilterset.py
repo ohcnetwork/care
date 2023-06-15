e8011e9a4da9242d87e950c6efdbfc3e2ebcad1e
yaswanthsaivendra <yaswanthsaivendra@gmail.com>
2023-06-15 23:07:05 +0530

    updated the test to make use of testbase

--- care/facility/tests/test_patientfilterset.py
+++ care/facility/tests/test_patientfilterset.py
@@ -6,45 +6,20 @@ from care.facility.models import (
     AssetLocation,
     Bed,
     ConsultationBed,
-    Facility,
-    PatientConsultation,
     PatientRegistration,
-    State,
 )
-from care.facility.tests.mixins import TestClassMixin
+from care.utils.tests.test_base import TestBase
 
 
-class PatientFilterSetTestCase(TestClassMixin, TestCase):
-    def setUp(self):
-        super().setUp()
-        self.creator = self.users[0]
-        state = State.objects.create(name="Kerala")
+class PatientFilterSetTestCase(TestBase, TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
 
-        # create patient registrations
-
-        patient_data = {"gender": 1, "age": 30, "is_antenatal": False, "state": state}
-
-        self.patient1 = PatientRegistration.objects.create(**patient_data)
-        self.patient2 = PatientRegistration.objects.create(**patient_data)
-        self.patient3 = PatientRegistration.objects.create(**patient_data)
-
-        # Create Facility
-        sample_data = {
-            "name": "Hospital X",
-            "ward": self.creator.ward,
-            "local_body": self.creator.local_body,
-            "district": self.creator.district,
-            "state": self.creator.state,
-            "facility_type": 1,
-            "address": "Nearby",
-            "pincode": 390024,
-            "features": [],
-        }
-        self.facility = Facility.objects.create(
-            external_id="550e8400-e29b-41d4-a716-446655440000",
-            created_by=self.creator,
-            **sample_data,
-        )
+    def test_filter_by_bed_type(self):
+        patient1 = self.create_patient(name="patient1")
+        patient2 = self.create_patient(name="patient2")
+        patient3 = self.create_patient(name="patient3")
 
         # create asset
         asset1 = AssetLocation.objects.create(
@@ -68,16 +43,14 @@ class PatientFilterSetTestCase(TestClassMixin, TestCase):
         bed1 = Bed.objects.create(**bed1_data)
         bed2 = Bed.objects.create(**bed2_data)
 
-        # create consultations
-        consultation1 = PatientConsultation.objects.create(
-            patient=self.patient1,
-            facility=self.facility,
+        consultation1 = self.create_consultation(
+            patient=patient1, facility=self.facility
         )
-        consultation2 = PatientConsultation.objects.create(
-            patient=self.patient2, facility=self.facility
+        consultation2 = self.create_consultation(
+            patient=patient2, facility=self.facility
         )
-        consultation3 = PatientConsultation.objects.create(
-            patient=self.patient3, facility=self.facility
+        consultation3 = self.create_consultation(
+            patient=patient3, facility=self.facility
         )
 
         # consultation beds
@@ -94,30 +67,26 @@ class PatientFilterSetTestCase(TestClassMixin, TestCase):
         consultation2.save(update_fields=["current_bed"])
         # None for consultation 3
 
-        self.patient1.last_consultation = consultation1
-        self.patient1.save(update_fields=["last_consultation"])
-        self.patient2.last_consultation = consultation2
-        self.patient2.save(update_fields=["last_consultation"])
-        self.patient3.last_consultation = consultation3
-        self.patient3.save(update_fields=["last_consultation"])
+        patient1.last_consultation = consultation1
+        patient1.save(update_fields=["last_consultation"])
+        patient2.last_consultation = consultation2
+        patient2.save(update_fields=["last_consultation"])
+        patient3.last_consultation = consultation3
+        patient3.save(update_fields=["last_consultation"])
 
-    def test_filter_by_bed_type(self):
-        # Create the filter set instance with the desired queryset
+        # Create the filter set instance
         filterset = PatientFilterSet(queryset=PatientRegistration.objects.all())
 
-        # Apply the filter manually
+        # filter
         filtered_queryset = filterset.filter_by_bed_type(
             name="last_consultation_admitted_bed_type_list",
             value="1,None",
             queryset=PatientRegistration.objects.all(),
         )
 
-        # Assert the expected results
-        self.assertEqual(len(filtered_queryset), 2)
-        self.assertTrue(self.patient1 in filtered_queryset)
-        self.assertFalse(self.patient2 in filtered_queryset)
-        self.assertTrue(self.patient3 in filtered_queryset)
-        self.assertEqual(filtered_queryset[0].last_consultation.current_bed, None)
-        self.assertEqual(
-            filtered_queryset[1].last_consultation.current_bed.bed.bed_type, 1
-        )
+        # Assert
+        self.assertEqual(len(filtered_queryset), 3)  # patient, patient1 and patient3
+        self.assertTrue(patient1 in filtered_queryset)
+        self.assertFalse(patient2 in filtered_queryset)
+        self.assertTrue(patient3 in filtered_queryset)
+        self.assertTrue(self.patient in filtered_queryset)
