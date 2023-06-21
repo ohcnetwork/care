c580e4b77c594bb997abef569e3ffe4cbc3a8025
Khavin Shankar <khavinshankar@gmail.com>
2023-06-21 08:42:19 +0530

    set debug to True

--- config/settings/base.py
+++ config/settings/base.py
@@ -29,7 +29,7 @@ if READ_DOT_ENV_FILE:
 # GENERAL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#debug
-DEBUG = env.bool("DJANGO_DEBUG", False)
+DEBUG = env.bool("DJANGO_DEBUG", True)
 # Local time zone. Choices are
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # though not all of them may be available with every OS.
