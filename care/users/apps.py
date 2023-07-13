d061cd6526a53d8bd9f2bbc6a8398ddd904c3273
Aakash Singh <mail@singhaakash.dev>
2023-07-13 15:12:53 +0530

    add table to track user facility allocation

--- care/users/apps.py
+++ care/users/apps.py
@@ -7,7 +7,4 @@ class UsersConfig(AppConfig):
     verbose_name = _("Users")
 
     def ready(self):
-        try:
-            import care.users.signals  # noqa F401
-        except ImportError:
-            pass
+        import care.users.signals  # noqa F401
