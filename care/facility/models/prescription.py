e9d59348a748b0dd058969df63e5e3e697bf6d3c
rithviknishad <mail@rithviknishad.dev>
2023-07-14 10:57:17 +0530

    improve string repr

--- care/facility/models/prescription.py
+++ care/facility/models/prescription.py
@@ -64,7 +64,7 @@ class MedibaseMedicine(BaseModel):
     atc_classification = models.TextField(blank=True, null=True)
 
     def __str__(self):
-        return " - ".join([self.name, self.generic or "", self.company or ""])
+        return " - ".join([i for i in [self.name, self.generic, self.company] if i])
 
 
 class Prescription(BaseModel):
