11b91dfb260ab40ccc73ba65d732a69d90ed1bd2
Ashesh3 <3626859+Ashesh3@users.noreply.github.com>
2023-07-19 17:24:46 +0530

    format files

--- care/facility/migrations/0336_patientnotes_editable_until.py
+++ care/facility/migrations/0336_patientnotes_editable_until.py
@@ -4,15 +4,14 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('facility', '0335_auto_20230207_1914'),
+        ("facility", "0335_auto_20230207_1914"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='patientnotes',
-            name='editable_until',
+            model_name="patientnotes",
+            name="editable_until",
             field=models.DateTimeField(blank=True, null=True),
         ),
     ]
