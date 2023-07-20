54317daf46015e738ae99ada603f88b2f3dad386
khavinshankar <khavinshankar@gmail.com>
2023-07-20 22:44:01 +0530

    use UUIDField for uuids

--- care/abdm/api/serializers/healthid.py
+++ care/abdm/api/serializers/healthid.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import CharField, Serializer
+from rest_framework.serializers import CharField, Serializer, UUIDField
 
 
 class AadharOtpGenerateRequestPayloadSerializer(Serializer):
@@ -61,13 +61,8 @@ class QRContentSerializer(Serializer):
         required=True,
         help_text="Name",
     )
-    # {"statelgd":"33","distlgd":"573","address":"C/O Gopalsamy NO 33 A WESTSTREET ODANILAI KASTHURIBAI GRAMAM ARACHALUR Erode","state name":"TAMIL NADU","dist name":"Erode","mobile":"7639899448"}
 
 
-# {
-#   "authMethod": "AADHAAR_OTP",
-#   "healthid": "43-4221-5105-6749"
-# }
 class HealthIdAuthSerializer(Serializer):
     authMethod = CharField(
         max_length=64,
@@ -81,12 +76,6 @@ class HealthIdAuthSerializer(Serializer):
         required=True,
         help_text="Health ID",
     )
-
-
-#   "gender": "M",
-#   "mobile": "9545812125",
-#   "name": "suraj singh karki",
-#   "yearOfBirth": "1994"
 
 
 class ABHASearchRequestSerializer:
@@ -132,9 +121,9 @@ class VerifyOtpRequestPayloadSerializer(Serializer):
         help_text="Transaction ID",
         validators=[],
     )
-    patientId = CharField(
+    patientId = UUIDField(
         required=False, help_text="Patient ID to be linked", validators=[]
-    )  # TODO: Add UUID Validation
+    )
 
 
 class VerifyDemographicsRequestPayloadSerializer(Serializer):
@@ -168,16 +157,6 @@ class VerifyDemographicsRequestPayloadSerializer(Serializer):
     )
 
 
-# {
-#   "email": "Example@Demo.com",
-#   "firstName": "manoj",
-#   "healthId": "deepak.pant",
-#   "lastName": "singh",
-#   "middleName": "kishan",
-#   "password": "India@143",
-#   "profilePhoto": "/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkJCQkJCQoLCwoODw0PDhQSERESFB4WFxYXFh4uHSEdHSEdLikxKCUoMSlJOTMzOUlUR0NHVGZbW2aBeoGoqOIBCQkJCQkJCgsLCg4PDQ8OFBIRERIUHhYXFhcWHi4dIR0dIR0uKTEoJSgxKUk5MzM5SVRHQ0dUZltbZoF6gaio4v/CABEIBLAHgAMBIgACEQEDEQH/xAAbAAACAwEBAQAAAAAAAAAAAAACAwABBAUGB//aAAgBAQAAAADwawLpMspcK7qrlE5F0Vtul2bVywMUNeBHUkW/bmxvYELGuNjh2VDvixxo5ViljKjDRMoahCULjs2JCShjhjh2OGxo0Y2MoXHOLszsKLhw7tD99mpZQxj8xceofmLEKFwXLTIyHwY1Ls+iEotjHY0M0pjRYxtGj4VFKLPohQlFQyy4Qipc0XG9pS+CP/2Q==",
-#   "txnId": "a825f76b-0696-40f3-864c-5a3a5b389a83"
-# }
 class CreateHealthIdSerializer(Serializer):
     healthId = CharField(
         max_length=64,
@@ -193,6 +172,6 @@ class CreateHealthIdSerializer(Serializer):
         help_text="PreVerified Transaction ID",
         validators=[],
     )
-    patientId = CharField(
+    patientId = UUIDField(
         required=False, help_text="Patient ID to be linked", validators=[]
-    )  # TODO: Add UUID Validation
+    )
