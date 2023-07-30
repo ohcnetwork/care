60cdc318f69629749c12eb49d3cebbf9e02b98c3
Mathew Alex <matthewzalex@gmail.com>
2023-07-30 23:45:42 +0530

    migration lambda test

--- migrations_handler.py
+++ migrations_handler.py
@@ -1,33 +1,38 @@
 from django.core.management import call_command
+import django
+import os
 from django.conf import settings
-import psycopg2
-from psycopg2 import sql
-from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT # <-- ADD THIS LINE
+import psycopg
+from psycopg import sql
 
 
 # define the command options
-options = {
-    'verbosity': 1,   # adjust verbosity as needed
-    'interactive': False  # set to True if you want to prompt for user input
-}
+# options = {
+#     'verbosity': 1,   # adjust verbosity as needed
+#     'interactive': False  # set to True if you want to prompt for user input
+# }
 
-def handler():
-    con = psycopg2.connect(dbname='postgres',
-                           user=settings.POSTGRES_USER,
-                           host=settings.POSTGRES_HOST,
-                           password = settings.POSTGRES_PASSWORD)
-
-    con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)  # <-- ADD THIS LINE
+def handler(*args, **kwargs):
+    con = psycopg.connect(dbname=settings.POSTGRES_DB,
+                        user=settings.POSTGRES_USER,
+                        host=settings.POSTGRES_HOST,
+                        password=settings.POSTGRES_PASSWORD,
+                        autocommit=True
+                        )
 
     cur = con.cursor()
 
     # Use the psycopg2.sql module instead of string concatenation
     # in order to avoid sql injection attacks.
-    cur.execute(sql.SQL("IF NOT EXISTS CREATE DATABASE {}").format(
-        sql.Identifier(settings.POSTGRES_DB))
-    )
+    try:
+        cur.execute(sql.SQL("CREATE DATABASE {};").format(
+            sql.Identifier('previewlocal'))
+        )
+    except psycopg.errors.DuplicateDatabase:
+        pass
 
-    call_command('migrate', options)
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
 
-
+    django.setup()
+    call_command('migrate')
 
