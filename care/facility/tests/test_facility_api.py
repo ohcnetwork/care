d206c24437c556ffa967bbc151646deea81bfc39
Suyash Singh <suyashsingh.stem@gmail.com>
2023-08-04 01:04:31 +0530

    Add facility module test cases

--- care/facility/tests/test_facility_api.py
+++ care/facility/tests/test_facility_api.py
@@ -1,11 +1,178 @@
-from django.test import TestCase
+from enum import Enum
+
 from rest_framework import status
+from rest_framework.test import APIRequestFactory
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from care.facility.api.viewsets.facility import AllFacilityViewSet, FacilityViewSet
 from care.facility.tests.mixins import TestClassMixin
+from care.utils.tests.test_base import TestBase
 
 
-class FacilityTests(TestClassMixin, TestCase):
+class ExpectedLocalBodyObject(Enum):
+    ID = "id"
+    NAME = "name"
+    BODY_TYPE = "body_type"
+    LOCALBODY_CODE = "localbody_code"
+    DISTRICT = "district"
+
+
+class ExpectedFacilityListKeys(Enum):
+    ID = "id"
+    NAME = "name"
+    MODIFIED_DATE = "modified_date"
+    CREATED_DATE = "created_date"
+    KASP_EMPANELLED = "kasp_empanelled"
+    FACILITY_TYPE = "facility_type"
+    FEATURES = "features"
+    LOCAL_BODY_OBJECT = "local_body_object"
+    PHONE_NUMBER = "phone_number"
+    READ_COVER_IMAGE_URL = "read_cover_image_url"
+    PATIENT_COUNT = "patient_count"
+    BED_COUNT = "bed_count"
+
+
+class ExpectedWardKeys(Enum):
+    ID = "id"
+    NAME = "name"
+    NUMBER = "number"
+    LOCAL_BODY = "local_body"
+
+
+class ExpectedDistrictKeys(Enum):
+    ID = "id"
+    NAME = "name"
+    STATE = "state"
+
+
+class ExpectedStateKeys(Enum):
+    ID = "id"
+    NAME = "name"
+
+
+class ExpectedFacilityRetrieveKeys(Enum):
+    ID = "id"
+    NAME = "name"
+    WARD = "ward"
+    LOCAL_BODY = "local_body"
+    DISTRICT = "district"
+    STATE = "state"
+    FACILITY_TYPE = "facility_type"
+    ADDRESS = "address"
+    LONGITUDE = "longitude"
+    LATITUDE = "latitude"
+    FEATURES = "features"
+    PINCODE = "pincode"
+    OXYGEN_CAPACITY = "oxygen_capacity"
+    PHONE_NUMBER = "phone_number"
+    WARD_OBJECT = "ward_object"
+    LOCAL_BODY_OBJECT = "local_body_object"
+    DISTRICT_OBJECT = "district_object"
+    STATE_OBJECT = "state_object"
+    MODIFIED_DATE = "modified_date"
+    CREATED_DATE = "created_date"
+    KASP_EMPANELLED = "kasp_empanelled"
+    MIDDLEWARE_ADDRESS = "middleware_address"
+    EXPECTED_OXYGEN_REQUIREMENT = "expected_oxygen_requirement"
+    TYPE_B_CYLINDERS = "type_b_cylinders"
+    TYPE_C_CYLINDERS = "type_c_cylinders"
+    TYPE_D_CYLINDERS = "type_d_cylinders"
+    EXPECTED_TYPE_B_CYLINDERS = "expected_type_b_cylinders"
+    EXPECTED_TYPE_C_CYLINDERS = "expected_type_c_cylinders"
+    EXPECTED_TYPE_D_CYLINDERS = "expected_type_d_cylinders"
+    READ_COVER_IMAGE_URL = "read_cover_image_url"
+    PATIENT_COUNT = "patient_count"
+    BED_COUNT = "bed_count"
+
+
+class FacilityTests(TestClassMixin, TestBase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.factory = APIRequestFactory()
+
+    def setUp(self):
+        # Refresh token to header
+        refresh_token = RefreshToken.for_user(self.user)
+        self.client.credentials(
+            HTTP_AUTHORIZATION=f"Bearer {refresh_token.access_token}"
+        )
+
+    def test_list_facilities(self):
+        response = self.client.get("/api/v1/facility/")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIsInstance(response.json()["results"], list)
+
+        # Ensure only necessary data is being sent and no extra data
+        expected_keys = [key.value for key in ExpectedFacilityListKeys]
+        data = response.json()["results"][0]
+        self.assertCountEqual(data.keys(), expected_keys)
+
+        local_body_object_keys = [key.value for key in ExpectedLocalBodyObject]
+
+        # if data["local_body_object"] is not None:
+        self.assertCountEqual(data["local_body_object"].keys(), local_body_object_keys)
+
+        # Ensure the data is coming in correctly
+        data = response.json()["results"][0]
+        self.assertIsInstance(data["id"], str)
+        self.assertIsInstance(data["name"], str)
+        self.assertIsInstance(data["modified_date"], str)
+        self.assertIsInstance(data["created_date"], str)
+        self.assertIsInstance(data["kasp_empanelled"], bool)
+        self.assertIsInstance(data["facility_type"], str)
+        self.assertIsInstance(data["features"], list)
+        self.assertIsInstance(data["local_body_object"]["id"], int)
+        self.assertIsInstance(data["local_body_object"]["name"], str)
+        self.assertIsInstance(data["local_body_object"]["body_type"], int)
+        self.assertIsInstance(data["local_body_object"]["localbody_code"], str)
+        self.assertIsInstance(data["local_body_object"]["district"], int)
+        self.assertIsInstance(data["phone_number"], str)
+        self.assertIsNone(data["read_cover_image_url"])
+        self.assertIsInstance(data["patient_count"], int)
+        self.assertIsInstance(data["bed_count"], int)
+
+    def test_retrieve_facility(self):
+        response = self.client.get(f"/api/v1/facility/{self.facility.external_id}/")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIsInstance(response.json(), dict)
+
+        # Ensure only necessary data is being sent and no extra data
+        expected_keys = [key.value for key in ExpectedFacilityRetrieveKeys]
+        data = response.json()
+        self.assertCountEqual(data.keys(), expected_keys)
+
+        ward_object_keys = [key.value for key in ExpectedWardKeys]
+        self.assertCountEqual(data["ward_object"].keys(), ward_object_keys)
+
+        local_body_object_keys = [key.value for key in ExpectedLocalBodyObject]
+        self.assertCountEqual(data["local_body_object"].keys(), local_body_object_keys)
+
+        district_object_keys = [key.value for key in ExpectedDistrictKeys]
+        self.assertCountEqual(data["district_object"].keys(), district_object_keys)
+
+        state_object_keys = [key.value for key in ExpectedStateKeys]
+        self.assertCountEqual(data["state_object"].keys(), state_object_keys)
+
+        # Ensure the data is coming in correctly
+        data = response.json()
+        self.assertIsInstance(data["id"], str)
+        self.assertIsInstance(data["name"], str)
+        self.assertIsInstance(data["ward"], int)
+        self.assertIsInstance(data["local_body"], int)
+        self.assertIsInstance(data["district"], int)
+        self.assertIsInstance(data["state"], int)
+        self.assertIsInstance(data["facility_type"], str)
+        self.assertIsInstance(data["address"], str)
+        self.assertIsNone(data["longitude"])
+        self.assertIsNone(data["latitude"])
+        self.assertIsInstance(data["features"], list)
+        self.assertIsInstance(data["pincode"], int)
+        self.assertIsInstance(data["oxygen_capacity"], int)
+        self.assertIsInstance(data["phone_number"], str)
+
     def test_all_listing(self):
         response = self.new_request(
             ("/api/v1/getallfacilitiess/",), {"get": "list"}, AllFacilityViewSet
@@ -14,12 +181,12 @@ class FacilityTests(TestClassMixin, TestCase):
 
     def test_listing(self):
         response = self.new_request(
-            ("/api/v1/facility/",), {"get": "list"}, FacilityViewSet, self.users[0]
+            ("/api/v1/facility/",), {"get": "list"}, FacilityViewSet, self.user
         )
         self.assertIs(response.status_code, status.HTTP_200_OK)
 
     def test_create(self):
-        user = self.users[0]
+        user = self.user
         sample_data = {
             "name": "Hospital X",
             "ward": user.ward.pk,
--- care/utils/tests/test_base.py
+++ care/utils/tests/test_base.py
@@ -22,6 +22,7 @@ from care.facility.models import (
     PatientNotes,
     PatientRegistration,
     User,
+    Ward,
 )
 from care.users.models import District, State
 from config.tests.helper import EverythingEquals, mock_equal
@@ -35,6 +36,12 @@ class TestBase(APITestCase):
     maxDiff = None
 
     @classmethod
+    def create_ward(cls, local_body: LocalBody, **kwargs) -> Ward:
+        return Ward.objects.create(
+            local_body=local_body, name="Ward", number=1, **kwargs
+        )
+
+    @classmethod
     def create_user(cls, district: District, username: str = "user", **kwargs) -> User:
         data = {
             "email": f"{username}@somedomain.com",
@@ -46,6 +53,8 @@ class TestBase(APITestCase):
             "password": "bar",
             "district": district,
             "user_type": User.TYPE_VALUE_MAP["Staff"],
+            "ward": cls.ward,
+            "local_body": cls.local_body,
         }
         data.update(kwargs)
         return User.objects.create_user(**data)
@@ -82,6 +91,15 @@ class TestBase(APITestCase):
         return State.objects.create(name=f"State{datetime.datetime.now().timestamp()}")
 
     @classmethod
+    def create_local_body(cls) -> LocalBody:
+        return LocalBody.objects.create(
+            name=f"LocalBody{datetime.datetime.now().timestamp()}",
+            body_type=1,
+            localbody_code="G071401",
+            district=cls.district,
+        )
+
+    @classmethod
     def create_facility(
         cls, district: District, user: User = None, **kwargs
     ) -> Facility:
@@ -95,6 +113,8 @@ class TestBase(APITestCase):
             "oxygen_capacity": 10,
             "phone_number": "9998887776",
             "created_by": user,
+            "ward": cls.ward,
+            "local_body": cls.local_body,
         }
         data.update(kwargs)
         f = Facility(**data)
@@ -228,8 +248,10 @@ class TestBase(APITestCase):
         super(TestBase, cls).setUpClass()
         cls.state = cls.create_state()
         cls.district = cls.create_district(cls.state)
+        cls.local_body = cls.create_local_body()
+        cls.ward = cls.create_ward(cls.local_body)
         cls.user_type = User.TYPE_VALUE_MAP["Staff"]
-        cls.user = cls.create_user(cls.district)
+        cls.user = cls.create_user(cls.district, cls.ward)
         cls.super_user = cls.create_super_user(district=cls.district)
         cls.facility = cls.create_facility(cls.district)
         cls.patient = cls.create_patient()
