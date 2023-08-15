1240b65b95bdce76b02b491b3b5dbcad5df15be8
Pranshu Aggarwal <pranshu1902@gmail.com>
2023-08-15 20:04:03 +0530

    switched to positiveintegerfield

--- care/facility/models/facility.py
+++ care/facility/models/facility.py
@@ -268,7 +268,7 @@ class HospitalDoctors(FacilityBaseModel, FacilityRelatedPermissionMixin):
         "Facility", on_delete=models.CASCADE, null=False, blank=False
     )
     area = models.IntegerField(choices=DOCTOR_TYPES)
-    count = models.IntegerField(default=0, validators=[MinValueValidator(0)])
+    count = models.PositiveIntegerField()
 
     def __str__(self):
         return str(self.facility) + str(self.count)
