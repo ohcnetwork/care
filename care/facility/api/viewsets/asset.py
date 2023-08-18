5383974ff62d896c30ea365171581f998762bde2
Aakash Singh <mail@singhaakash.dev>
2023-08-18 12:07:34 +0530

    filter permanent assets
    Signed-off-by: Aakash Singh <mail@singhaakash.dev>

--- care/facility/api/viewsets/asset.py
+++ care/facility/api/viewsets/asset.py
@@ -113,19 +113,30 @@ class AssetFilter(filters.FilterSet):
     in_use_by_consultation = filters.BooleanFilter(
         method="filter_in_use_by_consultation"
     )
+    is_permanent = filters.BooleanFilter(method="filter_is_permanent")
 
     def filter_in_use_by_consultation(self, queryset, name, value):
-        if value is not None:
-            if value:
-                return queryset.filter(
-                    assigned_consultation_beds__end_date__isnull=True
-                ).distinct()
-            else:
-                return queryset.filter(
-                    Q(assigned_consultation_beds__isnull=True)
-                    | Q(assigned_consultation_beds__end_date__isnull=False)
-                ).distinct()
-        return queryset
+        if value is None:
+            return queryset
+        if value:
+            return queryset.filter(assigned_consultation_beds__end_date__isnull=True)
+        else:
+            return queryset.filter(
+                Q(assigned_consultation_beds__isnull=True)
+                | Q(assigned_consultation_beds__end_date__isnull=False)
+            )
+
+    def filter_is_permanent(self, queryset, name, value):
+        if value is None:
+            return queryset
+        if value:
+            return queryset.filter(
+                asset_class__in=[AssetClasses.ONVIF, AssetClasses.HL7MONITOR]
+            )
+        else:
+            return queryset.exclude(
+                asset_class__in=[AssetClasses.ONVIF, AssetClasses.HL7MONITOR]
+            )
 
 
 class AssetPublicViewSet(GenericViewSet):
