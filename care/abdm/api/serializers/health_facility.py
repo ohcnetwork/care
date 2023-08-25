5f733c7496405b5a1bf47fd8042dfe928b9a0f2c
khavinshankar <khavinshankar@gmail.com>
2023-08-25 15:07:01 +0530

    use external_id instead of id

--- care/abdm/api/serializers/health_facility.py
+++ care/abdm/api/serializers/health_facility.py
@@ -4,6 +4,8 @@ from care.abdm.models import HealthFacility
 
 
 class HealthFacilitySerializer(serializers.ModelSerializer):
+    id = serializers.CharField(source="external_id", read_only=True)
+
     class Meta:
         model = HealthFacility
         exclude = ("deleted",)
