0eb7f645f7c77d36b4017b1dfc0bf92cb8dd3a05
khavinshankar <khavinshankar@gmail.com>
2023-08-31 13:26:20 +0530

    send external_id as id in consent serializer

--- care/abdm/api/serializers/consent.py
+++ care/abdm/api/serializers/consent.py
@@ -5,8 +5,9 @@ from care.users.api.serializers.user import UserBaseMinimumSerializer
 
 
 class ConsentSerializer(serializers.ModelSerializer):
+    id = serializers.CharField(source="external_id", read_only=True)
     requester = UserBaseMinimumSerializer(read_only=True)
 
     class Meta:
         model = Consent
-        exclude = ("deleted",)
+        exclude = ("deleted", "external_id")
