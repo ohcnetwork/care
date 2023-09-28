391582257b10d449372195932bcb43e424424439
rithviknishad <mail@rithviknishad.dev>
2023-09-28 14:01:32 +0530

    add docs

--- care/facility/models/meta_hub_spokes.py
+++ care/facility/models/meta_hub_spokes.py
@@ -2,6 +2,11 @@ from django.db import models
 
 
 class MetaHubSpokes(models.Model):
+    """
+    Not for production use. For Metabase purposes only.
+    Do not build relations to this model.
+    """
+
     state = models.CharField(max_length=255)
     hub = models.CharField(max_length=255, help_text="Hub hospital name")
     spoke = models.CharField(max_length=255, help_text="Spoke hospital name")
