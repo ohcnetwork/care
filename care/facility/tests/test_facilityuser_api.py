5428d66032a13d072ec950668bb4f25a44730a1b
Pranshu Aggarwal <70687348+Pranshu1902@users.noreply.github.com>
2023-10-31 00:42:30 +0530

    Filter out those facilities that are already linked to user (#1588)
    * template methos
    
    * filter unlinked facilities
    
    * rename params
    
    * add tests
    
    * fix import
    
    * use facilityfilter
    
    * refactor
    
    * Update care/facility/api/viewsets/facility.py
    
    Co-authored-by: Aakash Singh <mail@singhaakash.dev>
    
    * Update care/facility/api/viewsets/facility.py
    
    Co-authored-by: Aakash Singh <mail@singhaakash.dev>
    
    * fix errors
    
    * fix tests
    
    * fix lint ci
    
    * fix test cases
    
    * fix filter
    
    ---------
    
    Co-authored-by: Aakash Singh <mail@singhaakash.dev>

--- care/facility/tests/test_facilityuser_api.py
+++ care/facility/tests/test_facilityuser_api.py
@@ -1,7 +1,6 @@
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from care.users.models import Skill
 from care.utils.tests.test_utils import TestUtils
 
 
@@ -14,9 +13,16 @@ class FacilityUserTest(TestUtils, APITestCase):
         cls.super_user = cls.create_super_user("su", cls.district)
         cls.facility = cls.create_facility(cls.super_user, cls.district, cls.local_body)
         cls.user = cls.create_user("staff", cls.district, home_facility=cls.facility)
-        cls.skill1 = Skill.objects.create(name="Skill 1")
-        cls.skill2 = Skill.objects.create(name="Skill 2")
-        cls.user.skills.add(cls.skill1, cls.skill2)
+
+        cls.facility1 = cls.create_facility(
+            cls.super_user, cls.district, cls.local_body
+        )
+        cls.facility2 = cls.create_facility(
+            cls.super_user, cls.district, cls.local_body
+        )
+
+    def setUp(self) -> None:
+        self.client.force_authenticate(self.super_user)
 
     def test_get_queryset_with_prefetching(self):
         response = self.client.get(
@@ -25,3 +31,17 @@ class FacilityUserTest(TestUtils, APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertNumQueries(2)
+
+    def test_link_new_facility(self):
+        response = self.client.get("/api/v1/facility/")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data["results"]), 3)
+
+    def test_link_existing_facility(self):
+        response = self.client.get(
+            f"/api/v1/facility/?exclude_user={self.user.username}"
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data["results"]), 2)
