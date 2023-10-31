451ef67ef360d857ea59d4e00dd0258d3482a422
rithviknishad <mail@rithviknishad.dev>
2023-10-31 17:33:40 +0530

    add tests

--- care/facility/tests/test_patient_consultation_api.py
+++ care/facility/tests/test_patient_consultation_api.py
@@ -67,6 +67,16 @@ class TestPatientConsultation(TestUtils, APITestCase):
     def update_consultation(self, consultation, **kwargs):
         return self.client.patch(self.get_url(consultation), kwargs, "json")
 
+    def add_diagnosis(self, consultation, **kwargs):
+        return self.client.post(
+            f"{self.get_url(consultation)}diagnoses/", kwargs, "json"
+        )
+
+    def edit_diagnosis(self, consultation, id, **kwargs):
+        return self.client.patch(
+            f"{self.get_url(consultation)}diagnoses/{id}/", kwargs, "json"
+        )
+
     def discharge(self, consultation, **kwargs):
         return self.client.post(
             f"{self.get_url(consultation)}discharge_patient/", kwargs, "json"
@@ -318,3 +328,113 @@ class TestPatientConsultation(TestUtils, APITestCase):
             consultation, symptoms=[1, 2], category="MILD", suggestion="A"
         )
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_add_diagnoses_and_duplicate_diagnoses(self):
+        consultation = self.create_admission_consultation(
+            suggestion="A",
+            admission_date=make_aware(datetime.datetime(2020, 4, 1, 15, 30, 00)),
+        )
+        diagnosis = ICD11Diagnosis.objects.all()[0].id
+        res = self.add_diagnosis(
+            consultation,
+            diagnosis=diagnosis,
+            is_principal=True,
+            verification_status=ConditionVerificationStatus.CONFIRMED,
+        )
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        res = self.add_diagnosis(
+            consultation,
+            diagnosis=diagnosis,
+            is_principal=True,
+            verification_status=ConditionVerificationStatus.PROVISIONAL,
+        )
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_add_diagnosis_inactive(self):
+        consultation = self.create_admission_consultation(
+            suggestion="A",
+            admission_date=make_aware(datetime.datetime(2020, 4, 1, 15, 30, 00)),
+        )
+        diagnosis = ICD11Diagnosis.objects.first().id
+        res = self.add_diagnosis(
+            consultation,
+            diagnosis=diagnosis,
+            is_principal=False,
+            verification_status=ConditionVerificationStatus.ENTERED_IN_ERROR,
+        )
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        res = self.add_diagnosis(
+            consultation,
+            diagnosis=diagnosis,
+            is_principal=True,
+            verification_status=ConditionVerificationStatus.ENTERED_IN_ERROR,
+        )
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_change_diagnosis(self):
+        consultation = self.create_admission_consultation(
+            suggestion="A",
+            admission_date=make_aware(datetime.datetime(2020, 4, 1, 15, 30, 00)),
+        )
+        res = self.add_diagnosis(
+            consultation,
+            diagnosis=ICD11Diagnosis.objects.all()[0].id,
+            is_principal=False,
+            verification_status=ConditionVerificationStatus.CONFIRMED,
+        )
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        res = self.edit_diagnosis(
+            consultation,
+            res.data["id"],
+            diagnosis=ICD11Diagnosis.objects.all()[1].id,
+            is_principal=True,
+            verification_status=ConditionVerificationStatus.PROVISIONAL,
+        )
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_add_multiple_principal_diagnosis(self):
+        consultation = self.create_admission_consultation(
+            suggestion="A",
+            admission_date=make_aware(datetime.datetime(2020, 4, 1, 15, 30, 00)),
+        )
+        res = self.add_diagnosis(
+            consultation,
+            diagnosis=ICD11Diagnosis.objects.all()[0].id,
+            is_principal=True,
+            verification_status=ConditionVerificationStatus.CONFIRMED,
+        )
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        res = self.add_diagnosis(
+            consultation,
+            diagnosis=ICD11Diagnosis.objects.all()[1].id,
+            is_principal=True,
+            verification_status=ConditionVerificationStatus.PROVISIONAL,
+        )
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_add_principal_edit_as_inactive_add_principal(self):
+        consultation = self.create_admission_consultation(
+            suggestion="A",
+            admission_date=make_aware(datetime.datetime(2020, 4, 1, 15, 30, 00)),
+        )
+        res = self.add_diagnosis(
+            consultation,
+            diagnosis=ICD11Diagnosis.objects.all()[0].id,
+            is_principal=True,
+            verification_status=ConditionVerificationStatus.CONFIRMED,
+        )
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        res = self.edit_diagnosis(
+            consultation,
+            res.data["id"],
+            verification_status=ConditionVerificationStatus.ENTERED_IN_ERROR,
+        )
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertFalse(res.data["is_principal"])
+        res = self.add_diagnosis(
+            consultation,
+            diagnosis=ICD11Diagnosis.objects.all()[1].id,
+            is_principal=True,
+            verification_status=ConditionVerificationStatus.PROVISIONAL,
+        )
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
