5ef0c9bc71b1a3c9ed42a34d0de5086239bd9d6c
Pranshu Aggarwal <pranshu1902@gmail.com>
2023-11-03 15:59:59 +0530

    fix lint

--- care/users/api/viewsets/users.py
+++ care/users/api/viewsets/users.py
@@ -282,7 +282,9 @@ class UserViewSet(
             raise ValidationError({"facility": "Cannot Delete User's Home Facility"})
         # ensure that district admin only able to delete in the same district
         if user.district_id != requesting_user.district_id:
-            raise ValidationError({"facility": "Cannot delete User's Facility from other district"})
+            raise ValidationError(
+                {"facility": "Cannot delete User's Facility from other district"}
+            )
         FacilityUser.objects.filter(facility=facility, user=user).delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
