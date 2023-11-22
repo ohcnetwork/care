01be9b48d3a9b208ff5a0ca06e9b9d4f1ecb7ebf
khavinshankar <khavinshankar@gmail.com>
2023-11-22 16:39:21 +0530

    fix tests

--- care/facility/tests/test_asset_operate_api.py
+++ care/facility/tests/test_asset_operate_api.py
@@ -1,50 +1,35 @@
 from rest_framework import status
-from rest_framework.test import APIRequestFactory, APITestCase
+from rest_framework.test import APITestCase
 
-from care.facility.api.viewsets.asset import AssetViewSet
-from care.facility.models import Asset, AssetBed, AssetLocation, Bed, FacilityUser
-from care.facility.tests.mixins import TestClassMixin
-from care.utils.tests.test_base import TestBase
+from care.facility.models import AssetBed
+from care.utils.tests.test_utils import TestUtils
 
 
-class AssetViewSetTestCase(TestBase, TestClassMixin, APITestCase):
-    asset_id = None
-
-    def setUp(self):
-        self.factory = APIRequestFactory()
-        state = self.create_state()
-        district = self.create_district(state=state)
-        self.user = self.create_user(district=district, username="test user")
-        self.user_2 = self.create_user(district=district, username="test user 2")
-        facility = self.create_facility(district=district, user=self.user)
-        self.facility_user = FacilityUser.objects.create(
-            user=self.user_2, facility=facility, created_by=self.user
+class AssetViewSetTestCase(TestUtils, APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.state = cls.create_state()
+        cls.district = cls.create_district(state=cls.state)
+        cls.local_body = cls.create_local_body(cls.district)
+        cls.user = cls.create_user(district=cls.district, username="test user")
+        cls.facility = cls.create_facility(
+            district=cls.district, local_body=cls.local_body, user=cls.user
         )
-        self.asset1_location = AssetLocation.objects.create(
-            name="asset1 location", location_type=1, facility=facility
-        )
+        cls.asset1_location = cls.create_asset_location(facility=cls.facility)
 
         # depends upon the operational dev camera config
-        self.onvif_meta = {
+        cls.onvif_meta = {
             "asset_type": "CAMERA",
             "local_ip_address": "192.168.1.64",
             "camera_access_key": "remote_user:2jCkrCRSeahzKEU:d5694af2-21e2-4a39-9bad-2fb98d9818bd",
             "middleware_hostname": "dev_middleware.coronasafe.live",
         }
-        self.hl7monitor_meta = {}
-        self.ventilator_meta = {}
-        self.bed = Bed.objects.create(
-            name="Test Bed",
-            facility=facility,
-            location=self.asset1_location,
-            meta={},
-            bed_type=1,
+        cls.hl7monitor_meta = {}
+        cls.ventilator_meta = {}
+        cls.bed = cls.create_bed(
+            facility=cls.facility, location=cls.asset1_location, meta={}
         )
-        self.asset: Asset = Asset.objects.create(
-            name="Test Asset",
-            current_location=self.asset1_location,
-            asset_type=50,
-        )
+        cls.asset = cls.create_asset(location=cls.asset1_location)
 
     def test_onvif_relative_move(self):
         self.asset.asset_class = "ONVIF"
@@ -74,16 +59,10 @@ class AssetViewSetTestCase(TestBase, TestClassMixin, APITestCase):
                 },
             }
         }
-        response = self.new_request(
-            (
-                f"/api/v1/asset/{self.asset.external_id}/operate_assets/",
-                sample_data,
-                "json",
-            ),
-            {"post": "operate_assets"},
-            AssetViewSet,
-            self.user,
-            {"external_id": self.asset.external_id},
+        response = self.client.post(
+            f"/api/v1/asset/{self.asset.external_id}/operate_assets/",
+            sample_data,
+            format="json",
         )
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -100,16 +79,10 @@ class AssetViewSetTestCase(TestBase, TestClassMixin, APITestCase):
                 },
             }
         }
-        response_invalid = self.new_request(
-            (
-                f"/api/v1/asset/{self.asset.external_id}/operate_assets/",
-                sample_data_invald,
-                "json",
-            ),
-            {"post": "operate_assets"},
-            AssetViewSet,
-            self.user,
-            {"external_id": self.asset.external_id},
+        response_invalid = self.client.post(
+            f"/api/v1/asset/{self.asset.external_id}/operate_assets/",
+            sample_data_invald,
+            "json",
         )
 
         self.assertEqual(response_invalid.status_code, status.HTTP_400_BAD_REQUEST)
@@ -128,73 +101,3 @@ class AssetViewSetTestCase(TestBase, TestClassMixin, APITestCase):
         self.asset.meta = self.ventilator_meta
         self.asset.save()
         pass
-
-    def test_lock_asset(self):
-        self.asset.asset_class = "ONVIF"
-        self.asset.meta = self.onvif_meta
-        self.asset.save()
-        sample_data_lock = {
-            "action": {
-                "type": "lock_asset",
-                "data": {},
-            }
-        }
-        sample_data_unlock = {
-            "action": {
-                "type": "unlock_asset",
-                "data": {},
-            }
-        }
-        response = self.new_request(
-            (
-                f"/api/v1/asset/{self.asset.external_id}/operate_assets/",
-                sample_data_lock,
-                "json",
-            ),
-            {"post": "operate_assets"},
-            AssetViewSet,
-            self.user,
-            {"external_id": self.asset.external_id},
-        )
-
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-        response = self.new_request(
-            (
-                f"/api/v1/asset/{self.asset.external_id}/operate_assets/",
-                sample_data_unlock,
-                "json",
-            ),
-            {"post": "operate_assets"},
-            AssetViewSet,
-            self.user_2,
-            {"external_id": self.asset.external_id},
-        )
-        self.assertEqual(response.status_code, status.HTTP_409_CONFLICT)
-
-        response = self.new_request(
-            (
-                f"/api/v1/asset/{self.asset.external_id}/operate_assets/",
-                sample_data_unlock,
-                "json",
-            ),
-            {"post": "operate_assets"},
-            AssetViewSet,
-            self.user,
-            {"external_id": self.asset.external_id},
-        )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-        response = self.new_request(
-            (
-                f"/api/v1/asset/{self.asset.external_id}/operate_assets/",
-                sample_data_lock,
-                "json",
-            ),
-            {"post": "operate_assets"},
-            AssetViewSet,
-            self.user_2,
-            {"external_id": self.asset.external_id},
-        )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.asset.delete()
