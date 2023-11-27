9120efdd182d81be54016ead62d058720cbeabdd
rithviknishad <mail@rithviknishad.dev>
2023-11-27 15:00:33 +0530

    update migration

--- care/facility/migrations/0397_rename_admission_date_to_encounter_date_and_more.py
+++ care/facility/migrations/0397_rename_admission_date_to_encounter_date_and_more.py
@@ -12,6 +12,36 @@ class Migration(migrations.Migration):
 
     def clean_encounter_date(apps, schema_editor):
         PatientConsultation = apps.get_model("facility", "PatientConsultation")
+
+        # Set `encounter_date` to `created_date` for all consultations with
+        # suggestions that are not Admission, Domiciliary Care.
+        #
+        # Reason:
+        # We were not capturing `admission_date` (now `encounter_date`) for
+        # these consultations earlier, but could still be set by mistake.
+        #
+        # Example:
+        # Edit Consultation allowed changing suggestion to non Admission/Domiciliary Care
+        # without clearing `admission_date` (now `encounter_date`).
+        PatientConsultation.objects.exclude(suggestion__in=["A", "DC"]).update(
+            encounter_date=F("created_date")
+        )
+
+        # Set `encounter_date` to `created_date` for all consultations with
+        # `encounter_date` as `null`.
+        #
+        # Reason:
+        # We were not capturing `encounter_date` for these consultations as
+        # it was not set for these.
+        PatientConsultation.objects.filter(encounter_date=None).update(
+            encounter_date=F("created_date")
+        )
+
+    def reverse_clean_encounter_date(apps, schema_editor):
+        PatientConsultation = apps.get_model("facility", "PatientConsultation")
+
+        # Set `encounter_date` to `null` for all consultations with suggestions
+        # that are not Admission/Domiciliary Care.
         PatientConsultation.objects.exclude(suggestion__in=["A", "DC"]).update(
             encounter_date=None
         )
@@ -28,7 +58,7 @@ class Migration(migrations.Migration):
         ),
         migrations.RunPython(
             clean_encounter_date,
-            reverse_code=migrations.RunPython.noop,
+            reverse_code=reverse_clean_encounter_date,
         ),
         migrations.AlterField(
             model_name="patientconsultation",
