292347017cc797a3d118dd337321099983d36fbb
Pranshu1902 <pranshu1902@gmail.com>
2023-12-05 15:41:23 +0530

    check if user is from same district

--- care/facility/api/viewsets/patient_external_test.py
+++ care/facility/api/viewsets/patient_external_test.py
@@ -139,6 +139,13 @@ class PatientExternalTestViewSet(
         # csv_file = request.FILES[list(request.FILES.keys())[0]]
         # csv_file.seek(0)
         # reader = csv.DictReader(io.StringIO(csv_file.read().decode("utf-8-sig")))
+
+        # check if the user is from same district
+        user = User.objects.filter(username=request.user).first()
+        for data in request.data["sample_tests"]:
+            if user.district != data["district"]:
+                raise ValidationError({"Error": "User must belong to same district"})
+
         if "sample_tests" not in request.data:
             raise ValidationError({"sample_tests": "No Data was provided"})
         if not isinstance(request.data["sample_tests"], list):
