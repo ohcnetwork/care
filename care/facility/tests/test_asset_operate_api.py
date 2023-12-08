ccf784f02e5d574e38e9f11d062d83d5eacf9487
Pranshu1902 <pranshu1902@gmail.com>
2023-12-08 15:04:40 +0530

    mock middleware tests

--- care/facility/tests/test_asset_operate_api.py
+++ care/facility/tests/test_asset_operate_api.py
@@ -1,4 +1,7 @@
+from unittest.mock import MagicMock, patch
+
 from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.test import APITestCase
 
 from care.facility.models import AssetBed
@@ -22,7 +25,7 @@ class AssetViewSetTestCase(TestUtils, APITestCase):
             "asset_type": "CAMERA",
             "local_ip_address": "192.168.1.64",
             "camera_access_key": "remote_user:2jCkrCRSeahzKEU:d5694af2-21e2-4a39-9bad-2fb98d9818bd",
-            "middleware_hostname": "dev_middleware.coronasafe.live",
+            "middleware_hostname": "mock_middleware",
         }
         cls.hl7monitor_meta = {}
         cls.ventilator_meta = {}
@@ -31,7 +34,37 @@ class AssetViewSetTestCase(TestUtils, APITestCase):
         )
         cls.asset = cls.create_asset(location=cls.asset1_location)
 
-    def test_onvif_relative_move(self):
+    @patch("care.facility.api.viewsets.asset.AssetViewSet.operate_assets")
+    def test_onvif_relative_move(self, mock_asset):
+        # Set up your mock Asset instance
+        asset_instance = MagicMock()
+        asset_instance.meta = {"middleware_hostname": "mock_hostname"}
+        asset_instance.current_location.middleware_address = "mock_middleware_address"
+        mock_asset.objects.get.return_value = asset_instance
+
+        def validate_action(data):
+            boundary_range = boundary_asset_bed.meta.get("range", None)
+            camera_state = data["action"]["data"]["camera_state"]
+            action_data = data["action"]["data"]
+
+            if (
+                (camera_state["x"] + action_data["x"] < boundary_range["min_x"])
+                or (camera_state["x"] + action_data["x"] > boundary_range["max_x"])
+                or (camera_state["y"] + action_data["y"] < boundary_range["min_y"])
+                or (camera_state["y"] + action_data["y"] > boundary_range["max_y"])
+            ):
+                mock_asset.return_value = Response(
+                    {
+                        "result": "mock_result",
+                        "message": {"action": {"code": "invalid"}},
+                    },
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+            else:
+                mock_asset.return_value = Response(
+                    {"result": "mock_result"}, status=status.HTTP_200_OK
+                )
+
         self.asset.asset_class = "ONVIF"
         self.asset.meta = self.onvif_meta
         self.asset.save()
@@ -47,6 +80,7 @@ class AssetViewSetTestCase(TestUtils, APITestCase):
                 }
             },
         )
+
         sample_data = {
             "action": {
                 "type": "relative_move",
@@ -59,6 +93,8 @@ class AssetViewSetTestCase(TestUtils, APITestCase):
                 },
             }
         }
+
+        validate_action(sample_data)
         response = self.client.post(
             f"/api/v1/asset/{self.asset.external_id}/operate_assets/",
             sample_data,
@@ -79,6 +115,8 @@ class AssetViewSetTestCase(TestUtils, APITestCase):
                 },
             }
         }
+
+        validate_action(sample_data_invald)
         response_invalid = self.client.post(
             f"/api/v1/asset/{self.asset.external_id}/operate_assets/",
             sample_data_invald,
@@ -87,7 +125,8 @@ class AssetViewSetTestCase(TestUtils, APITestCase):
 
         self.assertEqual(response_invalid.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(
-            response_invalid.data.get("message", {}).get("action", {}).code, "invalid"
+            response_invalid.data.get("message", {}).get("action", {})["code"],
+            "invalid",
         )
 
     def test_hl7monitor(self):
