3dda74ec78e9b56333031e17966e2005f2872adf
rithviknishad <mail@rithviknishad.dev>
2023-12-12 12:12:11 +0530

    rebase migrations (again :/)

--- care/facility/migrations/0403_rename_admission_date_to_encounter_date_and_more.py
+++ care/facility/migrations/0403_rename_admission_date_to_encounter_date_and_more.py
@@ -7,7 +7,7 @@ from django.db.models import F
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("facility", "0401_merge_20231208_0054"),
+        ("facility", "0402_patientconsultation_new_discharge_reason"),
     ]
 
     def clean_encounter_date(apps, schema_editor):
