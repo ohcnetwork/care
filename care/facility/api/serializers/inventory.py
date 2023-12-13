c93b19feac080154ac3bc47ee48cc145dc2c1e7f
Vasudev Ravula <vasudev.ravula@gmail.com>
2023-12-12 23:57:51 -0500

    Update FacilityInventorySummary Serializer to return min_quantity_required field based on min_quantity configured or default

--- care/facility/api/serializers/inventory.py
+++ care/facility/api/serializers/inventory.py
@@ -122,6 +122,11 @@ class FacilityInventoryLogSerializer(serializers.ModelSerializer):
             pass
 
         summary_obj.is_low = current_quantity < current_min_quantity
+        summary_obj.min_quantity_required = (
+            current_quantity
+            if current_quantity < current_min_quantity
+            else current_min_quantity
+        )
 
         validated_data["current_stock"] = current_quantity
 
@@ -207,6 +212,11 @@ class FacilityInventoryMinQuantitySerializer(serializers.ModelSerializer):
                 facility=validated_data["facility"], item=item
             )
             summary_obj.is_low = summary_obj.quantity < validated_data["min_quantity"]
+            summary_obj.min_quantity_required = (
+                summary_obj.quantity
+                if summary_obj.quantity < validated_data["min_quantity"]
+                else validated_data["min_quantity"]
+            )
             summary_obj.save()
         except FacilityInventorySummary.DoesNotExist:
             pass
@@ -225,6 +235,11 @@ class FacilityInventoryMinQuantitySerializer(serializers.ModelSerializer):
                 facility=instance.facility, item=item
             )
             summary_obj.is_low = summary_obj.quantity < validated_data["min_quantity"]
+            summary_obj.min_quantity_required = (
+                summary_obj.quantity
+                if summary_obj.quantity < validated_data["min_quantity"]
+                else validated_data["min_quantity"]
+            )
             summary_obj.save()
         except FacilityInventorySummary.DoesNotExist:
             pass
