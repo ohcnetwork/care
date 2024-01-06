70508d0b1ed0c63a1614d9990faeb2c2e2b08e06
Amjith Titus <amjithtitus09@gmail.com>
2024-01-06 16:09:24 +0530

    Summarization Tasks Disable - Added env variables

--- care/facility/tasks/__init__.py
+++ care/facility/tasks/__init__.py
@@ -1,5 +1,6 @@
 from celery import current_app
 from celery.schedules import crontab
+from django.conf import settings
 
 from care.facility.tasks.asset_monitor import check_asset_status
 from care.facility.tasks.cleanup import delete_old_notifications
@@ -20,31 +21,37 @@ def setup_periodic_tasks(sender, **kwargs):
         delete_old_notifications.s(),
         name="delete_old_notifications",
     )
-    sender.add_periodic_task(
-        crontab(hour="*/4", minute="59"),
-        summarise_triage.s(),
-        name="summarise_triage",
-    )
-    sender.add_periodic_task(
-        crontab(hour="23", minute="59"),
-        summarise_tests.s(),
-        name="summarise_tests",
-    )
-    sender.add_periodic_task(
-        crontab(minute="*/5"),
-        summarise_facility_capacity.s(),
-        name="summarise_facility_capacity",
-    )
-    sender.add_periodic_task(
-        crontab(hour="*/1", minute="59"),
-        summarise_patient.s(),
-        name="summarise_patient",
-    )
-    sender.add_periodic_task(
-        crontab(hour="*/1", minute="59"),
-        summarise_district_patient.s(),
-        name="summarise_district_patient",
-    )
+    if not settings.TASK_SUMMARIZE_TRIAGE_DISABLED:
+        sender.add_periodic_task(
+            crontab(hour="*/4", minute="59"),
+            summarise_triage.s(),
+            name="summarise_triage",
+        )
+    if not settings.TASK_SUMMARIZE_TESTS_DISABLED:
+        sender.add_periodic_task(
+            crontab(hour="23", minute="59"),
+            summarise_tests.s(),
+            name="summarise_tests",
+        )
+    if not settings.TASK_SUMMARIZE_FACILITY_CAPACITY_DISABLED:
+        sender.add_periodic_task(
+            crontab(minute="*/5"),
+            summarise_facility_capacity.s(),
+            name="summarise_facility_capacity",
+        )
+    if not settings.TASK_SUMMARIZE_PATIENT_DISABLED:
+        sender.add_periodic_task(
+            crontab(hour="*/1", minute="59"),
+            summarise_patient.s(),
+            name="summarise_patient",
+        )
+    if not settings.TASK_SUMMARIZE_DISTRICT_PATIENT_DISABLED:
+        sender.add_periodic_task(
+            crontab(hour="*/1", minute="59"),
+            summarise_district_patient.s(),
+            name="summarise_district_patient",
+        )
+
     sender.add_periodic_task(
         crontab(minute="*/30"),
         check_asset_status.s(),
