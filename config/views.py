d389f1e1a9f540d25714519c33b7a6b1eff54c7b
Aakash Singh <mail@singhaakash.dev>
2024-01-10 18:45:01 +0530

    use a different endpoint for docker health check

--- config/views.py
+++ config/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from rest_framework.response import Response
 
 
 def home_view(request):
     return render(request, "pages/home.html")
+
+
+def ping(request):
+    return Response({"status": "OK"})
