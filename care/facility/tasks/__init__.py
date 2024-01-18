7ba0905a29495aa0923b1524bc1c43b5f77fb779
Amjith Titus <amjithtitus09@gmail.com>
2024-01-18 10:28:09 +0530

    Add env variables to toggle summarization tasks (#1809)
    * Summarization Tasks Disable - Added env variables
    
    * Env variable as Boolean instead of String, added Doc
    
    * Linting fix
    
    * add django-configuration doc to index
    
    ---------
    
    Co-authored-by: Aakash Singh <mail@singhaakash.dev>

--- care/facility/tasks/__init__.py
+++ care/facility/tasks/__init__.py
@@ -1,5 +1,6 @@
 from celery import current_app
 from celery.schedules import crontab
+from django.conf import settings
 
 from care.facility.tasks.asset_monitor import check_asset_status
 from care.facility.tasks.cleanup import delete_old_notifications
@@ -21,31 +22,37 @@ def setup_periodic_tasks(sender, **kwargs):
         delete_old_notifications.s(),
         name="delete_old_notifications",
     )
-    sender.add_periodic_task(
-        crontab(hour="*/4", minute="59"),
-        summarise_triage.s(),
-        name="summarise_triage",
-    )
-    sender.add_periodic_task(
-        crontab(hour="23", minute="59"),
-        summarise_tests.s(),
-        name="summarise_tests",
-    )
-    sender.add_periodic_task(
-        crontab(minute="*/5"),
-        summarise_facility_capacity.s(),
-        name="summarise_facility_capacity",
-    )
-    sender.add_periodic_task(
-        crontab(hour="*/1", minute="59"),
-        summarise_patient.s(),
-        name="summarise_patient",
-    )
-    sender.add_periodic_task(
-        crontab(hour="*/1", minute="59"),
-        summarise_district_patient.s(),
-        name="summarise_district_patient",
-    )
+    if settings.TASK_SUMMARIZE_TRIAGE:
+        sender.add_periodic_task(
+            crontab(hour="*/4", minute="59"),
+            summarise_triage.s(),
+            name="summarise_triage",
+        )
+    if settings.TASK_SUMMARIZE_TESTS:
+        sender.add_periodic_task(
+            crontab(hour="23", minute="59"),
+            summarise_tests.s(),
+            name="summarise_tests",
+        )
+    if settings.TASK_SUMMARIZE_FACILITY_CAPACITY:
+        sender.add_periodic_task(
+            crontab(minute="*/5"),
+            summarise_facility_capacity.s(),
+            name="summarise_facility_capacity",
+        )
+    if settings.TASK_SUMMARIZE_PATIENT:
+        sender.add_periodic_task(
+            crontab(hour="*/1", minute="59"),
+            summarise_patient.s(),
+            name="summarise_patient",
+        )
+    if settings.TASK_SUMMARIZE_DISTRICT_PATIENT:
+        sender.add_periodic_task(
+            crontab(hour="*/1", minute="59"),
+            summarise_district_patient.s(),
+            name="summarise_district_patient",
+        )
+
     sender.add_periodic_task(
         crontab(minute="*/30"),
         check_asset_status.s(),
