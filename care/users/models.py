5a087e1fb527451ea11510c45484c5d89d0c2d07
Aakash Singh <mail@singhaakash.dev>
2024-02-10 15:17:19 +0530

    Update username validator (#1865)
    * update username validator
    
    * lint fix

--- care/users/models.py
+++ care/users/models.py
@@ -1,7 +1,6 @@
 import uuid
 
 from django.contrib.auth.models import AbstractUser, UserManager
-from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.urls import reverse
@@ -10,6 +9,7 @@ from django.utils.translation import gettext_lazy as _
 
 from care.utils.models.base import BaseModel
 from care.utils.models.validators import (
+    UsernameValidator,
     mobile_or_landline_number_validator,
     mobile_validator,
 )
@@ -147,13 +147,6 @@ class Skill(BaseModel):
         return self.name
 
 
-class UsernameValidator(UnicodeUsernameValidator):
-    regex = r"^[\w.@+-]+[^.@+_-]$"
-    message = _(
-        "Please enter letters, digits and @ . + - _ only and username should not end with @ . + - or _"
-    )
-
-
 class UserSkill(BaseModel):
     user = models.ForeignKey("User", on_delete=models.CASCADE, null=True, blank=True)
     skill = models.ForeignKey("Skill", on_delete=models.CASCADE, null=True, blank=True)
@@ -175,7 +168,7 @@ class User(AbstractUser):
         _("username"),
         max_length=150,
         unique=True,
-        help_text=_("150 characters or fewer. Letters, digits and @/./+/-/_ only."),
+        help_text=username_validator.message,
         validators=[username_validator],
         error_messages={"unique": _("A user with that username already exists.")},
     )
