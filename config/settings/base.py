307a7c50bc38ebe7c45906e8edc8c1bd3563e75e
Khavin Shankar <khavinshankar@gmail.com>
2024-02-10 14:59:06 +0530

    Add custom prefix and suffix to hip name (#1861)
    * add postfix and prefix and clean hip name before service registration
    
    * set register to true if the  facility is already associated with the bridge id
    
    * renamed postfix to suffix

--- config/settings/base.py
+++ config/settings/base.py
@@ -597,6 +597,8 @@ HEALTH_SERVICE_API_URL = env(
     "HEALTH_SERVICE_API_URL", default="https://healthidsbx.abdm.gov.in/api"
 )
 ABDM_FACILITY_URL = env("ABDM_FACILITY_URL", default="https://facilitysbx.abdm.gov.in")
+HIP_NAME_PREFIX = env("HIP_NAME_PREFIX", default="")
+HIP_NAME_SUFFIX = env("HIP_NAME_SUFFIX", default="")
 ABDM_USERNAME = env("ABDM_USERNAME", default="abdm_user_internal")
 X_CM_ID = env("X_CM_ID", default="sbx")
 FIDELIUS_URL = env("FIDELIUS_URL", default="http://fidelius:8090")
