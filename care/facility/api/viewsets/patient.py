f7ac6a640b5e63eed1b32c43904b58e11402fd49
rithviknishad <mail@rithviknishad.dev>
2024-03-21 14:01:47 +0530

    fixes #1998

--- care/facility/api/viewsets/patient.py
+++ care/facility/api/viewsets/patient.py
@@ -410,7 +410,7 @@ class PatientViewSet(
         if self.action == "list" and settings.CSV_REQUEST_PARAMETER in self.request.GET:
             for backend in (PatientDRYFilter, filters.DjangoFilterBackend):
                 queryset = backend().filter_queryset(self.request, queryset, self)
-            return queryset
+            return queryset.filter(last_consultation__discharge_date_isnull=True)
 
         return super().filter_queryset(queryset)
 
