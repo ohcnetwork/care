eda759d7e9fa6e56e78aecfe1afe258adc9fc3d0
Shivank Kacker <kacker.shivank@gmail.com>
2024-03-26 22:18:36 +0530

    updated perms

--- care/facility/api/serializers/file_upload.py
+++ care/facility/api/serializers/file_upload.py
@@ -32,7 +32,10 @@ def check_permissions(file_type, associating_id, user, action="create"):
             if not has_facility_permission(user, patient.facility):
                 raise Exception("No Permission")
             return patient.id
-        elif file_type == FileUpload.FileType.CONSULTATION.value:
+        elif (
+            file_type == FileUpload.FileType.CONSULTATION.value
+            or file_type == FileUpload.FileType.CONSENT_RECORD.value
+        ):
             consultation = PatientConsultation.objects.get(external_id=associating_id)
             if consultation.discharge_date:
                 if not action == "read":
@@ -92,8 +95,6 @@ def check_permissions(file_type, associating_id, user, action="create"):
         elif file_type == FileUpload.FileType.CLAIM.value:
             return associating_id
         elif file_type == FileUpload.FileType.COMMUNICATION.value:
-            return associating_id
-        elif file_type == FileUpload.FileType.CONSENT_RECORD.value:
             return associating_id
         else:
             raise Exception("Undefined File Type")
