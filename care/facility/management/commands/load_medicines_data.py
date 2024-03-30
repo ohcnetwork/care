62a9e71790b904c7faa6d6cd964c76e290ccf6eb
Prafful <praffulsharma1230@gmail.com>
2024-03-30 18:15:06 +0530

    updated migrations

--- care/facility/management/commands/load_medicines_data.py
+++ care/facility/management/commands/load_medicines_data.py
@@ -21,33 +21,37 @@ class Command(BaseCommand):
         print("Loading Medibase Medicines into the database from medibase.json")
 
         medibase_objects = self.fetch_data()
-        MedibaseMedicine.objects.bulk_create(
-            [
-                MedibaseMedicine(
-                    id=medicine["id"],
-                    name=medicine["name"],
-                    type=medicine["type"],
-                    company=medicine.get("company"),
-                    contents=medicine.get("contents"),
-                    cims_class=medicine.get("cims_class"),
-                    atc_classification=medicine.get("atc_classification"),
-                    generic=medicine.get("generic"),
-                )
-                for medicine in medibase_objects
-            ],
-            batch_size=5000,
-            update_conflicts=True,
-            unique_fields=["name"],
-            update_fields=[
-                "created_date",
-                "modified_date",
-                "deleted",
-                "name",
-                "type",
-                "generic",
-                "company",
-                "contents",
-                "cims_class",
-                "atc_classification",
-            ],
-        )
+
+        if not MedibaseMedicine.objects.exists():
+            MedibaseMedicine.objects.bulk_create(
+                [
+                    MedibaseMedicine(
+                        id=medicine["id"],
+                        name=medicine["name"],
+                        type=medicine["type"],
+                        company=medicine.get("company"),
+                        contents=medicine.get("contents"),
+                        cims_class=medicine.get("cims_class"),
+                        atc_classification=medicine.get("atc_classification"),
+                        generic=medicine.get("generic"),
+                    )
+                    for medicine in medibase_objects
+                ],
+                batch_size=5000,
+                update_conflicts=True,
+                unique_fields=["name"],
+                update_fields=[
+                    "created_date",
+                    "modified_date",
+                    "name",
+                    "type",
+                    "generic",
+                    "company",
+                    "contents",
+                    "cims_class",
+                    "atc_classification",
+                ],
+            )
+            print("Medicines loaded successfully.")
+        else:
+            print("Medicines already exist in the database. Skipping bulk creation.")
