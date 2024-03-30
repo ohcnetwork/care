db6f739a32310a6cc80e36d8c8395fdf01937aa0
Aakash Singh <mail@singhaakash.dev>
2024-03-31 02:48:42 +0530

    fix lint

--- config/settings/base.py
+++ config/settings/base.py
@@ -309,7 +309,7 @@ LOGGING = {
     "formatters": {
         "verbose": {
             "format": "%(levelname)s %(asctime)s %(module)s "
-                      "%(process)d %(thread)d %(message)s"
+            "%(process)d %(thread)d %(message)s"
         }
     },
     "handlers": {
