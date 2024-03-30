8332e86d804791c1319bebf283cc8b5fc4198ff0
Aakash Singh <mail@singhaakash.dev>
2024-03-30 14:15:37 +0530

    fix lint issues

--- care/facility/api/viewsets/patient.py
+++ care/facility/api/viewsets/patient.py
@@ -6,8 +6,8 @@ from django.conf import settings
 from django.contrib.postgres.search import TrigramSimilarity
 from django.db import models
 from django.db.models import Case, OuterRef, Q, Subquery, When
-from django.utils import timezone
 from django.db.models.query import QuerySet
+from django.utils import timezone
 from django_filters import rest_framework as filters
 from djqscsv import render_to_csv_response
 from drf_spectacular.utils import extend_schema, extend_schema_view
