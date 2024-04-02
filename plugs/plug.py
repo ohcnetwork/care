358a47d10a9094789570b90e715088336e720adf
Aakash Singh <mail@singhaakash.dev>
2024-04-02 23:38:04 +0530

    fix plug class

--- plugs/plug.py
+++ plugs/plug.py
@@ -3,7 +3,7 @@ class Plug:
     Abstraction of a plugin
     """
 
-    def __init__(self, name, package_name, version, **configs):
+    def __init__(self, name, package_name, version, configs):
         self.name = name
         self.package_name = package_name
         self.version = version
