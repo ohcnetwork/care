25af85389ff3dd04853d74a978c9b1422d152204
Aakash Singh <mail@singhaakash.dev>
2024-04-02 23:29:21 +0530

    add care_scribe config vars

--- plug_config.py
+++ plug_config.py
@@ -1,11 +1,19 @@
+import environ
+
 from plugs.manager import PlugManager
 from plugs.plug import Plug
+
+env = environ.Env()
 
 scribe_plug = Plug(
     name="care_scribe",
     package_name="git+https://github.com/coronasafe/care_scribe.git",
-    version="@master",
-    configs={},
+    version="@scribe",
+    configs={
+        "TRANSCRIBE_SERVICE_PROVIDER_API_KEY": env(
+            "TRANSCRIBE_SERVICE_PROVIDER_API_KEY"
+        ),
+    },
 )
 
 plugs = [scribe_plug]
