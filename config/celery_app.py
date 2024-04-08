5a111108e5fcb07e4465c42728202e9b3336ace6
Aakash Singh <mail@singhaakash.dev>
2024-04-08 11:50:27 +0530

    Discard changes to config/celery_app.py

--- config/celery_app.py
+++ config/celery_app.py
@@ -1,7 +1,6 @@
 import os
 
 from celery import Celery
-from celerybeat_schedule import CELERYBEAT_SCHEDULE
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
