21f37fa447e26b047353b3035c37aa54f4b3f1cb
Aakash Singh <mail@singhaakash.dev>
2024-04-12 19:24:01 +0530

    add docs to configure plugins

--- plug_config.py
+++ plug_config.py
@@ -1,21 +1,10 @@
 import environ
 
 from plugs.manager import PlugManager
-from plugs.plug import Plug
+from plugs.plug import Plug  # noqa F401
 
 env = environ.Env()
 
-scribe_plug = Plug(
-    name="care_scribe",
-    package_name="git+https://github.com/coronasafe/care_scribe.git",
-    version="@scribe",
-    configs={
-        "TRANSCRIBE_SERVICE_PROVIDER_API_KEY": env(
-            "TRANSCRIBE_SERVICE_PROVIDER_API_KEY"
-        ),
-    },
-)
-
-plugs = [scribe_plug]
+plugs = []
 
 manager = PlugManager(plugs)
