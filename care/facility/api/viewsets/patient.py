4eee1787857922bc5720d1a2c95af1ad05cd79da
Aakash Singh <mail@singhaakash.dev>
2024-04-15 16:37:26 +0530

    fix patient sorting filters

--- care/facility/api/viewsets/patient.py
+++ care/facility/api/viewsets/patient.py
@@ -301,6 +301,7 @@ class PatientDRYFilter(DRYPermissionFiltersBase):
                 q_filters = Q(facility__id__in=allowed_facilities)
                 if view.action == "retrieve":
                     q_filters |= Q(consultations__facility__id__in=allowed_facilities)
+                    queryset = queryset.distinct("id")
                 q_filters |= Q(last_consultation__assigned_to=request.user)
                 q_filters |= Q(assigned_to=request.user)
                 queryset = queryset.filter(q_filters)
@@ -340,7 +341,7 @@ class PatientCustomOrderingFilter(BaseFilterBackend):
                 )
             ).order_by(ordering)
 
-        return queryset.distinct(ordering.lstrip("-") if ordering else "id")
+        return queryset
 
 
 @extend_schema_view(history=extend_schema(tags=["patient"]))
