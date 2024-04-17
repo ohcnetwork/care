78c6db65947ae6f1d326200843f3d2fdb9fe4e44
Pranshu1902 <pranshu1902@gmail.com>
2024-04-18 01:46:53 +0530

    reduce the filter fields

--- care/facility/api/viewsets/patient.py
+++ care/facility/api/viewsets/patient.py
@@ -615,14 +615,10 @@ class FacilityDischargedPatientViewSet(GenericViewSet, mixins.ListModelMixin):
     )
 
     ordering_fields = [
-        "facility__name",
         "id",
         "name",
         "created_date",
         "modified_date",
-        "review_time",
-        "last_consultation__current_bed__bed__name",
-        "date_declared_positive",
     ]
 
     def get_queryset(self) -> QuerySet:
