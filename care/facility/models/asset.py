ce02ab69b84dd90b2a51268dd7de4de2c3323e5c
rithviknishad <mail@rithviknishad.dev>
2024-05-30 16:41:25 +0530

    Adds camera preset model

--- care/facility/models/asset.py
+++ care/facility/models/asset.py
@@ -292,3 +292,65 @@ class AssetServiceEdit(models.Model):
 
     class Meta:
         ordering = ["-edited_on"]
+
+
+CAMERA_PRESET_POSITION_SCHEMA = {
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "type": "object",
+    "properties": {
+        "x": {"type": "number"},
+        "y": {"type": "number"},
+        "z": {"type": "number"},
+    },
+    "additionalProperties": False,
+}
+
+CAMERA_PRESET_BOUNDARY_SCHEMA = {
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "type": "object",
+    "properties": {
+        "x0": {"type": "number"},
+        "y0": {"type": "number"},
+        "x1": {"type": "number"},
+        "y1": {"type": "number"},
+    },
+    "additionalProperties": False,
+}
+
+
+class CameraPreset(BaseModel):
+    name = models.CharField(max_length=255, null=True)
+    asset_bed = models.ForeignKey(
+        "facility.AssetBed", on_delete=models.PROTECT, related_name="camera_presets"
+    )
+
+    position = models.JSONField(
+        validators=[JSONFieldSchemaValidator(CAMERA_PRESET_POSITION_SCHEMA)], null=True
+    )
+    boundary = models.JSONField(
+        validators=[JSONFieldSchemaValidator(CAMERA_PRESET_BOUNDARY_SCHEMA)], null=True
+    )
+
+    created_by = models.ForeignKey(
+        "users.User", null=True, blank=True, on_delete=models.PROTECT, related_name="+"
+    )
+    updated_by = models.ForeignKey(
+        "users.User", null=True, blank=True, on_delete=models.PROTECT, related_name="+"
+    )
+    is_migrated = models.BooleanField(default=False)
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                name="position_xor_boundary",
+                check=(
+                    Q(position__isnull=False, boundary__isnull=True)
+                    | Q(position__isnull=True, boundary__isnull=False)
+                ),
+            ),
+            models.UniqueConstraint(
+                name="single_boundary_preset_for_assetbed",
+                fields=("asset_bed",),
+                condition=Q(boundary__isnull=False, deleted=False),
+            ),
+        ]
