4f1bc1e1da5c0416910a7db4a68e8a424b139c12
dhruv-goyal-10 <dhruvgoyal.dev@gmail.com>
2024-06-17 12:30:11 +0530

    Added the test case for number of queries executing in /api/v1/facility/

--- care/facility/tests/test_facility_api.py
+++ care/facility/tests/test_facility_api.py
@@ -18,7 +18,8 @@ class FacilityTests(TestUtils, APITestCase):
         self.assertIs(response.status_code, status.HTTP_200_OK)
 
     def test_listing(self):
-        response = self.client.get("/api/v1/facility/")
+        with self.assertNumQueries(3):
+            response = self.client.get("/api/v1/facility/")
         self.assertIs(response.status_code, status.HTTP_200_OK)
 
     def test_create(self):
