70ac2b038aaf714f9de32ab6689d12c7ae2b9c6a
Uday Sagar <udaysagar.mail@gmail.com>
2024-07-03 20:25:36 +0530

    fix lint

--- care/facility/api/serializers/patient.py
+++ care/facility/api/serializers/patient.py
@@ -550,7 +550,9 @@ class PatientNotesSerializer(serializers.ModelSerializer):
         if "thread" not in validated_data:
             raise serializers.ValidationError({"thread": "This field is required"})
         if "consultation" not in validated_data:
-            raise serializers.ValidationError({"consultation": "This field is required"})
+            raise serializers.ValidationError(
+                {"consultation": "This field is required"}
+            )
         user_type = User.REVERSE_TYPE_MAP[validated_data["created_by"].user_type]
         # If the user is a doctor and the note is being created in the home facility
         # then the user type is doctor else it is a remote specialist
