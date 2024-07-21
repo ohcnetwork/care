ad61ec8edc8d3571a0172ac35d8701be099ac56d
Khavin Shankar <khavinshankar@gmail.com>
2024-07-22 02:51:56 +0530

    added abdm plug to care

--- plug_config.py
+++ plug_config.py
@@ -1,5 +1,26 @@
-from plugs.manager import PlugManager
+from django.conf import settings
 
-plugs = []
+from plugs.manager import PlugManager
+from plugs.plug import Plug
+
+abdm_plugin = Plug(
+    name="care_abdm",
+    package_name="git+https://github.com/coronasafe/care_abdm.git",
+    version="@v1.0.0",
+    configs={
+        "ABDM_CLIENT_ID": settings.ABDM_CLIENT_ID,
+        "ABDM_CLIENT_SECRET": settings.ABDM_CLIENT_SECRET,
+        "ABDM_URL": settings.ABDM_URL,
+        "HEALTH_SERVICE_API_URL": settings.HEALTH_SERVICE_API_URL,
+        "ABDM_FACILITY_URL": settings.ABDM_FACILITY_URL,
+        "HIP_NAME_PREFIX": settings.HIP_NAME_PREFIX,
+        "HIP_NAME_SUFFIX": settings.HIP_NAME_SUFFIX,
+        "ABDM_USERNAME": settings.ABDM_USERNAME,
+        "X_CM_ID": settings.X_CM_ID,
+        "FIDELIUS_URL": settings.FIDELIUS_URL,
+    },
+)
+
+plugs = [abdm_plugin]
 
 manager = PlugManager(plugs)
