4d0713c7f7cb8e71f68d97c7f560c27a09590905
prafful <praffulsharma1230@gmail.com>
2024-07-29 12:17:34 +0530

    improved prescription tag

--- care/facility/templatetags/prescription_tags.py
+++ care/facility/templatetags/prescription_tags.py
@@ -8,6 +8,11 @@ def format_prescription(prescription):
     if prescription.dosage_type == "TITRATED":
         return f"{prescription.medicine_name}, titration from {prescription.base_dosage} to {prescription.target_dosage}, {prescription.route}, {prescription.frequency} for {prescription.days} days."
     if prescription.dosage_type == "PRN":
-        return f"{prescription.medicine_name}, {prescription.base_dosage} to {prescription.max_dosage}, Should have minimum {prescription.min_hours_between_doses} hours between two doses, Indicated by {prescription.indicator}"
+        min_hours_str = (
+            f" Should have minimum {prescription.min_hours_between_doses} hours between two doses,"
+            if prescription.min_hours_between_doses
+            else ""
+        )
+        return f"{prescription.medicine_name}, {prescription.base_dosage} to {prescription.max_dosage}, {min_hours_str} Indicated by {prescription.indicator}"
     else:
         return f"{prescription.medicine_name}, {prescription.base_dosage}, {prescription.route}, {prescription.frequency} for {prescription.days} days."
