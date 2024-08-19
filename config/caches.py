d15295541951c7c01fa5fbad563301415e62af29
Aakash Singh <mail@singhaakash.dev>
2024-08-19 22:30:19 +0530

    fix tests

--- config/caches.py
+++ config/caches.py
@@ -3,10 +3,12 @@ from django.core.cache.backends.base import DEFAULT_TIMEOUT
 
 
 class DummyCache(dummy.DummyCache):
-    def set(self, key, value, timeout=DEFAULT_TIMEOUT, version=None, nx=False):
-        return super().set(key, value, timeout, version)
+    def set(self, key, value, timeout=DEFAULT_TIMEOUT, version=None, nx=True):
+        super().set(key, value, timeout, version)
+        return nx
 
 
 class LocMemCache(locmem.LocMemCache):
-    def set(self, key, value, timeout=DEFAULT_TIMEOUT, version=None, nx=False):
-        return super().set(key, value, timeout, version)
+    def set(self, key, value, timeout=DEFAULT_TIMEOUT, version=None, nx=True):
+        super().set(key, value, timeout, version)
+        return nx
