af9da5b86f2ecff495f5837f8f4b73787dffc34e
Aakash Singh <mail@singhaakash.dev>
2024-08-19 22:39:52 +0530

    cleanups

--- config/caches.py
+++ config/caches.py
@@ -3,12 +3,14 @@ from django.core.cache.backends.base import DEFAULT_TIMEOUT
 
 
 class DummyCache(dummy.DummyCache):
-    def set(self, key, value, timeout=DEFAULT_TIMEOUT, version=None, nx=True):
+    def set(self, key, value, timeout=DEFAULT_TIMEOUT, version=None, nx=None):
         super().set(key, value, timeout, version)
-        return nx
+        # mimic the behavior of django_redis with setnx, for tests
+        return True
 
 
 class LocMemCache(locmem.LocMemCache):
-    def set(self, key, value, timeout=DEFAULT_TIMEOUT, version=None, nx=True):
+    def set(self, key, value, timeout=DEFAULT_TIMEOUT, version=None, nx=None):
         super().set(key, value, timeout, version)
-        return nx
+        # mimic the behavior of django_redis with setnx, for tests
+        return True
