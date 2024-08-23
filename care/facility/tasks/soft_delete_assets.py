1ab2e5a582185eb97c5ad70df291f0aac5ec3d75
Aakash Singh <mail@singhaakash.dev>
2024-08-23 17:56:43 +0530

    fix query

--- care/facility/tasks/soft_delete_assets.py
+++ care/facility/tasks/soft_delete_assets.py
@@ -1,12 +1,15 @@
 from celery import shared_task
 
-from care.facility.models.asset import Asset
+from care.facility.models.asset import Asset, AssetLocation
 from care.facility.models.facility import Facility
 
 
 @shared_task
 def soft_delete_assets_schedule():
-    facilities = Facility.objects.all().values_list("id", flat=True)
-    Asset.objects.exclude(current_location__facility__id__in=facilities).update(
-        deleted=True
-    )
+    Asset.objects.filter(
+        current_location__id__in=AssetLocation._base_manager.filter(
+            facility__id__in=Facility._base_manager.filter(deleted=True).values_list(
+                "id", flat=True
+            )
+        ).values_list("id", flat=True)
+    ).update(deleted=True)
