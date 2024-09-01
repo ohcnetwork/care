4e117e71ddedfb2e9839757c8438bff4e77f279e
Khavin Shankar <khavinshankar@gmail.com>
2024-09-01 09:15:33 +0530

    fixed signal conflicting with load_dummy_data

--- care/facility/management/commands/load_dummy_data.py
+++ care/facility/management/commands/load_dummy_data.py
@@ -2,6 +2,13 @@ import os
 
 from django.core import management
 from django.core.management import BaseCommand, CommandError
+from django.db.models.signals import (
+    m2m_changed,
+    post_delete,
+    post_save,
+    pre_delete,
+    pre_save,
+)
 
 
 class Command(BaseCommand):
@@ -21,6 +28,20 @@ class Command(BaseCommand):
                 "This command is not intended to be run in production environment."
             )
 
+        # Disconnecting signals temporarily to avoid conflicts
+        signals_to_disconnect = [
+            post_save,
+            post_delete,
+            pre_save,
+            pre_delete,
+            m2m_changed,
+        ]
+        original_receivers = {}
+
+        for signal in signals_to_disconnect:
+            original_receivers[signal] = signal.receivers
+            signal.receivers = []
+
         try:
             management.call_command("loaddata", self.BASE_URL + "states.json")
             management.call_command("load_skill_data")
@@ -33,3 +54,7 @@ class Command(BaseCommand):
             management.call_command("populate_investigations")
         except Exception as e:
             raise CommandError(e)
+        finally:
+            # Reconnect original signals
+            for signal in signals_to_disconnect:
+                signal.receivers = original_receivers[signal]
