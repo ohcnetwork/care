ab5bea84bc4ad7de0164b7fbf73ee44967d5fd3c
Man Of The Mans <jackronn555@gmail.com>
2024-10-11 12:37:02 +0530

    Improve backup script

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
+set -ue
 # Variables
-CONTAINER_NAME= $(docker ps --format '{{.Names}}' | grep 'care-db'
-)
-DB_NAME="care"
-DB_USER="postgres"
+container_name="$(docker ps --format '{{.Names}}' | grep 'care-db'
+)"
+db_name='care'
+db_user='postgres'
 #Adding separate directory
-BACKUP_DIR="$(pwd)/backup"
-DATE=$(date +%Y%m%d%H%M%S)
-BACKUP_FILE="${BACKUP_DIR}/${DB_NAME}_backup_${DATE}.sql"
+backup_dir="$(pwd)/backup"
+date=$(date +%Y%m%d%H%M%S)
+backup_file="${backup_dir}/${db_name}_backup_${date}.sql"
 
 # Ensure the backup directory exists
-mkdir -p ${BACKUP_DIR}
+[ -d "${backup_dir}" ] || mkdir "$backup_dir"
 
 # Remove old backups
-find ${BACKUP_DIR} -name "${DB_NAME}_backup_*.sql" -type f -mtime +0 -exec rm -f {} \;
+find "${backup_dir}" -name "${db_name}_backup_*.sql" -type f -mtime +7 -exec rm {} \;
 
 # add the new backup
-docker exec -t ${CONTAINER_NAME} pg_dump -U ${DB_USER} ${DB_NAME} > ${BACKUP_FILE}
-echo "Backup of database '${DB_NAME}' completed and saved as ${BACKUP_FILE}"
+docker exec -t ${container_name} pg_dump -U ${db_user} ${db_name} > ${backup_file}
+echo "Backup of database '${db_name}' completed and saved as ${backup_file}"
