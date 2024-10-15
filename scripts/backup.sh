89dd3ecfd4fe3b2d2ba31a8d8e8ffe54a5df575f
Man Of The Mans <jackronn555@gmail.com>
2024-10-15 21:18:01 +0530

    Fix typo

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -5,10 +5,10 @@ source ../.env
 container_name="$(docker ps --format '{{.Names}}' | grep 'care-db')"
 date=$(date +%Y%m%d%H%M%S)
 #name the file
-backup_file="./backup/${POSTGRES_DB}_backup_${date}.dump"
+backup_file="./backups/${POSTGRES_DB}_backup_${date}.dump"
 
-# Remove old backups
-docker exec -t ${container_name} find "./backup" -name "${POSTGRES_DB}_backup_*.dump" -type f -mtime +${RETENTION_PERIOD} -exec rm {} \;
+# Remove old backup/backups
+docker exec -t ${container_name} find "./backups" -name "${POSTGRES_DB}_backup_*.dump" -type f -mtime +${RETENTION_PERIOD} -exec rm {} \;
 
 #backup the database
 docker exec -t ${container_name} pg_dump -U ${POSTGRES_USER} -Fc -f ${backup_file} ${POSTGRES_DB}
