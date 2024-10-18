f1c2f772f5efc922f2e2ae9831fd2658c8abeb0e
Khavin Shankar <khavinshankar@gmail.com>
2024-10-19 01:55:01 +0530

    removed abdm config from plug_config

--- plug_config.py
+++ plug_config.py
@@ -1,13 +1,6 @@
 from plugs.manager import PlugManager
 from plugs.plug import Plug
 
-abdm_plugin = Plug(
-    name="abdm",
-    package_name="git+https://github.com/ohcnetwork/care_abdm.git",
-    version="@main",
-    configs={},
-)
-
 hcx_plugin = Plug(
     name="hcx",
     package_name="git+https://github.com/ohcnetwork/care_hcx.git",
@@ -15,6 +8,6 @@ hcx_plugin = Plug(
     configs={},
 )
 
-plugs = [hcx_plugin, abdm_plugin]
+plugs = [hcx_plugin]
 
 manager = PlugManager(plugs)
