e8fe8d7326cf6bb6da32243e9d4b1932b9c0ff9d
Man Of The Mans <jackronn555@gmail.com>
2024-10-26 20:36:12 +0530

    Fix path, var name

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -5,11 +5,11 @@ source ../.env
 container_name="$(docker ps --format '{{.Names}}' | grep 'care-db')"
 date=$(date +%Y%m%d%H%M%S)
 #name the file
-backup_file="./backups/${POSTGRES_DB}_backup_${date}.dump"
+backup_file="${POSTGRES_DB}_backup_${date}.dump"
 
 # Remove old backup/backups
-docker exec -t ${container_name} find "./backups" -name "${POSTGRES_DB}_backup_*.dump" -type f -mtime +${RETENTION_PERIOD} -exec rm {} \;
+docker exec -t ${container_name} find "/backups" -name "${POSTGRES_DB}_backup_*.dump" -type f -mtime +${DB_BACKUP_RETENTION_PERIOD} -exec rm {} \;
 
 #backup the database
-docker exec -t ${container_name} pg_dump -U ${POSTGRES_USER} -Fc -f ${backup_file} ${POSTGRES_DB}
+docker exec -t ${container_name} pg_dump -U ${POSTGRES_USER} -Fc -f /backups/${backup_file} ${POSTGRES_DB}
 echo "Backup of database '${POSTGRES_DB}' completed and saved as ${backup_file}"
