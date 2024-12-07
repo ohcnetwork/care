fa6f6bd6b06c12d2bf21080844dd3b5694d81c49
Shaurya Gupta <shauryag1000@gmail.com>
2024-12-07 18:15:29 +0530

    Update hospital_doctor.py

--- care/facility/api/serializers/hospital_doctor.py
+++ care/facility/api/serializers/hospital_doctor.py
@@ -8,11 +8,22 @@ from care.utils.serializers.fields import ChoiceField
 class HospitalDoctorSerializer(serializers.ModelSerializer):
     area_text = ChoiceField(choices=DOCTOR_TYPES, read_only=True, source="area")
     id = serializers.UUIDField(source="external_id", read_only=True)
+    total_doctors = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = HospitalDoctors
         read_only_fields = (
             "id",
             "area_text",
+            "total_doctors"
         )
         exclude = (*TIMESTAMP_FIELDS, "facility", "external_id")
+
+        def to_representation(self, instance):
+            representation = super().to_representation(instance)
+            representation["total_doctors"] = (
+                HospitalDoctors.objects.filter(facility=instance.facility)
+                .aggregate(total_doctors=Sum("count"))["total_doctors"]
+                or 0
+            )
+            return representation
