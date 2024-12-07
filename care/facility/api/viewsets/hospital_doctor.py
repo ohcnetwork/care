83a2f2b02e9ff16f587e573d8d3a956ac71c319b
Shaurya Gupta <shauryag1000@gmail.com>
2024-12-07 18:10:52 +0530

    Reverting hospital_doctor.py

--- care/facility/api/viewsets/hospital_doctor.py
+++ care/facility/api/viewsets/hospital_doctor.py
@@ -2,20 +2,16 @@ from dry_rest_permissions.generics import DRYPermissions
 from rest_framework.generics import get_object_or_404
 from rest_framework.mixins import ListModelMixin
 from rest_framework.permissions import IsAuthenticated
-from django.db.models import Sum
 
 from care.facility.api.serializers.hospital_doctor import HospitalDoctorSerializer
 from care.facility.api.viewsets import FacilityBaseViewset
 from care.facility.models import Facility, HospitalDoctors
 from care.users.models import User
 from care.utils.cache.cache_allowed_facilities import get_accessible_facilities
-
-
 class HospitalDoctorViewSet(FacilityBaseViewset, ListModelMixin):
     serializer_class = HospitalDoctorSerializer
     queryset = HospitalDoctors.objects.filter(facility__deleted=False)
     permission_classes = (IsAuthenticated, DRYPermissions)
-
     def get_queryset(self):
         user = self.request.user
         queryset = self.queryset.filter(
@@ -31,10 +27,8 @@ class HospitalDoctorViewSet(FacilityBaseViewset, ListModelMixin):
             allowed_facilities = get_accessible_facilities(user)
             queryset = queryset.filter(facility__id__in=allowed_facilities)
         return queryset
-
     def get_object(self):
         return get_object_or_404(self.get_queryset(), area=self.kwargs.get("pk"))
-
     def get_facility(self):
         facility_qs = Facility.objects.filter(
             external_id=self.kwargs.get("facility_external_id")
@@ -42,12 +36,6 @@ class HospitalDoctorViewSet(FacilityBaseViewset, ListModelMixin):
         if not self.request.user.is_superuser:
             facility_qs.filter(users__id__exact=self.request.user.id)
         return get_object_or_404(facility_qs)
-
-    def list(self, request, *args, **kwargs):
-        response = super().list(request, *args, **kwargs)
-        total_doctors = self.get_queryset().aggregate(total_doctors=Sum('count'))['total_doctors']
-        response.data["total_doctors"] = total_doctors or 0
-        return response
 
     def perform_create(self, serializer):
         serializer.save(facility=self.get_facility())
