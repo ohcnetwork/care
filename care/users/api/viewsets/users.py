107e547ecf122717c7fa9d7c2263aca43c34b55f
Vignesh Hari <14056798+vigneshhari@users.noreply.github.com>
2024-12-10 12:22:34 +0530

    Remove redundant check

--- care/users/api/viewsets/users.py
+++ care/users/api/viewsets/users.py
@@ -155,7 +155,7 @@ class UserViewSet(
 
     def get_object(self) -> User:
         try:
-            if self.request.method == "GET" and self.action == "retrieve":
+            if self.action == "retrieve":
                 username = self.kwargs.get("username")
                 return get_object_or_404(User, username=username)
             return super().get_object()
