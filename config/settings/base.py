6147c0bfcd78c7de3ecf72c82c57a0f05c8f9e57
Prafful <praffulsharma1230@gmail.com>
2025-01-30 00:50:17 +0530

    update the log setting

--- config/settings/base.py
+++ config/settings/base.py
@@ -391,7 +391,7 @@ SPECTACULAR_SETTINGS = {
     "TITLE": "Care API",
     "DESCRIPTION": "Documentation of API endpoints of Care ",
     "VERSION": "1.0.0",
-    "DISABLE_ERRORS_AND_WARNINGS": False,
+    "DISABLE_ERRORS_AND_WARNINGS": True,
 }
 
 # Simple JWT (JWT Authentication)
