484c185e96f6c18323dc7275c24203edeb92c3d9
yash-learner <yash.topaz@gmail.com>
2025-03-05 16:22:18 +0530

    Add MFA enabled flag to user serialization mapping

--- care/emr/resources/user/spec.py
+++ care/emr/resources/user/spec.py
@@ -89,11 +89,15 @@ class UserSpec(UserBaseSpec):
     user_type: str
     gender: str
     username: str
+    mfa_enabled: bool = False
 
     @classmethod
     def perform_extra_serialization(cls, mapping, obj: User):
         mapping["id"] = str(obj.external_id)
         mapping["profile_picture_url"] = obj.read_profile_picture_url()
+        mapping["mfa_enabled"] = bool(
+            obj.mfa_settings.get("totp", {}).get("enabled", False)
+        )
 
 
 class UserRetrieveSpec(UserSpec):
