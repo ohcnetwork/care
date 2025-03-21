d3443665fba6fc52278daf6488b722cdc2c877dd
Prafful <praffulsharma1230@gmail.com>
2025-03-21 21:36:48 +0530

    added a dynamic valueset type

--- care/emr/resources/condition/spec.py
+++ care/emr/resources/condition/spec.py
@@ -2,16 +2,18 @@ import datetime
 from enum import Enum
 
 from django.utils.timezone import is_aware, make_aware
-from pydantic import UUID4, Field, field_validator
+from pydantic import UUID4, field_validator
 from rest_framework.generics import get_object_or_404
 
 from care.emr.models.condition import Condition
 from care.emr.models.encounter import Encounter
-from care.emr.registries.care_valueset.care_valueset import validate_valueset
 from care.emr.resources.base import EMRResource
 from care.emr.resources.common.coding import Coding
 from care.emr.resources.condition.valueset import CARE_CODITION_CODE_VALUESET
 from care.emr.resources.user.spec import UserSpec
+from care.emr.utils.valueset_coding_type import (
+    ValueSetBoundCoding,
+)
 from care.utils.time_util import care_now
 
 
@@ -80,18 +82,11 @@ class ConditionSpec(BaseConditionSpec):
     clinical_status: ClinicalStatusChoices | None = None
     verification_status: VerificationStatusChoices
     severity: SeverityChoices | None = None
-    code: Coding = Field(json_schema_extra={"slug": CARE_CODITION_CODE_VALUESET.slug})
+    code: ValueSetBoundCoding[CARE_CODITION_CODE_VALUESET.slug]
     encounter: UUID4
     onset: ConditionOnSetSpec = {}
     abatement: ConditionAbatementSpec = {}
     note: str | None = None
-
-    @field_validator("code")
-    @classmethod
-    def validate_code(cls, code: int):
-        return validate_valueset(
-            "code", cls.model_fields["code"].json_schema_extra["slug"], code
-        )
 
     @field_validator("encounter")
     @classmethod
@@ -145,17 +140,10 @@ class ConditionUpdateSpec(BaseConditionSpec):
     clinical_status: ClinicalStatusChoices | None = None
     verification_status: VerificationStatusChoices
     severity: SeverityChoices | None = None
-    code: Coding = Field(json_schema_extra={"slug": CARE_CODITION_CODE_VALUESET.slug})
+    code: ValueSetBoundCoding[CARE_CODITION_CODE_VALUESET.slug]
     onset: ConditionOnSetSpec = {}
     abatement: ConditionAbatementSpec = {}
     note: str | None = None
-
-    @field_validator("code")
-    @classmethod
-    def validate_code(cls, code: int):
-        return validate_valueset(
-            "code", cls.model_fields["code"].json_schema_extra["slug"], code
-        )
 
 
 class ChronicConditionUpdateSpec(ConditionUpdateSpec):
