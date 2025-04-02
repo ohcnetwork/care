ee45ca5f5eed35e04372a791c8e7634eeee80379
Jacob John Jeevan <mail@jacobjeevan.me>
2025-04-02 13:37:58 +0530

    added none

--- care/emr/resources/location/spec.py
+++ care/emr/resources/location/spec.py
@@ -173,7 +173,7 @@ class FacilityLocationEncounterUpdateSpec(FacilityLocationEncounterBaseSpec):
     status: LocationEncounterAvailabilityStatusChoices
 
     start_datetime: datetime.datetime
-    end_datetime: datetime.datetime | None
+    end_datetime: datetime.datetime | None = None
 
 
 class FacilityLocationEncounterListSpec(FacilityLocationEncounterBaseSpec):
