8ace17ca4404a5b3a16081951ccb219828ce26d4
rithviknishad <mail@rithviknishad.dev>
2025-06-05 15:41:23 +0530

    weird things...

--- care/emr/api/viewsets/inventory/inventory_item.py
+++ care/emr/api/viewsets/inventory/inventory_item.py
@@ -14,6 +14,7 @@ class InventoryItemFilters(filters.FilterSet):
         field_name="product__product_knowledge__external_id"
     )
     status = filters.CharFilter(lookup_expr="iexact")
+    net_content_gt = filters.NumberFilter(field_name="net_content", lookup_expr="gt")
     net_content = filters.RangeFilter(field_name="net_content")
 
 
