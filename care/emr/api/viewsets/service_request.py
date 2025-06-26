8e22e2e9c42bd45a2a4201c05e404757d199305f
Aakash Singh <mail@singhaakash.dev>
2025-06-26 12:09:53 +0530

    Order service requests by descending created time

--- care/emr/api/viewsets/service_request.py
+++ care/emr/api/viewsets/service_request.py
@@ -5,6 +5,7 @@ from drf_spectacular.utils import extend_schema
 from pydantic import UUID4, BaseModel
 from rest_framework.decorators import action
 from rest_framework.exceptions import ValidationError
+from rest_framework.filters import OrderingFilter
 from rest_framework.response import Response
 
 from care.emr.api.viewsets.base import (
@@ -80,12 +81,15 @@ class ServiceRequestViewSet(
     pydantic_read_model = ServiceRequestReadSpec
     pydantic_retrieve_model = ServiceRequestRetrieveSpec
     filterset_class = ServiceRequestFilters
-    filter_backends = [filters.DjangoFilterBackend]
+    filter_backends = [filters.DjangoFilterBackend, OrderingFilter]
     questionnaire_type = "service_request"
     questionnaire_title = "Service Request"
     questionnaire_description = "Service Request"
     questionnaire_subject_type = SubjectType.patient.value
     resource_type = TagResource.service_request
+    ordering = [
+        "-created_date",
+    ]
 
     def get_facility_obj(self):
         return get_object_or_404(
