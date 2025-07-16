5616dc9034bee91e75cc7dba66ffb5b7c6a5fc54
nandkishorr <rachulr7@gmail.com>
2025-07-16 18:02:40 +0530

    fix:made some minor changes

--- care/emr/resources/common/validators.py
+++ care/emr/resources/common/validators.py
@@ -5,12 +5,10 @@ from django.utils.timezone import is_aware, make_aware
 from care.utils.time_util import care_now
 
 
-def validate_datetime(
-    datetime: datetime.datetime,
-) -> datetime.datetime:
-    if datetime:
-        if not is_aware(datetime):
-            datetime = make_aware(datetime)
-        if datetime > care_now():
+def validate_datetime(dt: datetime.datetime) -> datetime.datetime:
+    if dt:
+        if not is_aware(dt):
+            dt = make_aware(dt)
+        if dt > care_now():
             raise ValueError("Date cannot be in the future")
-    return datetime
+    return dt
