ae0940630e52dab6543383b787023f02ef243a60
Jacob Jeevan <mail@jacobjeevan.me>
2025-07-18 20:44:15 +0530

    serialize booked by

--- care/emr/resources/scheduling/slot/spec.py
+++ care/emr/resources/scheduling/slot/spec.py
@@ -107,5 +107,6 @@ class TokenBookingReadSpec(TokenBookingBaseSpec):
             FacilityBareMinimumSpec, id=obj.token_slot.resource.facility_id
         )
         mapping["tags"] = SingleFacilityTagManager().render_tags(obj)
+        mapping["booked_by"] = model_from_cache(UserSpec, id=obj.booked_by_id)
 
         cls.serialize_audit_users(mapping, obj)
