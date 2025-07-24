cb4eba0c0cc535fd5476d56566462195653c1ad5
Areeb Ahmed <areebahmed0709@gmail.com>
2025-07-24 20:30:11 +0300

    add traefik back (reduce cost)

--- scripts/minio_cron.sh
+++ scripts/minio_cron.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-mc alias set minio http://minio:9000 "$MINIO_ACCESS_KEY" "$MINIO_SECRET_KEY"
+until mc alias set minio http://minio:9000 "$MINIO_ACCESS_KEY" "$MINIO_SECRET_KEY" 2>/dev/null; do
+  echo "Waiting for MinIO.."
+  sleep 2
+done
+
+mc mb --ignore-existing minio/"$PATIENT_MINIO_BUCKET"
+mc mb --ignore-existing minio/"$FACILITY_MINIO_BUCKET"
+
 mc alias set s3 https://s3.amazonaws.com "$S3_ACCESS_KEY_ID" "$S3_SECRET_ACCESS_KEY"
 
 mc mirror minio/"$PATIENT_MINIO_BUCKET" s3/"$S3_BUCKET/backup"
