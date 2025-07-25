f4537433f2c1ad6255ed1233aa5d5226123924d5
NANDKISHOR R <96693626+nandkishorr@users.noreply.github.com>
2025-07-25 12:50:20 +0530

    Fixed typo error

--- care/emr/api/viewsets/patient_identifier.py
+++ care/emr/api/viewsets/patient_identifier.py
@@ -24,7 +24,7 @@ from care.security.authorization.base import AuthorizationController
 
 class PatientIdentifierConfigFilters(filters.FilterSet):
     facility = filters.UUIDFilter(field_name="facility__external_id")
-    status = (filters.CharFilter(lookup_expr="iexact"),)
+    status = (filters.CharFilter(lookup_expr="iexact")
     display = filters.CharFilter(field_name="config__display", lookup_expr="icontains")
 
 
