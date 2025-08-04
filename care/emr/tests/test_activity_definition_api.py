d6d94394665261c6eb6bb96f2749abf9f64fff33
nandkishorr <rachulr7@gmail.com>
2025-08-05 04:00:06 +0530

    feat:added create testcases for activity definition

--- care/emr/tests/test_activity_definition_api.py
+++ care/emr/tests/test_activity_definition_api.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.urls import reverse
 from model_bakery import baker
 
@@ -25,10 +27,7 @@ class ActivityDefinitionAPITestBase(CareAPITestBase):
                 ActivityDefinitionPermissions.can_write_activity_definition.name,
             ]
         )
-        self.base_url = self.get_base_urlreverse(
-            "activity_definition-list",
-            kwargs={"facility_external_id": self.facility.external_id},
-        )
+        self.base_url = self.get_base_url(facility=self.facility.external_id)
         self.facility_location = self.create_facility_location(
             facility=self.facility, name="Test Facility Location"
         )
@@ -51,15 +50,18 @@ class ActivityDefinitionAPITestBase(CareAPITestBase):
             **kwargs,
         }
 
-    def create_activity_definition(self, **kwargs):
+    def create_activity_definition(self, facility, **kwargs):
         data = self.generate_activity_definition_data(**kwargs)
         return baker.make(
-            "ActivityDefinition",
+            "emr.ActivityDefinition",
             **data,
-            specimen_requirements=[self.generate_specimen_definition().id],
-            observation_result_requirements=[self.generate_observation_definition().id],
-            healthcare_service=self.generate_healthcare_service().id,
-            charge_item_definitions=[self.charge_item_definition().id],
+            facility=facility,
+            specimen_requirements=[self.generate_specimen_definition(facility).id],
+            observation_result_requirements=[
+                self.generate_observation_definition(facility).id
+            ],
+            healthcare_service=self.generate_healthcare_service(facility),
+            charge_item_definitions=[self.charge_item_definition(facility).id],
         )
 
     def get_details_url(self, facility=None, activity_definition=None):
@@ -111,3 +113,260 @@ class ActivityDefinitionAPITestBase(CareAPITestBase):
 
     def create_facility_location(self, facility, **kwargs):
         return baker.make("emr.FacilityLocation", facility=facility, **kwargs)
+
+    # Test cases for create activity definition
+
+    def test_create_activity_definition_as_superuser(self):
+        self.client.force_authenticate(user=self.superuser)
+        data = self.generate_activity_definition_data(
+            specimen_requirements=[
+                self.generate_specimen_definition(self.facility).external_id
+            ],
+            observation_result_requirements=[
+                self.generate_observation_definition(self.facility).external_id
+            ],
+            healthcare_service=self.generate_healthcare_service(
+                self.facility
+            ).external_id,
+            charge_item_definitions=[
+                self.charge_item_definition(self.facility).external_id
+            ],
+            locations=[self.facility_location.external_id],
+        )
+        response = self.client.post(self.base_url, data, format="json")
+        self.assertEqual(response.status_code, 200)
+        get_response = self.client.get(
+            self.get_details_url(
+                facility=self.facility.external_id,
+                activity_definition=response.data["id"],
+            )
+        )
+        self.assertEqual(get_response.status_code, 200)
+        self.assertEqual(get_response.data["id"], response.data["id"])
+
+    def test_create_activity_definition_as_user_with_permissions(self):
+        self.attach_role_facility_organization_user(
+            user=self.user,
+            facility_organization=self.facility_organization,
+            role=self.role,
+        )
+        self.client.force_authenticate(user=self.user)
+        data = self.generate_activity_definition_data(
+            specimen_requirements=[
+                self.generate_specimen_definition(self.facility).external_id
+            ],
+            observation_result_requirements=[
+                self.generate_observation_definition(self.facility).external_id
+            ],
+            healthcare_service=self.generate_healthcare_service(
+                self.facility
+            ).external_id,
+            charge_item_definitions=[
+                self.charge_item_definition(self.facility).external_id
+            ],
+            locations=[self.facility_location.external_id],
+        )
+        response = self.client.post(self.base_url, data, format="json")
+        self.assertEqual(response.status_code, 200)
+        get_response = self.client.get(
+            self.get_details_url(
+                facility=self.facility.external_id,
+                activity_definition=response.data["id"],
+            )
+        )
+        self.assertEqual(get_response.status_code, 200)
+        self.assertEqual(get_response.data["id"], response.data["id"])
+
+    def test_create_activity_definition_as_user_without_permissions(self):
+        self.client.force_authenticate(user=self.user)
+        data = self.generate_activity_definition_data(
+            specimen_requirements=[
+                self.generate_specimen_definition(self.facility).external_id
+            ],
+            observation_result_requirements=[
+                self.generate_observation_definition(self.facility).external_id
+            ],
+            healthcare_service=self.generate_healthcare_service(
+                self.facility
+            ).external_id,
+            charge_item_definitions=[
+                self.charge_item_definition(self.facility).external_id
+            ],
+            locations=[self.facility_location.external_id],
+        )
+        response = self.client.post(self.base_url, data, format="json")
+        self.assertEqual(response.status_code, 403)
+
+    def test_create_activity_definition_with_invalid_facility(self):
+        self.client.force_authenticate(user=self.superuser)
+        data = self.generate_activity_definition_data(
+            specimen_requirements=[
+                self.generate_specimen_definition(self.facility).external_id
+            ],
+            observation_result_requirements=[
+                self.generate_observation_definition(self.facility).external_id
+            ],
+            healthcare_service=self.generate_healthcare_service(
+                self.facility
+            ).external_id,
+            charge_item_definitions=[
+                self.charge_item_definition(self.facility).external_id
+            ],
+            locations=[self.facility_location.external_id],
+        )
+        response = self.client.post(
+            self.get_base_url(facility="invalid-facility-id"), data, format="json"
+        )
+        self.assertEqual(response.status_code, 404)
+
+    def test_create_activity_definition_with_invalid_specimen(self):
+        self.client.force_authenticate(user=self.superuser)
+        invalid_specimen_id = uuid.uuid4()
+        data = self.generate_activity_definition_data(
+            specimen_requirements=[invalid_specimen_id],
+            observation_result_requirements=[
+                self.generate_observation_definition(self.facility).external_id
+            ],
+            healthcare_service=self.generate_healthcare_service(
+                self.facility
+            ).external_id,
+            charge_item_definitions=[
+                self.charge_item_definition(self.facility).external_id
+            ],
+            locations=[self.facility_location.external_id],
+        )
+        response = self.client.post(self.base_url, data, format="json")
+        self.assertEqual(response.status_code, 400)
+        self.assertContains(
+            response,
+            f"Specimen Definition with id {invalid_specimen_id}",
+            status_code=400,
+        )
+
+    def test_create_activity_definition_with_invalid_observation(self):
+        self.client.force_authenticate(user=self.superuser)
+        invalid_observation_id = uuid.uuid4()
+        data = self.generate_activity_definition_data(
+            specimen_requirements=[
+                self.generate_specimen_definition(self.facility).external_id
+            ],
+            observation_result_requirements=[invalid_observation_id],
+            healthcare_service=self.generate_healthcare_service(
+                self.facility
+            ).external_id,
+            charge_item_definitions=[
+                self.charge_item_definition(self.facility).external_id
+            ],
+            locations=[self.facility_location.external_id],
+        )
+        response = self.client.post(self.base_url, data, format="json")
+        self.assertEqual(response.status_code, 400)
+        self.assertContains(
+            response,
+            f"Observation Definition with id {invalid_observation_id}",
+            status_code=400,
+        )
+
+    def test_create_activity_definition_with_invalid_location(self):
+        self.client.force_authenticate(user=self.superuser)
+        invalid_location_id = uuid.uuid4()
+        data = self.generate_activity_definition_data(
+            specimen_requirements=[
+                self.generate_specimen_definition(self.facility).external_id
+            ],
+            observation_result_requirements=[
+                self.generate_observation_definition(self.facility).external_id
+            ],
+            healthcare_service=self.generate_healthcare_service(
+                self.facility
+            ).external_id,
+            charge_item_definitions=[
+                self.charge_item_definition(self.facility).external_id
+            ],
+            locations=[invalid_location_id],
+        )
+        response = self.client.post(self.base_url, data, format="json")
+        self.assertEqual(response.status_code, 400)
+        self.assertContains(
+            response, f"Location with id {invalid_location_id}", status_code=400
+        )
+
+    def test_create_activity_definition_with_invalid_charge_item(self):
+        self.client.force_authenticate(user=self.superuser)
+        invalid_charge_item_id = uuid.uuid4()
+        data = self.generate_activity_definition_data(
+            specimen_requirements=[
+                self.generate_specimen_definition(self.facility).external_id
+            ],
+            observation_result_requirements=[
+                self.generate_observation_definition(self.facility).external_id
+            ],
+            healthcare_service=self.generate_healthcare_service(
+                self.facility
+            ).external_id,
+            charge_item_definitions=[invalid_charge_item_id],
+            locations=[self.facility_location.external_id],
+        )
+        response = self.client.post(self.base_url, data, format="json")
+        self.assertEqual(response.status_code, 400)
+        self.assertContains(
+            response,
+            f"Charge Item Definition with id {invalid_charge_item_id}",
+            status_code=400,
+        )
+
+    def test_create_activity_definition_with_invalid_healthcare_service(self):
+        self.client.force_authenticate(user=self.superuser)
+        another_facility = self.create_facility(
+            name="Another Facility", user=self.superuser
+        )
+        invalid_healthcare_service_id = self.generate_healthcare_service(
+            another_facility
+        ).external_id
+        data = self.generate_activity_definition_data(
+            specimen_requirements=[
+                self.generate_specimen_definition(self.facility).external_id
+            ],
+            observation_result_requirements=[
+                self.generate_observation_definition(self.facility).external_id
+            ],
+            healthcare_service=invalid_healthcare_service_id,
+            charge_item_definitions=[
+                self.charge_item_definition(self.facility).external_id
+            ],
+            locations=[self.facility_location.external_id],
+        )
+        response = self.client.post(self.base_url, data, format="json")
+        self.assertEqual(response.status_code, 400)
+        self.assertContains(
+            response,
+            "Healthcare Service must be from the same facility",
+            status_code=400,
+        )
+
+    def test_create_activity_definition_with_duplicate_slug(self):
+        self.client.force_authenticate(user=self.superuser)
+        self.create_activity_definition(slug="duplicate-slug", facility=self.facility)
+        data = self.generate_activity_definition_data(
+            specimen_requirements=[
+                self.generate_specimen_definition(self.facility).external_id
+            ],
+            observation_result_requirements=[
+                self.generate_observation_definition(self.facility).external_id
+            ],
+            healthcare_service=self.generate_healthcare_service(
+                self.facility
+            ).external_id,
+            charge_item_definitions=[
+                self.charge_item_definition(self.facility).external_id
+            ],
+            locations=[self.facility_location.external_id],
+            slug="duplicate-slug",
+        )
+        response = self.client.post(self.base_url, data, format="json")
+        self.assertEqual(response.status_code, 400)
+        self.assertContains(
+            response,
+            "Activity Definition with this slug already exists.",
+            status_code=400,
+        )
