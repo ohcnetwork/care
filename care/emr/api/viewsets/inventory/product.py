c37a9569fa9807e0534ff2fc4d617d4a3e3c5734
nandkishorr <rachulr7@gmail.com>
2025-09-03 13:09:13 +0530

    fix:added update pydantic model and update validation

--- care/emr/api/viewsets/inventory/product.py
+++ care/emr/api/viewsets/inventory/product.py
@@ -12,7 +12,11 @@ from care.emr.api.viewsets.base import (
     EMRUpsertMixin,
 )
 from care.emr.models.product import Product
-from care.emr.resources.inventory.product.spec import ProductReadSpec, ProductWriteSpec
+from care.emr.resources.inventory.product.spec import (
+    ProductReadSpec,
+    ProductUpdateSpec,
+    ProductWriteSpec,
+)
 from care.facility.models.facility import Facility
 from care.security.authorization.base import AuthorizationController
 
@@ -33,6 +37,7 @@ class ProductViewSet(
 ):
     database_model = Product
     pydantic_model = ProductWriteSpec
+    pydantic_update_model = ProductUpdateSpec
     pydantic_read_model = ProductReadSpec
     filterset_class = ProductFilters
     filter_backends = [filters.DjangoFilterBackend, OrderingFilter]
@@ -43,13 +48,16 @@ class ProductViewSet(
             Facility, external_id=self.kwargs["facility_external_id"]
         )
 
-    def perform_create(self, instance):
-        instance.facility = self.get_facility_obj()
+    def validate_charge_item_definition(self, instance):
         if (
             instance.charge_item_definition
             and instance.charge_item_definition.facility != instance.facility
         ):
             raise ValidationError("Invalid Charge Item")
+
+    def perform_create(self, instance):
+        instance.facility = self.get_facility_obj()
+        self.validate_charge_item_definition(instance)
         if (
             instance.product_knowledge
             and instance.product_knowledge.facility
@@ -57,6 +65,10 @@ class ProductViewSet(
         ):
             raise ValidationError("Invalid Product Knowledge")
         super().perform_create(instance)
+
+    def perform_update(self, instance):
+        self.validate_charge_item_definition(instance)
+        return super().perform_update(instance)
 
     def authorize_create(self, instance):
         facility = self.get_facility_obj()
