6368f323c01e007fe584c41aa86cce4d8a9a3a57
Prafful Sharma <praffulsharma1230@gmail.com>
2025-09-09 03:29:21 +0530

    fixed changes

--- scripts/start-dev.sh
+++ scripts/start-dev.sh
@@ -13,7 +13,7 @@ python manage.py compilemessages -v 0
 echo "starting server..."
 if [[ "${ATTACH_DEBUGGER}" == "true" ]]; then
   echo "waiting for debugger..."
-  python -m debugpy --wait-for-client --listen 0.0.0.0:9876 manage.py runserver_plus 0.0.0.0:9000
+  python -m debugpy --wait-for-client --listen 0.0.0.0:9876 manage.py runserver_plus 0.0.0.0:9000 --print-sql
 else
-  python manage.py runserver_plus 0.0.0.0:9000
+  python manage.py runserver_plus 0.0.0.0:9000 --print-sql
 fi
