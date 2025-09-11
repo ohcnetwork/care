5220d0f4edd6dae6268722d5e9b8bcaf211254b9
Aakash Singh <mail@singhaakash.dev>
2025-09-11 19:11:18 +0530

    Change total_net and total_gross fields to DecimalField with 2 decimal places; add migration to round existing values.

--- care/emr/resources/invoice/spec.py
+++ care/emr/resources/invoice/spec.py
@@ -1,4 +1,5 @@
 import datetime
+from decimal import Decimal
 from enum import Enum
 
 from pydantic import UUID4
@@ -56,14 +57,12 @@ class InvoiceWriteSpec(BaseInvoiceSpec):
 class InvoiceReadSpec(BaseInvoiceSpec):
     """Invoice read specification"""
 
-    total_net: float
-    total_gross: float
+    total_net: Decimal
+    total_gross: Decimal
 
     @classmethod
     def perform_extra_serialization(cls, mapping, obj):
         mapping["id"] = obj.external_id
-        mapping["total_net"] = str(round(obj.total_net, 2))
-        mapping["total_gross"] = str(round(obj.total_gross, 2))
 
 
 class InvoiceRetrieveSpec(InvoiceReadSpec):
