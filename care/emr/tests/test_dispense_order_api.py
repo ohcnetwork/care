c4896c82db81353d00c0b0d036771833a6deb007
nandkishorr <rachulr7@gmail.com>
2025-10-29 15:17:36 +0530

    feat:testcases for create dispense order

--- care/emr/tests/test_dispense_order_api.py
+++ care/emr/tests/test_dispense_order_api.py
@@ -19,7 +19,11 @@ class DispenseOrderAPITestCase(CareAPITestBase):
         self.facility_organization = self.create_facility_organization(
             facility=self.facility, name="Test Org"
         )
-        self.location = self.create_facility_location(self.facility, name="Pharmacy")
+        self.location = self.create_facility_location(
+            self.facility,
+            name="Pharmacy",
+            facility_organization=self.facility_organization,
+        )
 
         self.dispense_order_data = {
             "status": MedicationDispenseOrderStatusOptions.draft,
@@ -40,14 +44,14 @@ class DispenseOrderAPITestCase(CareAPITestBase):
             ]
         )
 
-    def create_facility_location(self, facility, **kwargs):
+    def create_facility_location(self, facility, facility_organization, **kwargs):
         from care.emr.models import FacilityLocation, FacilityLocationOrganization
 
         location = baker.make(FacilityLocation, facility=facility, **kwargs)
         baker.make(
             FacilityLocationOrganization,
             location=location,
-            organization=self.facility_organization,
+            organization=facility_organization,
         )
         return location
 
@@ -65,3 +69,92 @@ class DispenseOrderAPITestCase(CareAPITestBase):
                 "external_id": str(dispense_order_external_id),
             },
         )
+
+    # Testcases for creating dispense order
+
+    def test_create_dispense_order_as_superuser(self):
+        self.client.force_authenticate(user=self.superuser)
+        response = self.client.post(
+            self.generate_base_url(self.facility.external_id),
+            data=self.dispense_order_data,
+            format="json",
+        )
+        self.assertEqual(response.status_code, 200)
+        get_response = self.client.get(
+            self.get_detail_url(self.facility.external_id, response.data["id"]),
+            format="json",
+        )
+        self.assertEqual(get_response.status_code, 200)
+        self.assertEqual(get_response.data["id"], response.data["id"])
+        self.assertEqual(get_response.data["name"], self.dispense_order_data["name"])
+
+    def test_create_dispense_order_as_pharmacist(self):
+        self.attach_role_facility_organization_user(
+            user=self.user,
+            role=self.pharmacist_role,
+            facility_organization=self.facility_organization,
+        )
+        self.client.force_authenticate(user=self.user)
+        response = self.client.post(
+            self.generate_base_url(self.facility.external_id),
+            data=self.dispense_order_data,
+            format="json",
+        )
+        self.assertEqual(response.status_code, 200)
+        get_response = self.client.get(
+            self.get_detail_url(self.facility.external_id, response.data["id"]),
+            format="json",
+        )
+        self.assertEqual(get_response.status_code, 200)
+        self.assertEqual(get_response.data["id"], response.data["id"])
+
+    def test_create_dispense_order_as_user_without_permission(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.post(
+            self.generate_base_url(self.facility.external_id),
+            data=self.dispense_order_data,
+            format="json",
+        )
+        self.assertEqual(response.status_code, 403)
+        self.assertIn(
+            "You do not have permission to create dispense order",
+            response.data["detail"],
+        )
+
+    def test_create_dispense_order_as_user_with_location_write_permission(self):
+        self.attach_role_facility_organization_user(
+            user=self.user,
+            role=self.role,
+            facility_organization=self.facility_organization,
+        )
+        self.client.force_authenticate(user=self.user)
+        response = self.client.post(
+            self.generate_base_url(self.facility.external_id),
+            data=self.dispense_order_data,
+            format="json",
+        )
+        self.assertEqual(response.status_code, 200)
+        get_response = self.client.get(
+            self.get_detail_url(self.facility.external_id, response.data["id"]),
+            format="json",
+        )
+        self.assertEqual(get_response.status_code, 200)
+        self.assertEqual(get_response.data["id"], response.data["id"])
+
+    def test_create_dispense_order_in_location_of_different_facility(self):
+        other_facility = self.create_facility(
+            user=self.superuser, name="Other Facility"
+        )
+        self.attach_role_facility_organization_user(
+            user=self.user,
+            role=self.role,
+            facility_organization=self.facility_organization,
+        )
+        self.client.force_authenticate(user=self.user)
+        response = self.client.post(
+            self.generate_base_url(other_facility.external_id),
+            data=self.dispense_order_data,
+            format="json",
+        )
+        self.assertEqual(response.status_code, 403)
+        self.assertIn("Location must be in the same facility", response.data["detail"])
