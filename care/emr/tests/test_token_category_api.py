e6820a8b717b15b2aa68e520564c26c9b9594893
nandkishorr <rachulr7@gmail.com>
2025-11-03 16:33:24 +0530

    feat:added testcases for create token category

--- care/emr/tests/test_token_category_api.py
+++ care/emr/tests/test_token_category_api.py
@@ -55,3 +55,50 @@ class TokenCategoryAPITestCase(CareAPITestBase):
             facility=facility,
             **kwargs,
         )
+
+    # Test cases for create token category
+
+    def test_create_token_category_as_superuser(self):
+        self.client.force_authenticate(user=self.superuser)
+        response = self.client.post(self.base_url, self.category_data, format="json")
+        self.assertEqual(response.status_code, 200)
+        get_response = self.client.get(
+            self.generate_detail_url(
+                facility=str(self.facility.external_id),
+                external_id=response.data["id"],
+            )
+        )
+        self.assertEqual(get_response.status_code, 200)
+        self.assertEqual(get_response.data["id"], response.data["id"])
+        self.assertEqual(get_response.data["name"], self.category_data["name"])
+        self.assertEqual(
+            get_response.data["shorthand"], self.category_data["shorthand"]
+        )
+
+    def test_create_token_category_as_user_with_permissions(self):
+        self.client.force_authenticate(user=self.user)
+        self.assign_role_to_user_in_facility(
+            user=self.user,
+            role=self.role,
+            facility_organization=self.facility_organization,
+        )
+        response = self.client.post(self.base_url, self.category_data, format="json")
+        self.assertEqual(response.status_code, 200)
+        get_response = self.client.get(
+            self.generate_detail_url(
+                facility=str(self.facility.external_id),
+                external_id=response.data["id"],
+            )
+        )
+        self.assertEqual(get_response.status_code, 200)
+        self.assertEqual(get_response.data["id"], response.data["id"])
+        self.assertEqual(get_response.data["name"], self.category_data["name"])
+        self.assertEqual(
+            get_response.data["shorthand"], self.category_data["shorthand"]
+        )
+
+    def test_create_token_category_as_user_without_permissions(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.post(self.base_url, self.category_data, format="json")
+        self.assertEqual(response.status_code, 403)
+        self.assertIn("Access Denied to Token Category", response.data["detail"])
