8b79fe4e51e5f8e5edd0ccc309fbc9b89a0758c3
Nandkishor R <96693626+nandkishorr@users.noreply.github.com>
2025-11-25 14:08:24 +0530

    Updated date range filter for account api (#3368)
    * added date filter
    
    * added date filter
    
    * refact:updated the date filter

--- care/emr/api/viewsets/account.py
+++ care/emr/api/viewsets/account.py
@@ -31,7 +31,7 @@ class AccountFilters(filters.FilterSet):
     name = filters.CharFilter(lookup_expr="icontains")
     billing_status = filters.CharFilter(lookup_expr="iexact")
     patient = filters.UUIDFilter(field_name="patient__external_id")
-    created_date = filters.DateRangeFilter(field_name="created_date")
+    created_date = filters.DateTimeFromToRangeFilter(field_name="created_date")
 
 
 class AccountViewSet(
