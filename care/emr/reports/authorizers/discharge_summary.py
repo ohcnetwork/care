cde2543f633f74156c892fedcd50248f1d3522d4
Prafful Sharma <praffulsharma1230@gmail.com>
2025-12-01 14:45:17 +0530

    merge report auth registry to report type

--- care/emr/reports/authorizers/discharge_summary.py
+++ care/emr/reports/authorizers/discharge_summary.py
@@ -1,9 +1,5 @@
 from care.emr.reports.authorizers.encounter import EncounterReportAuthorizer
-from care.emr.reports.report_authorizer_registry import ReportAuthorizerRegistry
 
 
 class DischargeSummaryReportAuthorizer(EncounterReportAuthorizer):
     pass
-
-
-ReportAuthorizerRegistry.register("discharge_summary", DischargeSummaryReportAuthorizer)
