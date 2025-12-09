b2808ceecb005ea2cde6f71b92136b63a079f2d6
Prafful Sharma <praffulsharma1230@gmail.com>
2025-12-09 20:19:35 +0530

    minor fixes

--- care/emr/reports/renderer/generators/weasyprint_generator.py
+++ care/emr/reports/renderer/generators/weasyprint_generator.py
@@ -21,12 +21,6 @@ class WeasyPrintGenerator(BaseOutputGenerator):
     options_model = WeasyPrintGeneratorOptions
 
     def __init__(self):
-        try:
-            self.HTML = HTML
-            self.CSS = CSS
-        except ImportError as e:
-            msg = "WeasyPrint is not installed. Install it with: pip install weasyprint"
-            raise ImportError(msg) from e
         self.HTML = HTML
         self.CSS = CSS
 
