b5e031c15136d18b0d9bfc418ff05e66fa01a0de
nandkishorr <rachulr7@gmail.com>
2025-12-16 17:18:57 +0530

    feat:added patient context builder

---
+++ care/emr/reports/context_builder/data_points/patient.py
@@ -0,0 +1,43 @@
+from care.emr.models.patient import Patient
+from care.emr.reports.context_builder.data_point_registry import DataPointRegistry
+from care.emr.reports.context_builder.data_points.base import (
+    Field,
+    SingleObjectContextBuilder,
+)
+
+
+class PatientContextBuilder(SingleObjectContextBuilder):
+    standalone_context = True
+    __slug__ = "patient_base"
+    __associating_model__ = Patient
+    __display_name__ = "Patient Report"
+    __description__ = "Report context for patient-based reports"
+    context_key = "patient"
+
+    name = Field(
+        display="Patient Name",
+        preview_value="Jane Doe",
+        description="Full name of the patient",
+    )
+
+    gender = Field(
+        display="Patient Gender",
+        preview_value="Male",
+        description="Gender of the patient",
+    )
+    age = Field(
+        display="Patient Age",
+        mapping=lambda p: p.get_age(),
+        preview_value="45 Y",
+        description="Age of the patient",
+    )
+
+    accounts = Field(
+        display="Patient Accounts",
+        target_context="care.emr.reports.context_builder.data_points.account.AccountContextBuilder",
+        preview_value="",
+        description="Accounts associated with the patient",
+    )
+
+
+DataPointRegistry.register(PatientContextBuilder)
