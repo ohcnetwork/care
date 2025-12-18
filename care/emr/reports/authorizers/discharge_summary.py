a36c741b5647eacc1ab1126eab2c485fcd7ceaef
nandkishorr <rachulr7@gmail.com>
2025-12-19 00:48:54 +0530

    updated mapping

--- care/emr/reports/authorizers/discharge_summary.py
+++ care/emr/reports/authorizers/discharge_summary.py
@@ -1,10 +1,5 @@
 from care.emr.reports.authorizers.encounter import EncounterReportAuthorizer
-from care.emr.reports.authorizers.patient import PatientReportAuthorizer
 
 
 class DischargeSummaryReportAuthorizer(EncounterReportAuthorizer):
-    pass
-
-
-class DischargeBillReportAuthorizer(PatientReportAuthorizer):
     pass
--- care/emr/reports/context_builder/data_points/monetary_component.py
+++ care/emr/reports/context_builder/data_points/monetary_component.py
@@ -18,24 +18,27 @@ class MonetaryComponentContextBuilder(QuerysetContextBuilder):
         preview_value="Base",
         description="Type of the monetary component",
         mapping=lambda mc: MONETARY_COMPONENT_TYPE_DISPLAY.get(
-            mc.monetary_component_type, mc.monetary_component_type.title()
+            mc.get("monetary_component_type"),
+            mc.get("monetary_component_type", "").title(),
         ),
     )
     code = Field(
         display="Code",
         preview_value="MRP",
-        description="Coding representing the monetary component",
-        mapping=lambda mc: mc.code.get("display", "") if mc.code else "",
+        description="Code representing the monetary component",
+        mapping=lambda mc: mc.get("code").get("display", "") if mc.get("code") else "",
     )
     factor = Field(
         display="Factor",
         preview_value="10.0",
         description="Factor applied to the base amount for this monetary component",
+        mapping=lambda mc: mc.get("factor"),
     )
     amount = Field(
         display="Amount",
         preview_value="100.00",
-        description="Fixed amount for this monetary component",
+        description="Amount for this monetary component",
+        mapping=lambda mc: mc.get("amount"),
     )
 
     def get_context(self):
