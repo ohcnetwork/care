01ec20df4fd100690a3d9209a8403f6b9078792a
Nandkishor R <96693626+nandkishorr@users.noreply.github.com>
2026-01-13 18:05:33 +0530

    fix: added status to CreateDispenseOrder spec (#3427)
    * fix: added status to dispense order and validation
    
    * fix:updated status filter
    
    * fix:made some validation changes
    
    * fix:review changes
    
    * fix status default value
    
    ---------
    
    Co-authored-by: Prafful Sharma <115104695+praffq@users.noreply.github.com>

--- care/emr/resources/medication/dispense/spec.py
+++ care/emr/resources/medication/dispense/spec.py
@@ -106,10 +106,16 @@ class BaseMedicationDispenseSpec(EMRResource):
     substitution: MedicationDispenseSubstitution | None = None
 
 
+class CreateDispenseOrderStatusOptions(str, Enum):
+    draft = "draft"
+    in_progress = "in_progress"
+
+
 class CreateDispenseOrder(BaseModel):
     name: str | None = None
     note: str | None = None
     alternate_identifier: str
+    status: CreateDispenseOrderStatusOptions = CreateDispenseOrderStatusOptions.draft
 
 
 class MedicationDispenseWriteSpec(BaseMedicationDispenseSpec):
@@ -171,7 +177,7 @@ class MedicationDispenseWriteSpec(BaseMedicationDispenseSpec):
                 raise ValidationError("Prescription is not active")
             if not dispense_order_obj:
                 dispense_order_obj = DispenseOrder.objects.create(
-                    status=MedicationDispenseOrderStatusOptions.draft.value,
+                    status=self.create_dispense_order.status,
                     alternate_identifier=self.create_dispense_order.alternate_identifier,
                     patient=obj.patient,
                     location=obj.location,
