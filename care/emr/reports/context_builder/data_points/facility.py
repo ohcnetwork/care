46cccf7c234e273785965f4e4c7c4cae4c26540d
nandkishorr <rachulr7@gmail.com>
2026-01-13 13:47:38 +0530

    refact:sorted field schema

--- care/emr/reports/context_builder/data_points/facility.py
+++ care/emr/reports/context_builder/data_points/facility.py
@@ -2,14 +2,11 @@ from care.emr.reports.context_builder.data_points.base import (
     Field,
     SingleObjectContextBuilder,
 )
-from care.facility.models.facility import Facility
 
 
 class FacilityContextBuilder(SingleObjectContextBuilder):
     def get_context(self):
-        return Facility.objects.get(
-            id=getattr(self.parent_context, self.parent_context)
-        )
+        return getattr(self.parent_context, self.parent_attribute)
 
     name = Field(
         display="Facility Name",
@@ -36,9 +33,4 @@ class FacilityContextBuilder(SingleObjectContextBuilder):
         display="Contact Number",
         preview_value="+1-555-1234",
         description="Contact number of the healthcare facility",
-    )
-    cover_image_url = Field(
-        display="Cover Image URL",
-        preview_value="http://example.com/image.jpg",
-        description="URL of the facility's cover image",
     )
