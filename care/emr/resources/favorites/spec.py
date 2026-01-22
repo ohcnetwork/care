a68ae79b8563455a0577701f4599be2478988119
abhimanyurajeesh <abhimanyurajeesh@gmail.com>
2026-01-23 05:06:06 +0530

    Integrate favorites functionality into QuestionnaireViewSet and update FavoriteResourceChoices

--- care/emr/resources/favorites/spec.py
+++ care/emr/resources/favorites/spec.py
@@ -8,3 +8,4 @@ class FavoriteResourceChoices(str, Enum):
     charge_item_definition = "charge_item_definition"
     product_knowledge = "product_knowledge"
     observation_definition = "observation_definition"
+    questionnaire = "questionnaire"
