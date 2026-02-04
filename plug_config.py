f23d8573fa0da3c242f5f6ce097b86005c040505
Aakash Singh <mail@singhaakash.dev>
2026-02-04 11:36:02 +0530

    Fix plug configuration for care_radiology

--- plug_config.py
+++ plug_config.py
@@ -3,12 +3,12 @@ from plugs.plug import Plug
 
 
 plugs = [
-    {
-        name=care_radiology,
+    Plug(
+        name="care_radiology",
         package_name="git+https://github.com/10bedicu/care_radiology.git",
         version="@main",
         configs={},
-    }
+    )
 ]
 
 manager = PlugManager(plugs)
