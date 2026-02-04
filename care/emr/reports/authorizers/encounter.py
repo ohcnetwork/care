5f1ef971e09dcc7b65077505d36839899cf94cb9
Nandkishor R <96693626+nandkishorr@users.noreply.github.com>
2026-02-04 19:33:32 +0530

    refact:added new permission for template generation in completed encounter (#3512)
    * refact:added new permission for completed encounter
    
    * feat:updated the report file name

--- care/emr/reports/authorizers/encounter.py
+++ care/emr/reports/authorizers/encounter.py
@@ -14,5 +14,5 @@ class EncounterReportAuthorizer(BaseReportAuthorizer):
     def authorize_write(self, user, associating_id: str) -> bool:
         encounter_obj = get_object_or_404(Encounter, external_id=associating_id)
         return AuthorizationController.call(
-            "can_update_encounter_obj", user, encounter_obj
+            "can_generate_report_for_encounter", user, encounter_obj
         )
