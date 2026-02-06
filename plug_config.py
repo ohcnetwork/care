44e9fe834f71443b290caa90119562f9e2ff461e
Vysakh Premkumar <84713473+tellmeY18@users.noreply.github.com>
2026-02-06 12:51:24 +0530

    Refactor plug configuration to include new plug

--- plug_config.py
+++ plug_config.py
@@ -1,20 +1,25 @@
 from plugs.manager import PlugManager
 from plugs.plug import Plug
 
-
 plugs = [
-    Plug(
-        name="care_radiology",
-        package_name="git+https://github.com/10bedicu/care_radiology.git",
-        version="@main",
-        configs={},
-    ),
-    Plug(
-        name="care_state_hmis",
-        package_name="git+https://github.com/10bedicu/care_state_hmis.git",
-        version="@main",
-        configs={},
-    )
+    Plug(
+        name="care_radiology",
+        package_name="git+https://github.com/10bedicu/care_radiology.git",
+        version="@main",
+        configs={},
+    ),
+    Plug(
+        name="care_state_hmis",
+        package_name="git+https://github.com/10bedicu/care_state_hmis.git",
+        version="@main",
+        configs={},
+    ),
+    Plug(
+        name="abdm",
+        package_name="git+https://github.com/10bedicu/care_abdm.git",
+        version="@develop",
+        configs={},
+    ),
 ]
 
 manager = PlugManager(plugs)
