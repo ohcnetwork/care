4214c0044c04d04ccb66bc6993ed10e884f036b4
Gigin George <mail.gigin@gmail.com>
2026-02-06 20:00:52 +0530

    Enable plugs care_state_hmis and abdm

--- plug_config.py
+++ plug_config.py
@@ -1,5 +1,18 @@
 from plugs.manager import PlugManager
 
-plugs = []
+plugs = [
+    Plug(
+        name="care_state_hmis",
+        package_name="git+https://github.com/10bedicu/care_state_hmis.git",
+        version="@main",
+        configs={},
+    ),
+    Plug(
+        name="abdm",
+        package_name="git+https://github.com/10bedicu/care_abdm.git",
+        version="@develop",
+        configs={},
+    ),
+]
 
 manager = PlugManager(plugs)
