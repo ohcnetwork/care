14d0a303e32bf25315fadb219d8ad014207ceae1
nandkishorr <rachulr7@gmail.com>
2026-02-10 02:31:59 +0530

    feat:added reference based validation

--- care/emr/api/viewsets/batch_request.py
+++ care/emr/api/viewsets/batch_request.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.db import transaction
 from drf_spectacular.utils import extend_schema
 from pydantic import BaseModel, Field
+from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
@@ -53,11 +54,38 @@ class BatchRequestView(GenericViewSet):
     def get_exception_handler(self):
         return emr_exception_handler
 
+    def validate_replacements(self, requests):
+        reference_id_order = {}
+        for index, req in enumerate(requests.requests):
+            reference_id_order[req.reference_id] = index
+
+        for req in requests.requests:
+            current_index = reference_id_order[req.reference_id]
+            for replacement in req.replacements:
+                source_reference_id = replacement.source_path.reference_id
+                if source_reference_id not in reference_id_order:
+                    error_msg = (
+                        f"Invalid source_path reference_id : {source_reference_id}"
+                    )
+                    raise ValidationError(error_msg)
+                if reference_id_order[source_reference_id] >= current_index:
+                    raise ValidationError(
+                        "Source request must come before the current request."
+                    )
+
+                value_reference_id = replacement.value_path.reference_id
+                if value_reference_id != req.reference_id:
+                    error_msg = (
+                        f"Invalid value_path reference_id : {value_reference_id}"
+                    )
+                    raise ValidationError(error_msg)
+
     @extend_schema(
         request=BatchRequest,
     )
     def create(self, request, *args, **kwargs):
         requests = BatchRequest(**request.data)
+        self.validate_replacements(requests)
         errored = False
         loop = 0
         data_references = {}
