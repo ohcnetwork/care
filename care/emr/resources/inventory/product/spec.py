a87d22e554608651c983bbe5a21993b7592f94ac
vigneshhari <vichuhari100@gmail.com>
2026-02-12 19:16:56 +0530

    Purchase Price Added

--- care/emr/resources/inventory/product/spec.py
+++ care/emr/resources/inventory/product/spec.py
@@ -1,9 +1,10 @@
 import datetime
+from decimal import Decimal
 from enum import Enum
 
 from django.conf import settings
 from jsonschema import validate
-from pydantic import UUID4, BaseModel, field_validator
+from pydantic import UUID4, BaseModel, Field, field_validator
 
 from care.emr.extensions.base import ExtensionResource
 from care.emr.extensions.validator import ExtensionValidator
@@ -39,6 +40,8 @@ class BaseProductSpec(EMRResource):
     expiration_date: datetime.datetime | None = None
     extensions: dict
     standard_pack_size: int | None = None
+    purchase_price: Decimal | None = Field(max_digits=20, decimal_places=6)
+
 
     @field_validator("extensions")
     @classmethod
