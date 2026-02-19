1dbdc659aca48daa86bd0f6a1f9afc8555331e69
Nandkishor R <96693626+nandkishorr@users.noreply.github.com>
2026-02-19 13:51:29 +0530

    feat:added primary_encounter in account datapoint and modified account,encounter datapoint structure (#3535)
    * feat:added primary_encounter and modified encounter datapoint structure
    
    * few changes to account datapoints
    
    * feat:added some account fields

--- care/emr/reports/context_builder/data_points/charge_items.py
+++ care/emr/reports/context_builder/data_points/charge_items.py
@@ -1,6 +1,6 @@
 from decimal import Decimal
 
-from django.db.models import Sum
+from django.db.models import Q, Sum
 from django_filters import rest_framework as filters
 
 from care.emr.models.charge_item import ChargeItem
@@ -8,6 +8,7 @@ from care.emr.models.resource_category import ResourceCategory
 from care.emr.reports.context_builder.data_points.base import (
     Field,
     QuerysetContextBuilder,
+    SingleObjectContextBuilder,
 )
 from care.emr.reports.context_builder.data_points.invoice import (
     ChargeItemInvoiceContextBuilder,
@@ -130,7 +131,7 @@ class CategoryChargeItemContextBuilder(ChargeItemContextBuilder):
 
 
 class AccountChargeItemCategoryContextBuilder(QuerysetContextBuilder):
-    def get_category_charge_items_summary(self, account):
+    def get_category_charge_items_summary(self, account, is_refund=False):
         categories = ResourceCategory.objects.filter(
             resource_type="charge_item_definition",
             facility_id=self.parent_context.facility_id,
@@ -138,11 +139,21 @@ class AccountChargeItemCategoryContextBuilder(QuerysetContextBuilder):
         )
         summary = []
         for category in categories:
-            charge_items = ChargeItem.objects.filter(
-                account_id=account.id,
-                charge_item_definition__category=category,
-                status__in=ACTIVE_CHARGE_ITEM_STATUSES,
-            )
+            if is_refund:
+                charge_items = ChargeItem.objects.filter(
+                    account_id=account.id,
+                    charge_item_definition__category=category,
+                    status__in=ACTIVE_CHARGE_ITEM_STATUSES,
+                    paid_invoice__is_refund=True,
+                )
+            else:
+                charge_items = ChargeItem.objects.filter(
+                    account_id=account.id,
+                    charge_item_definition__category=category,
+                    status__in=ACTIVE_CHARGE_ITEM_STATUSES,
+                ).filter(
+                    Q(paid_invoice__isnull=True) | Q(paid_invoice__is_refund=False)
+                )
             if not charge_items.exists():
                 continue
             paid_charge_items = charge_items.filter(
@@ -226,4 +237,29 @@ class AccountChargeItemCategoryContextBuilder(QuerysetContextBuilder):
         if item.get("total_billed_charge_items")
         else "0.00",
         description="Total billed price of charge items in this category",
+    )
+
+
+class AccountReturnedChargeItemCategoryContextBuilder(
+    AccountChargeItemCategoryContextBuilder
+):
+    def get_context(self):
+        return self.get_category_charge_items_summary(
+            self.parent_context, is_refund=True
+        )
+
+
+class AccountChargeItemCategorySummaryContextBuilder(SingleObjectContextBuilder):
+    category_charge_items = Field(
+        display="Category Charge Items",
+        preview_value="",
+        target_context=AccountChargeItemCategoryContextBuilder,
+        description="Charge items under this category",
+    )
+
+    category_returned_charge_items = Field(
+        display="Category Returned Charge Items",
+        preview_value="",
+        target_context=AccountReturnedChargeItemCategoryContextBuilder,
+        description="Returned charge items under this category",
     )
