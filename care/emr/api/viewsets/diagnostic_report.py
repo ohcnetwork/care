5287e2bc29f66d24836eb12b6969736ab435f23a
Nandkishor R <96693626+nandkishorr@users.noreply.github.com>
2026-02-23 08:26:05 +0530

    fix:updated exception validations in diagnostic report api (#3540)
    fix:updated the validations

--- care/emr/api/viewsets/diagnostic_report.py
+++ care/emr/api/viewsets/diagnostic_report.py
@@ -2,7 +2,7 @@ from django_filters import rest_framework as filters
 from drf_spectacular.utils import extend_schema
 from pydantic import UUID4, BaseModel
 from rest_framework.decorators import action
-from rest_framework.exceptions import ValidationError
+from rest_framework.exceptions import PermissionDenied, ValidationError
 from rest_framework.filters import OrderingFilter
 from rest_framework.response import Response
 
@@ -88,7 +88,7 @@ class DiagnosticReportViewSet(
             self.request.user,
             get_object_or_404(ServiceRequest, external_id=instance.service_request),
         ):
-            raise ValidationError(
+            raise PermissionDenied(
                 "You do not have permission to write this diagnostic report"
             )
 
@@ -98,7 +98,7 @@ class DiagnosticReportViewSet(
             self.request.user,
             model_instance.service_request,
         ):
-            raise ValidationError(
+            raise PermissionDenied(
                 "You do not have permission to write this diagnostic report"
             )
 
@@ -109,8 +109,8 @@ class DiagnosticReportViewSet(
             model_instance.service_request,
         ):
             return
-        raise ValidationError(
-            "You do not have permission to write this diagnostic report"
+        raise PermissionDenied(
+            "You do not have permission to read this diagnostic report"
         )
 
     def get_queryset(self):
