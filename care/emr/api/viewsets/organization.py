1c1f22df22bfbe108c1382d49a4153f03835a1ed
vigneshhari <vichuhari100@gmail.com>
2026-03-22 15:45:02 +0530

    Update API to show accessible organizations as well

--- care/emr/api/viewsets/organization.py
+++ care/emr/api/viewsets/organization.py
@@ -252,15 +252,20 @@ class OrganizationViewSet(EMRModelViewSet):
         return Response({"count": len(data), "results": data})
 
     @action(detail=False, methods=["GET"])
-    def manageable_organizations(self, request, *args, **kwargs):
+    def accessible_role_organizations(self, request, *args, **kwargs):
         my_organizations = OrganizationUser.objects.filter(
             organization__org_type=OrganizationTypeChoices.role.value, user=request.user
         )
-        managing_organization = Organization.objects.filter(
-            managing_organizations__overlap=list(
-                my_organizations.values_list("organization_id", flat=True)
+        my_organizations_ids = list(my_organizations.values_list("organization_id", flat=True))
+        if not self.request.user.is_superuser:
+            managing_organization = Organization.objects.filter(org_type=OrganizationTypeChoices.role.value)
+        else:
+            managing_organization = Organization.objects.filter(
+                Q(managing_organizations__overlap=list(
+                    my_organizations_ids
+                )) | Q(id__in=my_organizations_ids)
             )
-        )
+
         rendered_data = [
             OrganizationReadSpec.serialize(org).to_json()
             for org in managing_organization
