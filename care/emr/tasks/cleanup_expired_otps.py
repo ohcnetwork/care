3fe211c0696798e0c0beb626b332b99be1ae3c6e
praffq <praffulsharma1230@gmail.com>
2026-05-06 23:26:48 +0530

    updated clean up time cutoff

--- care/emr/tasks/cleanup_expired_otps.py
+++ care/emr/tasks/cleanup_expired_otps.py
@@ -3,6 +3,7 @@ from logging import Logger
 
 from celery import shared_task
 from celery.utils.log import get_task_logger
+from django.conf import settings
 
 from care.facility.models.patient import PatientMobileOTP
 from care.utils.time_util import care_now
@@ -13,9 +14,9 @@ logger: Logger = get_task_logger(__name__)
 @shared_task
 def cleanup_expired_otps():
     """
-    Soft-deletes PatientMobileOTP rows older than 24 hours
+    Soft-deletes PatientMobileOTP rows older than the lockout window
     """
-    cutoff = care_now() - timedelta(hours=24)
+    cutoff = care_now() - timedelta(minutes=settings.OTP_LOCKOUT_MINUTES)
     count = PatientMobileOTP.objects.filter(created_date__lt=cutoff).update(
         deleted=True
     )
