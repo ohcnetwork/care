87da2622c8b8cd39e093397ae336df5e859a5709
Vipul Pandey <157928251+vipulpandey21@users.noreply.github.com>
2026-05-28 20:05:19 +0530

    Fix PlugConfig cache miss for empty list (#3430)
    Fix PlugConfig cache miss for empty list (#3430)
    
    ---------
    
    Co-authored-by: Vipul Pandey <157928251+19vip@users.noreply.github.com>

--- care/users/api/viewsets/plug_config.py
+++ care/users/api/viewsets/plug_config.py
@@ -7,17 +7,13 @@ from care.users.api.serializers.plug_config import PlugConfigSerializer
 from care.users.models import PlugConfig
 
 
-class PlugConfigViewset(
-    ModelViewSet,
-    GenericViewSet,
-):
+class PlugConfigViewset(ModelViewSet, GenericViewSet):
     lookup_field = "slug"
     serializer_class = PlugConfigSerializer
     queryset = PlugConfig.objects.all().order_by("slug")
     cache_key = "care_plug_viewset_list"
 
     def list(self, request, *args, **kwargs):
-        # Cache data and return
         response = cache.get(self.cache_key)
         if response is None:
             serializer = self.get_serializer(self.get_queryset(), many=True)
@@ -26,16 +22,16 @@ class PlugConfigViewset(
         return Response({"configs": response})
 
     def perform_create(self, serializer):
-        cache.delete(self.cache_key)
         serializer.save()
+        cache.delete(self.cache_key)
 
     def perform_update(self, serializer):
-        cache.delete(self.cache_key)
         serializer.save()
+        cache.delete(self.cache_key)
 
     def perform_destroy(self, instance):
-        cache.delete(self.cache_key)
         instance.delete()
+        cache.delete(self.cache_key)
 
     def get_authenticators(self):
         if getattr(self, "request", None) and self.request.method == "GET":
