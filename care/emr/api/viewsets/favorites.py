f40ecfbb6fbf5a2e3f2b9e63540a1f4d4abdd7a7
vigneshhari <vichuhari100@gmail.com>
2026-05-28 20:01:38 +0530

    Cleanup changes

--- care/emr/api/viewsets/favorites.py
+++ care/emr/api/viewsets/favorites.py
@@ -41,7 +41,7 @@ class EMRFavoritesMixin:
             favorite_lists_cache_key(user, self.FAVORITE_RESOURCE, facility)
         )
         if favorite_lists is None:
-            favorite_list = list(
+            favorite_lists = list(
                 dict.fromkeys(
                     UserResourceFavorites.objects.filter(
                         user=user,
@@ -54,7 +54,7 @@ class EMRFavoritesMixin:
             )
             cache.set(
                 favorite_lists_cache_key(user, self.FAVORITE_RESOURCE, facility),
-                favorite_list,
+                favorite_lists,
             )
         return Response({"lists": favorite_lists})
 
