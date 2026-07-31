31fb098b55ab5a4a3d31dd0583e5db3c398b651b
nandkishorr <rachulr7@gmail.com>
2026-07-31 20:42:22 +0530

    feat:added default filter setup and cleanup

--- care/emr/api/otp_viewsets/medication_request_prescription.py
+++ care/emr/api/otp_viewsets/medication_request_prescription.py
@@ -1,21 +1,25 @@
 from django_filters import rest_framework as filters
 
-from care.emr.api.otp_viewsets.base import QuerysetEnablerMixin
+from care.emr.api.otp_viewsets.base import OTPResourceType, QuerysetEnablerMixin
 from care.emr.api.viewsets.base import EMRBaseViewSet, EMRListMixin, EMRRetrieveMixin
 from care.emr.models.medication_request import MedicationRequestPrescription
 from care.emr.resources.medication.request_prescription.spec import (
     MedicationRequestPrescriptionReadSpec,
     MedicationRequestPrescriptionRetrieveMedicationsSpec,
 )
-from care.utils.filters.default_filter import DefaultOTPFilters
+from care.utils.filters.multiselect import MultiSelectFilter
 from config.patient_otp_authentication import (
     JWTTokenPatientAuthentication,
     OTPAuthenticatedPermission,
 )
 
 
-class OTPMedicationRequestPrescriptionFilters(DefaultOTPFilters):
+class OTPMedicationRequestPrescriptionFilters(filters.FilterSet):
     facility = filters.UUIDFilter(field_name="encounter__facility__external_id")
+    status = MultiSelectFilter(field_name="status")
+    encounter = filters.UUIDFilter(field_name="encounter__external_id")
+    patient = filters.UUIDFilter(field_name="patient__external_id")
+    created_date = filters.DateTimeFromToRangeFilter(field_name="created_date")
 
 
 class OTPMedicationRequestPrescriptionViewSet(
@@ -31,6 +35,7 @@ class OTPMedicationRequestPrescriptionViewSet(
     pydantic_retrieve_model = MedicationRequestPrescriptionRetrieveMedicationsSpec
     filterset_class = OTPMedicationRequestPrescriptionFilters
     filter_backends = [filters.DjangoFilterBackend]
+    resource_type = OTPResourceType.medication_request_prescription
 
     def get_queryset(self):
         return (
--- care/utils/filters/default_filter.py
+++ care/utils/filters/default_filter.py
@@ -1,8 +1,5 @@
 from django_filters import BooleanFilter
-from django_filters import rest_framework as filters
 from django_filters.constants import EMPTY_VALUES
-
-from care.utils.filters.multiselect import MultiSelectFilter
 
 
 class DefaultBooleanFilter(BooleanFilter):
@@ -16,11 +13,3 @@ class DefaultBooleanFilter(BooleanFilter):
         if value in EMPTY_VALUES:
             value = self.default_value
         return super().filter(qs, value)
-
-
-class DefaultOTPFilters(filters.FilterSet):
-    patient = filters.UUIDFilter(field_name="patient__external_id")
-    status = MultiSelectFilter(field_name="status")
-    facility = filters.UUIDFilter(field_name="facility__external_id")
-    encounter = filters.UUIDFilter(field_name="encounter__external_id")
-    created_date = filters.DateTimeFromToRangeFilter()
