d8f79e93d35e6761dcfd7a820e067405ca447fdd
nandkishorr <rachulr7@gmail.com>
2026-08-04 16:24:46 +0530

    testcases for cancel api

--- care/emr/tests/test_invoice_api.py
+++ care/emr/tests/test_invoice_api.py
@@ -1,7 +1,8 @@
 import random
-from datetime import UTC, datetime
+from datetime import UTC, datetime, timedelta
 from decimal import Decimal
 
+from django.test import override_settings
 from django.urls import reverse
 from model_bakery import baker
 
@@ -28,7 +29,6 @@ from care.utils.tests.base import CareAPITestBase
 
 class InvoiceAPITestBase(CareAPITestBase):
     def setUp(self):
-        super().setUp()
         NameIdentifierConfig.CACHED_CONFIG = {}
         PhoneNumberIdentifierConfig.CACHED_CONFIG = {}
         FacilityPatientNameIdentifierConfig.CACHED_CONFIG = {}
@@ -630,134 +630,124 @@ class InvoiceAPITestBase(CareAPITestBase):
         response_data = response.data
         self.assertEqual(response_data["id"], str(invoice.external_id))
 
+    # Testcases for cancel invoice api
 
-# class TestAttachAccountToInvoice(CareAPITestBase):
-#     def setUp(self):
-#         super().setUp()
-#         self.user = self.create_user()
-#         self.facility = self.create_facility(user=self.user)
-#         self.organization = self.create_facility_organization(facility=self.facility)
-#         self.patient = self.create_patient()
-#         self.encounter = self.create_encounter(
-#             patient=self.patient, facility=self.facility, organization=self.organization
-#         )
+    def cancel_invoice(self, external_id, reason):
+        """
+        Helper method to cancel an invoice.
+        """
+        url = reverse(
+            "invoice-cancel-invoice",
+            kwargs={
+                "facility_external_id": self.facility.external_id,
+                "external_id": external_id,
+            },
+        )
+        data = {"reason": reason}
+        return self.client.post(url, data, format="json")
 
-#         self.account = Account.objects.create(
-#             facility=self.facility,
-#             patient=self.patient,
-#             name=f"Account for {self.patient.name}",
-#             status=AccountStatusOptions.active.value,
-#             billing_status=AccountBillingStatusOptions.open.value,
-#         )
+    def test_cancel_invoice_with_superuser(self):
+        """
+        Test cancelling an invoice with a superuser.
+        """
+        self.client.force_authenticate(user=self.superuser)
+        invoice = self.create_invoice()
+        response = self.cancel_invoice(
+            invoice.external_id, InvoiceStatusOptions.cancelled.value
+        )
+        self.assertEqual(response.status_code, 200)
+        response_data = response.data
+        self.assertEqual(response_data["status"], InvoiceStatusOptions.cancelled.value)
 
-#         self.charge_item_1 = baker.make(
-#             "emr.ChargeItem",
-#             facility=self.facility,
-#             patient=self.patient,
-#             encounter=self.encounter,
-#             account=self.account,
-#             status=ChargeItemStatusOptions.billable.value,
-#             quantity=Decimal("1.00"),
-#             unit_price_components=[{"amount": 100, "monetary_component_type": "base"}],
-#             total_price_components=[{"amount": 100, "monetary_component_type": "base"}],
-#             total_price=Decimal("100.00"),
-#         )
-#         self.charge_item_2 = baker.make(
-#             "emr.ChargeItem",
-#             facility=self.facility,
-#             patient=self.patient,
-#             encounter=self.encounter,
-#             account=self.account,
-#             status=ChargeItemStatusOptions.billable.value,
-#             quantity=Decimal("1.00"),
-#             unit_price_components=[{"amount": 200, "monetary_component_type": "base"}],
-#             total_price_components=[{"amount": 200, "monetary_component_type": "base"}],
-#             total_price=Decimal("200.00"),
-#         )
+    def test_cancel_invoice_with_user_without_permission(self):
+        """
+        Test cancelling an invoice with a user without write permission.
+        """
+        permissions = [
+            InvoicePermissions.can_read_invoice.name,
+        ]
+        self.role = self.create_role_with_permissions(permissions)
+        self.attach_role_facility_organization_user(
+            self.organization, self.user, self.role
+        )
+        self.client.force_authenticate(user=self.user)
+        invoice = self.create_invoice()
+        response = self.cancel_invoice(
+            invoice.external_id, InvoiceStatusOptions.cancelled.value
+        )
+        self.assertEqual(response.status_code, 403)
+        self.assertEqual(response.data["detail"], "Cannot cancel invoice")
 
-#     def _get_url(self, invoice_external_id):
-#         return reverse(
-#             "invoice-attach-account-to-invoice",
-#             kwargs={
-#                 "facility_external_id": self.facility.external_id,
-#                 "external_id": invoice_external_id,
-#             },
-#         )
+    @override_settings(INVOICE_FREE_CANCEL_PERIOD_MINUTES=5)
+    def test_cancel_invoice_with_user_with_permission_within_period(self):
+        """
+        Test cancelling an invoice with a user with write permission.
+        """
+        self.attach_role_facility_organization_user(
+            self.organization, self.user, self.role
+        )
+        self.client.force_authenticate(user=self.user)
+        invoice = self.create_invoice()
+        response = self.cancel_invoice(
+            invoice.external_id, InvoiceStatusOptions.cancelled.value
+        )
+        self.assertEqual(response.status_code, 200)
+        response_data = response.data
+        self.assertEqual(response_data["status"], InvoiceStatusOptions.cancelled.value)
 
-#     def test_attach_account_to_invoice_attaches_all_billable_charge_items(self):
-#         role = self.create_role_with_permissions(
-#             [
-#                 InvoicePermissions.can_read_invoice.name,
-#                 InvoicePermissions.can_write_invoice.name,
-#             ]
-#         )
-#         self.attach_role_facility_organization_user(self.organization, self.user, role)
-#         self.client.force_authenticate(user=self.user)
+    def test_cancel_invoice_with_already_cancelled_invoice(self):
+        """
+        Test cancelling an invoice that is already cancelled.
+        """
+        self.client.force_authenticate(user=self.superuser)
+        invoice = self.create_invoice(status=InvoiceStatusOptions.cancelled.value)
+        response = self.cancel_invoice(
+            invoice.external_id, InvoiceStatusOptions.cancelled.value
+        )
+        self.assertEqual(response.status_code, 400)
+        response_data = response.data
+        self.assertEqual(
+            response_data["errors"][0]["msg"], "Invoice is already cancelled"
+        )
 
-#         invoice = baker.make(
-#             "emr.Invoice",
-#             facility=self.facility,
-#             account=self.account,
-#             patient=self.patient,
-#             status=InvoiceStatusOptions.draft.value,
-#             total_net=Decimal("0.00"),
-#             total_gross=Decimal("0.00"),
-#         )
+    @override_settings(INVOICE_FREE_CANCEL_PERIOD_MINUTES=5)
+    def test_cancel_invoice_with_user_with_permission_outside_period(self):
+        """
+        Test cancelling an invoice with a user with write permission outside the free cancel period.
+        """
+        permissions = [
+            InvoicePermissions.can_destroy_invoice.name,
+            InvoicePermissions.can_read_invoice.name,
+        ]
+        self.role = self.create_role_with_permissions(permissions)
+        self.attach_role_facility_organization_user(
+            self.organization, self.user, self.role
+        )
+        self.client.force_authenticate(user=self.user)
+        invoice = self.create_invoice()
+        # Manually set the issue_date to be outside the free cancel period
+        invoice.created_date = datetime.now(UTC) - timedelta(minutes=10)
+        invoice.save()
+        response = self.cancel_invoice(
+            invoice.external_id, InvoiceStatusOptions.cancelled.value
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["status"], InvoiceStatusOptions.cancelled.value)
 
-#         response = self.client.post(self._get_url(invoice.external_id))
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-#         invoice.refresh_from_db()
-#         # Assert invoice.charge_items is a proper Python list, not a queryset
-#         self.assertIsInstance(invoice.charge_items, list)
-#         self.assertEqual(len(invoice.charge_items), 2)
-#         self.assertIn(self.charge_item_1.id, invoice.charge_items)
-#         self.assertIn(self.charge_item_2.id, invoice.charge_items)
-
-#         # Assert charge items status updated to billed and paid_invoice set
-#         self.charge_item_1.refresh_from_db()
-#         self.charge_item_2.refresh_from_db()
-#         self.assertEqual(
-#             self.charge_item_1.status, ChargeItemStatusOptions.billed.value
-#         )
-#         self.assertEqual(
-#             self.charge_item_2.status, ChargeItemStatusOptions.billed.value
-#         )
-#         self.assertEqual(self.charge_item_1.paid_invoice, invoice)
-#         self.assertEqual(self.charge_item_2.paid_invoice, invoice)
-
-#     def test_attach_account_to_invoice_requires_draft_status(self):
-#         role = self.create_role_with_permissions(
-#             [
-#                 InvoicePermissions.can_read_invoice.name,
-#                 InvoicePermissions.can_write_invoice.name,
-#             ]
-#         )
-#         self.attach_role_facility_organization_user(self.organization, self.user, role)
-#         self.client.force_authenticate(user=self.user)
-
-#         invoice = baker.make(
-#             "emr.Invoice",
-#             facility=self.facility,
-#             account=self.account,
-#             patient=self.patient,
-#             status=InvoiceStatusOptions.issued.value,
-#         )
-
-#         response = self.client.post(self._get_url(invoice.external_id))
-#         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-
-#     def test_attach_account_to_invoice_without_permission(self):
-#         # Authenticate without attaching the role with permissions to the user
-#         self.client.force_authenticate(user=self.user)
-
-#         invoice = baker.make(
-#             "emr.Invoice",
-#             facility=self.facility,
-#             account=self.account,
-#             patient=self.patient,
-#             status=InvoiceStatusOptions.draft.value,
-#         )
-
-#         response = self.client.post(self._get_url(invoice.external_id))
-#         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+    def test_cancel_invoice_with_user_without_permission_outside_period(self):
+        """
+        Test cancelling an invoice with a user without write permission outside the free cancel period.
+        """
+        self.attach_role_facility_organization_user(
+            self.organization, self.user, self.role
+        )
+        self.client.force_authenticate(user=self.user)
+        invoice = self.create_invoice()
+        # Manually set the issue_date to be outside the free cancel period
+        invoice.created_date = datetime.now(UTC) - timedelta(minutes=10)
+        invoice.save()
+        response = self.cancel_invoice(
+            invoice.external_id, InvoiceStatusOptions.cancelled.value
+        )
+        self.assertEqual(response.status_code, 403)
+        self.assertEqual(response.data["detail"], "Cannot cancel invoice")
