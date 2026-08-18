14837b36f71a76bf17b3360f0b39b4c6d638d7e3
nandkishorr <rachulr7@gmail.com>
2026-08-18 16:13:18 +0530

    fix rr

--- plug_config.py
+++ plug_config.py
@@ -1,16 +1,6 @@
 from plugs.manager import PlugManager
-from plugs.plug import Plug
 
-plugs = [
-    Plug(
-        name="care_encounter_auto_close_be",
-        package_name="/Users/nandkishorr/work/care/care_encounter_auto_close_be",
-        version="",
-        configs={
-            "CARE_ENCOUNTER_AUTO_CLOSE_ENABLED": True,
-        },
-    )
-]
+plugs = []
 
 
 manager = PlugManager(plugs)
